55d773c76a70316459c3e05ab32478ca8f9d29cf
mikkel <mikkelking@hotmail.com>
2018-05-13 07:39:33 +1000

    Fixed up a couple of stories

--- meteor-app/imports/ui/components/roles-main/role-card/index.stories.js
+++ meteor-app/imports/ui/components/roles-main/role-card/index.stories.js
@@ -4,27 +4,24 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 import { Welcome } from '@storybook/react/demo'
-import StoryRouter from 'storybook-router'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react'
 
-import { Link, Router, browserHistory } from 'react-router-dom'
 import { Grid, Container, Segment } from 'semantic-ui-react'
 
-import Avatar from './avatar'
+import Card from './index'
 
-storiesOf('Components', module)
-  .addDecorator(StoryRouter())
+storiesOf('Role', module)
   .addDecorator(withKnobs)
 
-  .add('Avatar', withInfo('Checked in/out')(() => {
+  .add('Card', withInfo('Checked in/out')(() => {
     const story = (
       <div><p>Use Knobs to show checked in/out status</p>
-        <Avatar 
+        <Card 
           _id="aab45bb"
           firstName="Ed"
           lastName="Sheeran"
-          fileName="3.jpg"
+          avatar="3.jpg"
           isCheckedin={boolean('Checked in', false)}
         />
       </div>
