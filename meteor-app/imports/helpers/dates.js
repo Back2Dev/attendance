26ee0bdb544601959cc245b78e6de667a0e681db
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-12 10:55:44 +1000

    change unit of time

--- meteor-app/imports/helpers/dates.js
+++ meteor-app/imports/helpers/dates.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 export function humaniseDate(lastIn) {
   const then = moment(lastIn)
   const now = moment(new Date())
-  const hours = Math.floor(moment.duration(now.diff(then)).as('seconds'))
-  return moment.duration(hours, 'seconds').humanize()
+  const hours = Math.floor(moment.duration(now.diff(then)).as('minutes'))
+  return moment.duration(hours, 'minutes').humanize()
 }
 
