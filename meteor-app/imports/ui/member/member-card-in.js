e844a0218a11317adf097efce39e38c2ce79c72c
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-15 17:55:45 +1000

    fix proptype error

--- meteor-app/imports/ui/member/member-card-in.js
+++ meteor-app/imports/ui/member/member-card-in.js
@@ -28,9 +28,7 @@ const MemberCardIn = (props) => {
             <List.Description>
               <p>Arrived: {humaniseDate(props.lastIn)} ago </p>
             </List.Description>
-            <Button onClick={signOutClick} compact icon={'sign out'}>
-              Sign Out
-            </Button>
+            <Button onClick={signOutClick} compact icon={'sign out'} content='Sign Out' />
           </div>
         }
       </List.Content>
@@ -50,7 +48,6 @@ MemberCardIn.propTypes = {
   lastIn: PropTypes.object,
   toggleStatus: PropTypes.func.isRequired,
   visibleStatus: PropTypes.string.isRequired,
-  depart: PropTypes.func.isRequired,
 }
 
 export default withRouter(MemberCardIn)
