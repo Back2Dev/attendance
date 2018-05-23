ced80e5e0a1a52ec5d7caf72983f55a6dcc77496
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-23 15:18:25 +1000

    remove firstname lastname, change avatar

--- meteor-app/imports/api/members/members.js
+++ meteor-app/imports/api/members/members.js
@@ -1,47 +1,19 @@
 import { Mongo } from 'meteor/mongo'
 import { ValidatedMethod } from 'meteor/mdg:validated-method'
-import * as _ from 'lodash'
 
-class Member {
-  constructor(doc) {
-    _.extend(this, doc)
-    this.name = `${this.firstname} ${this.lastname}`
-  }
-}
-
-const Members = new Mongo.Collection('members', {
-  transform: (doc) => new Member(doc)
-})
-
-// 
-// Members.search = function(query){
-//   console.log('searching method... ', query)
-//   return Members.find({
-//     firstname: { $regex: RegExp(query), $options: 'i' }
-//   }, {
-//     limit: 20
-//   })
-// }
+const Members = new Mongo.Collection('members')
 
 Members.attachSchema(new SimpleSchema({
-  firstname: {
+  name: {
     type: String,
     label: "Name",
     max: 128,
-    defaultValue: "Firstname",
-    optional: true,
-  },
-  lastname: {
-    type: String,
-    label: "Lastname",
-    max: 128,
-    defaultValue: "Lastname",
     optional: true,
   },
   avatar: {
     type: String,
     label: "Avatar file name",
-    defaultValue: "1.jpg",
+    defaultValue: "default.jpg",
   },
   isHere: {
     type: Boolean,
