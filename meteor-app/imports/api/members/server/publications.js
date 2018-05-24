bfa7900c40f47e39621dece0711c3b46f1e2f114
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-24 12:03:48 +1000

    add name / email search on server side

--- meteor-app/imports/api/members/server/publications.js
+++ meteor-app/imports/api/members/server/publications.js
@@ -1,15 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import Members from '../members';
 
-Meteor.publish('all.members', function () {
+Meteor.publish('all.members', () => {
   return Members.find({}, { sort: { lastIn: -1, lastname: 1 } });
 });
 
-Meteor.publish('members.search', function(query){
-  check(query, String)
+Meteor.publish('members.search', (query) => {
+  return Members.find({
+    $or: [
+      { name: { $regex: new RegExp(query), $options: 'i' } },
+      { email: { $regex: new RegExp(query), $options: 'i' } },
+    ],
+    isHere: false
+  })
 
-  if(query == '')
-    return this.ready()
-
-  return Members.search(query)
 })
