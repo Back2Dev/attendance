4b05607c6d95b7d6fdaec384a72a22eaebe6909d
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-24 12:05:50 +1000

    geting search going with conditional subscriptions (issues)

--- meteor-app/imports/ui/member/member-container.js
+++ meteor-app/imports/ui/member/member-container.js
@@ -1,18 +1,28 @@
 import { Meteor } from 'meteor/meteor'
 import { withTracker } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session'
 import MemberMain from '/imports/ui/main'
 import Members from '/imports/api/members/members';
-
+import { debounce } from 'lodash'
 
 export default withTracker((props) => {
-  const membersHandle = Meteor.subscribe('all.members')
-  const loading = !membersHandle.ready()
-  const membersIn = Members.find({ isHere: true }).fetch()
-  const membersOut = Members.find({ isHere: false }).fetch()
-  
+  let membersHandle
+
+  const onSearchInput = q => {
+    debounce( () => Session.set('searchQuery', q) , 300)() 
+  }
+
+  if (Session.get('searchQuery') == '') {
+    membersHandle = Meteor.subscribe('all.members')
+  } else {
+    membersHandle = Meteor.subscribe('members.search', Session.get('searchQuery'))
+  }
+
+
   return {
-    membersIn,
-    membersOut,
-    loading,
+    membersIn: Members.find({ isHere: true }).fetch(),
+    membersOut: Members.find({isHere: false}).fetch(),
+    loading:!membersHandle.ready(),
+    onSearchInput,
   }
 })(MemberMain)
