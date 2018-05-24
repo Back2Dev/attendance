6df87ec7f6092d67cb5e08e3d306091659e3de4a
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-24 15:35:33 +1000

    remove search publish and move query to client

--- meteor-app/imports/api/members/server/publications.js
+++ meteor-app/imports/api/members/server/publications.js
@@ -2,16 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import Members from '../members';
 
 Meteor.publish('all.members', () => {
-  return Members.find({}, { sort: { lastIn: -1, lastname: 1 } });
+  return Members.find({}, { sort: { lastIn: -1, name: 1 } });
 });
-
-Meteor.publish('members.search', (query) => {
-  return Members.find({
-    $or: [
-      { name: { $regex: new RegExp(query), $options: 'i' } },
-      { email: { $regex: new RegExp(query), $options: 'i' } },
-    ],
-    isHere: false
-  })
-
-})
--- meteor-app/imports/ui/member/member-container.js
+++ meteor-app/imports/ui/member/member-container.js
@@ -4,25 +4,32 @@ import { Session } from 'meteor/session'
 import MemberMain from '/imports/ui/main'
 import Members from '/imports/api/members/members';
 import { debounce } from 'lodash'
+import { ReactiveVar } from 'meteor/reactive-var'
+
+const query = new ReactiveVar('')
 
 export default withTracker((props) => {
-  let membersHandle
 
-  const onSearchInput = q => {
-    debounce( () => Session.set('searchQuery', q) , 300)() 
+  const membersHandle = Meteor.subscribe('all.members')
+
+  const onSearchInput = q => query.set(q)
+
+  const filter = (query) => {
+    const searching = query != ''
+    if (searching) {
+      return {
+        name: { $regex: new RegExp(query), $options: 'i' },
+        isHere: false
+      }
+    } else {
+      return { isHere: false }
+    }
   }
-
-  if (Session.get('searchQuery') == '') {
-    membersHandle = Meteor.subscribe('all.members')
-  } else {
-    membersHandle = Meteor.subscribe('members.search', Session.get('searchQuery'))
-  }
-
 
   return {
     membersIn: Members.find({ isHere: true }).fetch(),
-    membersOut: Members.find({isHere: false}).fetch(),
-    loading:!membersHandle.ready(),
-    onSearchInput,
+    membersOut: Members.find(filter(query.get())).fetch(),
+    loading: !membersHandle.ready(),
+    onSearchInput: debounce(onSearchInput, 300),
   }
 })(MemberMain)
