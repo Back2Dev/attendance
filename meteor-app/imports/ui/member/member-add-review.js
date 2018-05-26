33ec0acf320a04f8a7a386b9a48e4c9aab0a7eab
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-26 18:39:28 +1000

    add review page to form

---
+++ meteor-app/imports/ui/member/member-add-review.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { Header, Segment } from 'semantic-ui-react'
+
+const MemberAddReview = (props) => {
+  return (
+    <Segment style={{ textAlign: 'left' }}>
+      
+    <Header as='h1' content='Review your details:' textAlign='center'/>
+{
+        props.steps.map(step => {
+          return (
+            <Segment padded='very'>
+              <Header as='h2' attached='top'>
+                {step.stepTitle}
+              </Header>
+              <Segment.Group>
+                {
+                  Object.keys(step.schema.properties).map(key => {
+                    if (props.formData[key]) {
+                      return (
+                        <Segment>
+                          <strong>{step.schema.properties[key].title}</strong>
+                          <span style={{ paddingLeft: '1em' }}>
+                            {props.formData[key]}
+                          </span>
+                        </Segment>)
+                    }
+                  })
+                }
+              </Segment.Group>
+            </Segment>
+          )
+        })
+      }
+    </Segment>
+  )
+}
+
+MemberAddReview.propTypes = {
+  formData: PropTypes.object.isRequired,
+};
+
+export default MemberAddReview
+
+// {
+//   Object.keys(props.formData).map(key => {
+//     if (props.formData[key]) {
+//       return <div>{key}: {props.formData[key]}</div>
+//     }
+//   })
+// }
+
