aa89da8861bf6810224867a52cefa153a86808a4
Matt Wiseman <mrslwiseman@gmail.com>
2018-05-27 15:41:20 +1000

    add custom avatar widget

---
+++ meteor-app/imports/ui/member/member-add-avatar-widget.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { Image, Segment } from 'semantic-ui-react'
+
+const AvatarWidget = (props) => {
+  return (
+    <Segment>
+      <Image
+        style={{ boxShadow: '0px 2px 3px 3px rgba(0,0,0,.15)' }}
+        centered
+        size='medium'
+        circular
+        src={`/images/avatars/${props.value}`}
+      />
+      <div style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        marginTop: '25px'
+      }}
+      >
+        {
+          props.options.enumOptions.map(option =>
+            (
+              <Image
+                circular
+                style={{
+                  flex: '0 1 20%',
+                  margin: '5px',
+                  boxShadow: '0px 2px 2px 1px rgba(0,0,0,.25)',
+                  border: (props.value == option.value) ? '5px solid #00ff8d' : 'none',
+                  filter: (props.value != option.value) ? 'brightness(80%)' : '',
+                  opacity: (props.value != option.value) ? '.75' : '1',
+                }}
+                size='tiny'
+                src={`/images/avatars/${option.value}`}
+                onClick={(event) => props.onChange(option.value)}
+              />
+            )
+          )
+        }
+      </div>
+      <input
+        hidden
+        type='text'
+        value={props.value}
+        required={props.required}
+      />
+    </Segment>
+  );
+}
+
+AvatarWidget.defaultProps = {
+  value: 'default.jpg'
+};
+
+AvatarWidget.propTypes = {
+  options: PropTypes.object.isRequired,
+};
+
+export default AvatarWidget
--- meteor-app/imports/ui/member/member-add-widgets.js
+++ meteor-app/imports/ui/member/member-add-widgets.js
@@ -1,22 +1,5 @@
-import React from 'react'
-import { Image } from 'semantic-ui-react'
-
-const avatarWidget = (props) => {
-  // 1: read filesystem and generate a list of files that are in there
-  // 2: have a predetermined range of them
-  return (
-    // need to somehow map over them and generate some custom radio checkboxes
-    <div>
-      <Image src="/images/avatars/1.jpg" />
-      <input type="select"
-        className="custom"
-        value={props.value}
-        required={props.required}
-        onChange={(event) => props.onChange(event.target.value)} />
-    </div>
-  );
-}
+import AvatarWidget from '/imports/ui/member/member-add-avatar-widget'
 
 export default {
-  avatarWidget,
+  avatarWidget: AvatarWidget
 };
