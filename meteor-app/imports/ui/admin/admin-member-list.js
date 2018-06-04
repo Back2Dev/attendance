18beeabfc2ea70dd0c15847b0cc470ebca318114
Matt Wiseman <mrslwiseman@gmail.com>
2018-06-04 13:27:21 +1000

    add details to admin list

--- meteor-app/imports/ui/admin/admin-member-list.js
+++ meteor-app/imports/ui/admin/admin-member-list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Image, List } from "semantic-ui-react";
+import { humaniseDate } from '/imports/helpers/dates'
+
 class Admin extends Component {
   constructor(props) {
     super(props)
@@ -12,21 +14,23 @@ class Admin extends Component {
       <List textAlign='left' divided verticalAlign='middle'>
         {
           members.map(member => (
-            <List.Item 
-            style={{ textAlign: 'left' }}
-            key={member._id}>
-              <Image avatar spaced src={"/images/avatars/" + member.avatar} style={{ border: '3px solid white' }} />
+            <List.Item
+              style={{ textAlign: 'left' }}
+              key={member._id}>
+              <Image avatar size='tiny' spaced src={"/images/avatars/" + member.avatar} style={{ border: '3px solid white' }} />
+              <List.Content>
+                <List.Header>
+                  {member.name}
+                </List.Header>
+                <List.Description>
+                  <p>{member.isHere ? 'Arrived:' : 'Last Seen'} {humaniseDate(member.lastIn)} ago </p>
+                </List.Description>
+              </List.Content>
               <List.Content floated='right'>
                 <Button
                   onClick={() => this.props.removeMember(member._id)}
                   content='Remove'
                 />
-              </List.Content>
-              <List.Content
-                as='span'
-                
-              >
-                {member.name}
               </List.Content>
             </List.Item>
           ))
