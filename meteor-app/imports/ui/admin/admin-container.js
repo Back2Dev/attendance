4d3d5d4c22b1914a64325c5b8d5e17bc9e0d8c32
Matt Wiseman <mrslwiseman@gmail.com>
2018-06-04 12:53:50 +1000

    gets search working on admin, could do with refactor

--- meteor-app/imports/ui/admin/admin-container.js
+++ meteor-app/imports/ui/admin/admin-container.js
@@ -2,13 +2,33 @@ import { Meteor } from 'meteor/meteor'
 import { withTracker } from 'meteor/react-meteor-data'
 import Alert from 'react-s-alert';
 const debug = require('debug')('att:admin')
+import { escapeRegExp } from 'lodash'
 
 import Admin from "/imports/ui/admin/admin";
 import Members from '/imports/api/members/members';
 export default withTracker((props) => {
   const membersHandle = Meteor.subscribe('all.members')
   const loading = !membersHandle.ready()
-  const members = Members.find({}, { sort: { sessionCount: -1 } }).fetch()
+
+  const filter = (query) => {
+    const searching = query != ''
+    if (searching) {
+      return {
+        name: { $regex: new RegExp(escapeRegExp(query)), $options: 'i' },
+      }
+    } else {
+      return { }
+    }
+  }
+
+
+  const members = Members.find(
+    filter(Session.get('searchQuery')), {
+      sort: {
+        sessionCount: -1,
+      },
+    },
+  ).fetch()
 
   const removeMember = (memberId) => {
     Meteor.call('members.remove', memberId, (err, res) => {
