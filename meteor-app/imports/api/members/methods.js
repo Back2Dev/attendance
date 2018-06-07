0556d965760b693fa270e960e011841a09570fe3
Matt Wiseman <mrslwiseman@gmail.com>
2018-06-07 18:36:15 +1000

    add forgotPin method

--- meteor-app/imports/api/members/methods.js
+++ meteor-app/imports/api/members/methods.js
@@ -30,5 +30,25 @@ Meteor.methods({
       log.error({ e })
       throw new Meteor.Error(500, e.sanitizedError.reason)
     }
-  }
+  },
+  'members.forgotPin'(id, method, destination) {
+    log.info(`sending pin for member ${id} via ${method} to ${destination}`)
+    try {
+      // make DB query and grab the pin.
+      const member = Members.findOne(id)
+      const pin = member.pin
+      // construct message.
+      const message = 'Your PIN for Back2Bikes attendance app is: \n ' + pin
+      if (method == 'email') {
+        debug('sending PIN reminder via email.', destination, message)
+        return Meteor.call('sendEmail', destination, message, 'Back2Bikes Pin Reminder')
+      } else {
+        // Meteor.call(send sms, message, destination)
+        debug('sending via sms.', message)
+      }
+    } catch (e) {
+      log.error({ e })
+      throw new Meteor.Error(500, e.sanitizedError.reason)
+    }
+  },
 })
