0bd7e2cf967bc0e053545160fe76da4961093e87
Matt Wiseman <mrslwiseman@gmail.com>
2018-07-12 13:24:43 +1000

    create sep layouts

---
+++ meteor-app/imports/ui/layouts/assessment.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Assessment = () => (
+  <div className="assessment-wrapper">
+    <title>Back 2 Bikes | Assessment</title>
+    <h1>Welcome to the B2B Assessment app!</h1>
+  </div>
+)
+
+export default Assessment
--- meteor-app/imports/ui/layouts/attendance.js
+++ meteor-app/imports/ui/layouts/attendance.js
@@ -9,15 +9,11 @@ import MemberVisitContainer from '/imports/ui/member/member-visit-container'
 import AdminContainer from '/imports/ui/admin/admin-container'
 import '/imports/ui/layouts/attendance.css'
 import Nav from '/imports/ui/member/member-nav'
-import Alert from 'react-s-alert';
-import 'react-s-alert/dist/s-alert-default.css';
-import { alertsConfig } from '/imports/ui/config/alerts'
 
 const Attendance = () => {
     return (
       <div className='attendance-wrapper'>
         <title>Back 2 Bikes | Attendance</title>
-        <Alert {...alertsConfig} />
         <Nav />
         <div style={{ marginTop: '70px', height: '100%' }}>
           <Switch>
