468d993f4305fb83dc74da2269499b8f5567c90c
Eryk Grasela <grasela.eryk@gmail.com>
2018-07-14 17:34:06 +1000

    Fix ordering-main-contianer, removed unnecessaary lines

--- meteor-app/imports/ui/ordering/ordering-main-container.js
+++ meteor-app/imports/ui/ordering/ordering-main-container.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { withTracker } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session'
-import { debounce, escapeRegExp } from 'lodash'
 
 import OrderingMain from '/imports/ui/ordering/ordering-main'
 import Parts from '/imports/api/parts/parts';
@@ -10,23 +9,6 @@ Session.set('searchQuery', '')
 
 export default withTracker((props) => {
   const partsHandle = Meteor.subscribe('all.parts')
-  
-//   prevents search filter from persisting after checkin / out
-  if (!partsHandle.ready()) {
-    Session.set('searchQuery', '')
-  }
-
-  const filter = (query) => {
-    const searching = query != ''
-    if (searching) {
-      return {
-        name: { $regex: new RegExp(escapeRegExp(query)), $options: 'i' },
-        isHere: false
-      }
-    } else {
-      return { isHere: false }
-    }
-  }
 
   return {
     parts: Parts.find({}).fetch(),
