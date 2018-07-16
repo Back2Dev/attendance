322ae86e7c111664787c72228fc8b5a26d978762
Jessica <30883219+jessodri@users.noreply.github.com>
2018-07-17 09:41:30 +1000

    Add assessment container and assessment component

---
+++ meteor-app/imports/ui/assessment/assessment-add-control.stories.js
@@ -0,0 +1,58 @@
+// member-add.stories.js
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+// import { linkTo } from '@storybook/addon-links'
+// import { Welcome } from '@storybook/react/demo'
+// import { withInfo } from '@storybook/addon-info'
+import { withKnobs, number, boolean, select } from '@storybook/addon-knobs/react'
+import StoryRouter from 'storybook-router'
+
+import schemas from '/imports/ui/config/bike-assessment-schemas'
+import AssessmentAddControl from './assessment-add-control'
+
+storiesOf('Assessment.Add', module)
+  .addDecorator(withKnobs)
+  .addDecorator(StoryRouter())
+
+  .add('AssessmentAddControl', (() => {
+// NB Select control only works with strings
+    let step = number("Step",1)
+//
+// This event handler fires ok, but doesn't update the control
+// I tried looking at the knobs code, but couldn't work out what
+// to call to trigger the update.
+//
+    const move = (delta) => {
+      try {
+        action("move")(delta)
+  // Updating the step does cause a refresh, methinks it should
+        step = step + delta
+      } catch(error) {
+        console.log("Error", error)
+      }
+    }
+    const story = (
+      <div><p>Use Knobs to select step, currently on step {step}</p>
+        <AssessmentAddControl
+          step={step}
+          backStep={() => move(-1)}
+          nextStep={() => move(1)}
+          onSubmit={action("submit")}
+          totalSteps={5}
+          resetId={action("resetId")}
+        />
+      </div>
+    )
+    // specs(() =>
+    //   describe('<MemberAddReview />', () => {
+    //     it('displays the add member wizard', () => {
+    //       const wrapper = mount(story)
+    //       expect(wrapper.find('button')).to.have.length(1)
+    //       // expect(wrapper.find('button').props().src).to.contain(avatar.url)
+    //     })
+    //   })
+    // )
+    return story
+  }))
---
+++ meteor-app/imports/ui/assessment/assessment-add.js
@@ -0,0 +1,181 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Grid } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom'
+import Form from "react-jsonschema-form-semanticui";
+import Alert from 'react-s-alert';
+
+import schemas from '/imports/ui/config/bike-assessment-schemas'
+
+import Steps from '/imports/ui/assessment/assessment-add-steps'
+import Control from '/imports/ui/assessment/assessment-add-control'
+
+// import MemberAddReview from '/imports/ui/member/member-add-review'
+// import widgets from '/imports/ui/member/member-add-widgets'
+// import fields from '/imports/ui/member/member-add-fields'
+
+const mapSchemaToState = schema => (
+  schema
+    .reduce((state, step) => {
+      Object.keys(step.schema.properties)
+        .forEach(prop => state[prop] = undefined)
+      return state
+    }, {})
+)
+
+class AssessmentAdd extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      step: (props.step) ? props.step : 0,
+      formData: mapSchemaToState(schemas),
+      progress: 0,
+    }
+  }
+
+componentDidMount(){
+  if(this.props.assessment){
+    this.setState({
+      formData: {...this.props.assessment},
+      step: 5,
+      progress: 5,
+    })
+  }
+}
+
+  componentDidUpdate(prevProps, prevState) {
+    window.scrollTo(0, 0)
+
+    const finalStep = schemas.length == this.state.step
+    if (finalStep && this.props.newId) {
+      Alert.success(this.props.message);
+      this.props.history.push(
+        this.props.isIframe
+          ? `/success/${this.props.newId}`
+          : '/'
+      )
+    }
+    if (finalStep && this.props.error) {
+      Alert.error(this.props.message);
+    }
+  }
+
+  componentWillUnmount() {
+    // prevents id from persisting between adding users
+      this.props.resetId()
+  }
+
+  onSubmit = ({ formData }) => {
+    const finalStep = schemas.length == this.state.step
+    if (finalStep) {
+      this.props.setAssessment(this.state.formData)
+      return
+    }
+    this.setState((prevState, props) => {
+      return {
+        formData: {
+          ...prevState.formData,
+          ...formData,
+        },
+        step: prevState.step + 1,
+        progress: prevState.step + 1,
+      }
+    })
+  }
+
+  backStep = () => {
+    this.setState({
+      step: this.state.step - 1
+    })
+  }
+  goToStep = (step) => {
+    if (step <= this.state.progress) {
+      this.setState({
+        step,
+      })
+    }
+  }
+  validate = (formData, errors) => {
+    if(this.props.member != null){
+      return true
+    }
+    if (formData.pin && formData.pin.length < 4) {
+      errors.pin.addError("PIN number must be at least 4 digits long.");
+    }
+    if (formData.pin !== formData.pinConfirm) {
+      errors.pinConfirm.addError("PIN numbers don't match");
+    }
+    return errors;
+  }
+  renderForm = () => {
+    return <Form
+      schema={schemas[this.state.step].schema}
+      uiSchema={schemas[this.state.step].uiSchema}
+      formData={this.state.formData}
+      onSubmit={this.onSubmit}
+      validate={this.validate}
+      widgets={widgets}
+      fields={fields}
+      showErrorList={false} 
+      liveValidate={true}
+    >
+      <Control
+        backStep={this.backStep}
+        step={this.state.step}
+        totalSteps={schemas.length}
+        onSubmit={f => f}
+      />
+    </Form>
+  }
+
+  render() {
+    const finalStep = schemas.length == this.state.step
+    return (
+      <Grid>
+        <Grid.Row centered>
+          <Steps
+            step={this.state.step}
+            steps={schemas}
+            goToStep={this.goToStep}
+            progress={this.state.progress}
+          />
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column style={{ maxWidth: '600px' }}>
+            {
+              finalStep &&
+              // this needs refactoring
+              <div>
+                <MemberAddReview
+                  formData={this.state.formData}
+                  steps={schemas}
+                  goToStep={this.goToStep}
+                />
+                <Control
+                  backStep={this.backStep}
+                  step={this.state.step}
+                  totalSteps={schemas.length}
+                  onSubmit={this.onSubmit}
+                />
+              </div>
+            }
+            {
+              !finalStep &&
+              this.renderForm()
+            }
+          </Grid.Column>
+        </Grid.Row>
+      </Grid >
+    )
+  }
+}
+
+AssessmentAdd.propTypes = {
+  setMember: PropTypes.func.isRequired,
+  resetId: PropTypes.func.isRequired,
+  error: PropTypes.bool.isRequired,
+  success: PropTypes.bool.isRequired,
+  message: PropTypes.string.isRequired,
+}
+
+export default withRouter(AssessmentAdd)
