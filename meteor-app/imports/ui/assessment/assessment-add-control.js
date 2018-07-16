e913868b66427594b4548d686d0a3832576dc01f
Jessica <30883219+jessodri@users.noreply.github.com>
2018-07-17 08:52:17 +1000

    Add assessment form steps and controls functionality

---
+++ meteor-app/imports/ui/assessment/assessment-add-control.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button, Icon } from 'semantic-ui-react'
+
+const AssessmentAddControl = (props) => {
+  return (
+    <div style={{marginTop: "50px"}}>
+      {
+        (props.step >= 1) &&
+        <Button type="button" floated="left" onClick={props.backStep}>
+          <Icon name="arrow left" />
+          Back
+        </Button>
+      }
+      {
+        (props.step < props.totalSteps) &&
+        <Button type="submit" floated="right">
+          Next
+          <Icon name="arrow right" />
+        </Button>
+      }
+      {
+        (props.step === props.totalSteps) &&
+        <Button onClick={props.onSubmit} floated="right" positive>
+          Submit
+        </Button>
+      }
+    </div>
+  )
+}
+
+AssessmentAddControl.propTypes = {
+  step: PropTypes.number.isRequired,
+  backStep: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  totalSteps: PropTypes.number.isRequired,
+}
+
+export default AssessmentAddControl
