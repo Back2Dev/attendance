b1c5ea88408a86445976eadbf2475697dc29c040
Jacob <jacob.avietti@gmail.com>
2018-07-16 13:19:28 +1000

    Add parts card component

---
+++ meteor-app/imports/ui/ordering/ordering-part-card.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const PartCard = (props) => {
+  const {
+    _id,
+    barcode,
+    createdAt,
+    desc,
+    imageUrl,
+    partNo,
+    retailPrice,
+    status,
+    wholesalePrice
+  } = props
+
+  return (
+    <Card className='part-card' key={_id}>
+      <Image src={imageUrl} />
+
+      <Card.Content>
+        <Card.Header>
+          Part Num: {partNo}
+          Description: {desc}
+          Price: {retailPrice}
+        </Card.Header>
+      </Card.Content>
+    </Card>
+  )
+}
+
+// PartsCard.propTypes = {
+
+//   // _id:PropTypes.string.isRequired,
+//   // barcode:,
+//   // createdAt:,
+//   // desc:,
+//   // imageUrl:,
+//   // partNo:,
+//   // retailPrice:,
+//   // status:,
+//   // wholesalePrice:
+
+
+
+//   // className: PropTypes.string,
+//   // _id: PropTypes.string.isRequired,
+//   // name: PropTypes.string.isRequired,
+//   // avatar: PropTypes.string.isRequired,
+//   // isHere: PropTypes.bool.isRequired,
+//   // sessions: PropTypes.array.isRequired,
+//   // lastIn: PropTypes.object,
+//   // sessionCount: PropTypes.number.isRequired,
+// };
+
+export default PartCard
