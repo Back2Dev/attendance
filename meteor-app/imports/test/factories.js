cf580317b5739b22759cc1679456ba9170681c9a
Meng <meng.lim@hotmail.com>
2018-07-18 01:35:01 +1000

    add parts factory

--- meteor-app/imports/test/factories.js
+++ meteor-app/imports/test/factories.js
@@ -12,6 +12,7 @@ import CONSTANTS from '/imports/api/constants'
 import Members from '/imports/api/members/members'
 import Sessions from '/imports/api/sessions/sessions'
 import Orders from '/imports/api/orders/schema'
+import Parts from '/imports/api/parts/schema'
 import { RegExId } from '/imports/api/schema'
 
 Factory.define('member', Members, {
@@ -66,7 +67,16 @@ Factory.define('order', Orders, {
     userId: "2ueueoaje",
   }],
   totalPrice: 9900,   // This is in cents
+})
 
+Factory.define('part', Parts, {
+  imageUrl: '/public/images/logo-large.jpg',
+  retailPrice: 6666, // This is in cents
+  wholesalePrice: 3333,
+  partNo: 'pt-123',
+  desc: 'carbonfibre frame',
+  barcode: '22413000022413',
+  status: 1,
 })
 
 export default Factory
