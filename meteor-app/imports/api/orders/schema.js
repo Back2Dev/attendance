ab9d17909539d558f0a99ecbde6e393317c9af71
Meng <meng.lim@hotmail.com>
2018-07-17 18:32:39 +1000

    fix schema and fix typos and single quotation marks

--- meteor-app/imports/api/orders/schema.js
+++ meteor-app/imports/api/orders/schema.js
@@ -1,41 +1,37 @@
 import { Mongo } from 'meteor/mongo'
 import SimpleSchema from 'simpl-schema'
 import CONSTANTS from '/imports/api/constants.js'
-import sinon from 'sinon';
-
-
 
 import { RegExId, createdAt, updatedAt } from '/imports/api/schema'
 
 export const orderedParts = new SimpleSchema({
   part: {
     type: String,
-    label: "Part",
+    label: 'Part',
   },
   partId: {
     type: String,
-    label: "Part Id",
+    label: 'Part Id',
   },
   partNo: {
     type: String,
-    label: "Part No",
+    label: 'Part No',
   },
   addedAt: {
     type: Date,
-    label: "Date Added",
-    optional: true,
+    label: 'Date Added',
   },
   price: {
     type: SimpleSchema.Integer,
-    label: "Price in cents",
+    label: 'Price in cents',
   },
   qty: {
     type: SimpleSchema.Integer,
-    label: "Quantity",
+    label: 'Quantity',
   },
   userId: {
     type: String,
-    label: "User Id",
+    label: 'User Id',
   },
 })
 
@@ -46,21 +42,19 @@ export const OrdersSchema = new SimpleSchema({
   status: {
     type: SimpleSchema.Integer,
     allowedValues: Object.keys(CONSTANTS.ORDER_STATUS_READABLE).map(key => parseInt(key, 10)),
-    label: "Order Status",
+    label: 'Order Status',
   },
   additionalNotes: {
     type: String,
-    label: "Notes",
+    label: 'Notes',
     optional: true,
   },
   totalPrice: {
     type: Number,
     label: 'Total Cost',
-    required: true,
   },
   orderedParts: {
     type: Array,
-    minCount: 1,
   },
   'orderedParts.$': orderedParts,
   createdAt,
