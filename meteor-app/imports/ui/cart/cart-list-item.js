0550695d6aaf13549c82e7996619e745327c6067
Jacob <jacob.avietti@gmail.com>
2018-07-18 16:44:43 +1000

    Add cart.js and cart-list.js

---
+++ meteor-app/imports/ui/cart/cart-list-item.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Table } from 'semantic-ui-react'
+
+<Table.Row>
+  <Table.Cell>Jamie</Table.Cell>
+  <Table.Cell>Approved</Table.Cell>
+  <Table.Cell>Requires call</Table.Cell>
+</Table.Row>
