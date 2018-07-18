cae3aa667555e7d44cc65beb74b63a40d3e75e2e
Eryk Grasela <grasela.eryk@gmail.com>
2018-07-18 16:13:47 +1000

    Add new navbar

--- meteor-app/imports/ui/layouts/ordering.js
+++ meteor-app/imports/ui/layouts/ordering.js
@@ -4,19 +4,23 @@ import { Switch, Route } from 'react-router-dom'
 import NotFound from '/imports/ui/not-found'
 import Layout from '/imports/ui/ordering/layout'
 import '/imports/ui/layouts/attendance.css'
-import Nav from '/imports/ui/member/member-nav'
+import Nav from '/imports/ui/ordering/navbar'
 import Alert from 'react-s-alert';
+import Cart from '/imports/ui/ordering/cart'
+import '/imports/ui/layouts/ordering.css'
 
 const Ordering = () => {
-  return (
-    <div className='ordering-wrapper'>
-      <title>Back2Bikes | Ordering</title>
-      <Nav />
-      <div style={{ marginTop: '70px', height: '100%' }}>
-        <Switch>
-          <Route path="/" component={Layout} />
-          <Route component={NotFound} />
-        </Switch>
+    return (
+      <div className='ordering-wrapper'>
+        <title>Back2Bikes | Ordering</title>
+        <Nav />
+        <div style={{ marginTop: '70px', height: '100%' }}>
+          <Switch>
+            <Route path="/ordering/cart" component={Cart} />
+            <Route path="/" component={Layout} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </div>
     </div>
   )
