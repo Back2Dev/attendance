d5c6ac0d589ae9f92e5f1e0066a3c6ed931edef5
Jacob <jacob.avietti@gmail.com>
2018-07-18 13:08:37 +1000

    resolve conflicts

--- meteor-app/imports/ui/ordering/ordering-part-card.js
+++ meteor-app/imports/ui/ordering/ordering-part-card.js
@@ -44,20 +44,15 @@ const PartCard = ({
 
         <Divider />
 
-<<<<<<< HEAD
-        <Card.Header>Price ${retailPrice}</Card.Header>
-=======
         <div className='price-container' >
           <b>${retailPrice}</b>
           <Button className='part-add-cart-button' floated='right'>Add To Cart</Button>
         </div>
->>>>>>> dc740869df08c24d83ba65337baa62b32971fbf7
       </Card.Content>
     </Card>
   )
 }
 
-<<<<<<< HEAD
 PartCard.propTypes = {
   _id: PropTypes.string.isRequired,
   barcode: PropTypes.string,
@@ -71,30 +66,3 @@ PartCard.propTypes = {
 };
 
 export default PartCard
-=======
-// PartsCard.propTypes = {
-
-//   // _id:PropTypes.string.isRequired,
-//   // barcode:,
-//   // createdAt:,
-//   // desc:,
-//   // imageUrl:,
-//   // partNo:,
-//   // retailPrice:,
-//   // status:,
-//   // wholesalePrice:
-
-
-
-//   // className: PropTypes.string,
-//   // _id: PropTypes.string.isRequired,
-//   // name: PropTypes.string.isRequired,
-//   // avatar: PropTypes.string.isRequired,
-//   // isHere: PropTypes.bool.isRequired,
-//   // sessions: PropTypes.array.isRequired,
-//   // lastIn: PropTypes.object,
-//   // sessionCount: PropTypes.number.isRequired,
-// };
-
-export default PartCard
->>>>>>> dc740869df08c24d83ba65337baa62b32971fbf7
