c373d0d45d64003d488d12a20aa80920f880792a
ongsterr <ong.chris11@gmail.com>
2018-07-18 11:11:48 +1000

    Add package field for service item

--- meteor-app/imports/api/assessments/services.js
+++ meteor-app/imports/api/assessments/services.js
@@ -8,6 +8,7 @@ export const ServicesSchema = new SimpleSchema({
   _id: RegExId,
   name: { type: String, label: 'Service description' },
   price: { type: SimpleSchema.Integer, label: 'Price in cents' },
+  package: { type: String, label: 'Minor or major service package' },
   createdAt,
   updatedAt,
 })
