ff9774807a9275b3dfcbf3684645eb7e80c125a7
ongsterr <ong.chris11@gmail.com>
2018-07-18 11:15:07 +1000

    Update array reducer code

--- meteor-app/imports/api/assessments/methods.js
+++ meteor-app/imports/api/assessments/methods.js
@@ -12,10 +12,10 @@ Meteor.methods({
     // Check to ensure that total cost from each service item is equal totalServiceCost
     // Check to ensure that total cost from each parts item is equal totalPartsCost
     const calcServiceCost = form.services.serviceItem.reduce((a, b) => {
-      return (a + b['price'])
+      return (a + b.price)
     }, 0)
     const calcPartsCost = form.parts.partsItem.reduce((a, b) => {
-      return (a + b['price'])
+      return (a + b.price)
     }, 0)
     const checkServiceCost = calcServiceCost === form.services.totalServiceCost
     const checkPartsCost = calcPartsCost === form.parts.totalPartsCost
@@ -27,7 +27,7 @@ Meteor.methods({
     // Check to ensure that total cost of repair is the sum of totalServiceCost, totalPartsCost and additionalFees
     const checkTotalCost = (form.services.totalServiceCost + form.parts.totalPartsCost + form.additionalFees) === form.totalCost
     if (!checkTotalCost) {
-      throw new Meteor.Error('Total cost does not equal sum of all cost!')
+      throw new Meteor.Error('Total cost does not equal sum of all costs!')
     }
 
     Assessment.insert(form)
