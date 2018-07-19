939713f3424dac6c0ee09ae8843164df2daa4f70
Meng <meng.lim@hotmail.com>
2018-07-19 17:04:37 +1000

    update methods.test.js

--- meteor-app/imports/api/parts/methods.test.js
+++ meteor-app/imports/api/parts/methods.test.js
@@ -1,15 +1,16 @@
-// import { expect } from 'chai'
-// import { Meteor } from 'meteor/meteor'
-// // import Parts from '/imports/api/parts/schema';
-// import '/imports/api/parts/methods'
+import { expect } from 'chai'
+import { Meteor } from 'meteor/meteor'
+// import Parts from '/imports/api/parts/schema';
+import '/imports/api/parts/methods'
+import { assert } from 'chai'
 
 
 
-// describe('method inserting', () => {
-//     it('insert part into database', () => {
-//         // Meteor.call('parts.insert')
-//     })
-//     console.log(Meteor.server)
-//     console.log(() => Meteor.call(('parts.insert')))
-// })
+describe('database inserting method', () => {
+    it('insert 1 part into database', () => {
+
+    })
+    console.log(Meteor.server)
+    expect(() => Meteor.call(('parts.insert'))).to.throw()
+})
 
