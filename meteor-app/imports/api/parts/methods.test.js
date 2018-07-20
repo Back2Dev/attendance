9f9d06b4a60c865a0c1f444b1a72d111863f0180
Meng <meng.lim@hotmail.com>
2018-07-20 15:12:18 +1000

    add parts.insert method

--- meteor-app/imports/api/parts/methods.test.js
+++ meteor-app/imports/api/parts/methods.test.js
@@ -1,5 +1,20 @@
 import { expect } from 'chai'
 import { Meteor } from 'meteor/meteor'
-import Parts from '/imports/api/parts/schema';
+import Parts from '/imports/api/parts/schema'
 import '/imports/api/parts/methods'
 import Factory from '/imports/test/factories'
+import { resetDatabase } from '/imports/test/util-test'
+
+
+const goodPart = Factory.build('part')
+
+if (Meteor.isServer) {
+  // beforeEach(resetDatabase)
+  describe('test methods for inserting part', () => {
+
+    it('parts.insert method is successful', () => {
+      expect(() => Meteor.call('parts.insert', goodPart)).to.not.throw()
+    })
+    console.log(goodPart)
+  })
+}
