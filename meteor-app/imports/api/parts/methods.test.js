48a94032c45a8f6a5c6d4a0f81a966cc11dcdce4
Meng <meng.lim@hotmail.com>
2018-07-22 16:22:47 +1000

    change factory field desc to name and update parts.insert methods test

--- meteor-app/imports/api/parts/methods.test.js
+++ meteor-app/imports/api/parts/methods.test.js
@@ -4,17 +4,22 @@ import Parts from '/imports/api/parts/schema'
 import '/imports/api/parts/methods'
 import Factory from '/imports/test/factories'
 import { resetDatabase } from '/imports/test/util-test'
+import { relativeTimeThreshold } from '../../../node_modules/moment';
 
 
 const goodPart = Factory.build('part')
 
-if (Meteor.isServer) {
-  // beforeEach(resetDatabase)
-  describe('test methods for inserting part', () => {
 
+if (Meteor.isServer) {
+  describe('test methods for inserting part', () => {
     it('parts.insert method is successful', () => {
       expect(() => Meteor.call('parts.insert', goodPart)).to.not.throw()
+      expect(goodPart).to.be.a('object')
     })
-    console.log(goodPart)
+
+    it('should contain required keys: imageUrl,retailPrice,wholesalePrice and partNo', () => {
+      expect(goodPart).to.contain.keys('_id', 'imageUrl', 'retailPrice', 'wholesalePrice', 'partNo')
+    })
   })
 }
+
