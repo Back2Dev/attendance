7cc5d96c327905e1d19fd10ca9385e1598c0f341
WhalleyMac <andrewcwhalley@gmail.com>
2018-07-23 23:54:00 +1000

    Complete PD MAKEF to print for jobs

--- meteor-app/imports/ui/assessment/assessment-print-job.js
+++ meteor-app/imports/ui/assessment/assessment-print-job.js
@@ -1,145 +1,105 @@
 import pdfMake from "pdfmake/build/pdfmake";
 import vfsFonts from "pdfmake/build/vfs_fonts";
 
+export default assessment => {
+  const {
+    assessor,
+    bikeDetails,
+    customerDetails,
+    parts,
+    pickupDate,
+    services,
+    temporaryBike,
+    totalCost,
+    urgent
+  } = assessment;
 
-export default (assessment) => {
-  const { assessor, bikeDetails, customerDetails, dropoffDate, parts, pickupDate, services, temporaryBike, totalCost, urgent } = assessment 
-  var docDefinition = { 
+  const serviceItemNames = services.serviceItem.map(item => item.name);
+  const servicePartNames = parts.partsItem.map(item => {
+    if (!item.name) {
+      return "There are no parts for this service";
+    } else {
+      return item.name;
+    }
+  });
+
+  const bikeModel = bikeDetails.model ? ` - ${bikeDetails.model}` : "";
+
+  var docDefinition = {
     content: [
-      {text: `Bike: ${bikeDetails.make} ${bikeDetails.model} ${bikeDetails.color}`, style: 'subheader'},
-    `Assessor: ${assessor} `,
-    `Base Service: ${services.baseService}`,
-		{
-			bold: true,
-			ul: [
-				'auto'
-			]
-		},
-		{
-			style: 'tableExample',
-			table: {
-				widths: [100, '*', 200, '*'],
-				body: [
-					['width=100', 'star-sized', 'width=200', 'star-sized'],
-					['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}]
-				]
-			}
-		},
-		{
-			style: 'tableExample',
-			table: {
-				widths: ['*', 'auto'],
-				body: [
-					['This is a star-sized column. The next column over, an auto-sized column, will wrap to accomodate all the text in this cell.', 'I am auto sized.'],
-				]
-			}
-		},
-		{
-			style: 'tableExample',
-			table: {
-				widths: ['*', 'auto'],
-				body: [
-					['This is a star-sized column. The next column over, an auto-sized column, will not wrap to accomodate all the text in this cell, because it has been given the noWrap style.', {text: 'I am auto sized.', noWrap: true}],
-				]
-			}
-		},
-		{text: 'Defining row heights', style: 'subheader'},
-		{
-			style: 'tableExample',
-			table: {
-				heights: [20, 50, 70],
-				body: [
-					['row 1 with height 20', 'column B'],
-					['row 2 with height 50', 'column B'],
-					['row 3 with height 70', 'column B']
-				]
-			}
-		},
-		'With same height:',
-		{
-			style: 'tableExample',
-			table: {
-				heights: 40,
-				body: [
-					['row 1', 'column B'],
-					['row 2', 'column B'],
-					['row 3', 'column B']
-				]
-			}
-		},
-		'With height from function:',
-		{
-			style: 'tableExample',
-			table: {
-				heights: function (row) {
-					return (row + 1) * 25;
-				},
-				body: [
-					['row 1', 'column B'],
-					['row 2', 'column B'],
-					['row 3', 'column B']
-				]
-			}
-		},
-		{text: 'Column/row spans', pageBreak: 'before', style: 'subheader'},
-		'Each cell-element can set a rowSpan or colSpan',
-		{
-			style: 'tableExample',
-			color: '#444',
-			table: {
-				widths: [200, 'auto', 'auto'],
-				headerRows: 2,
-				// keepWithHeaderRows: 1,
-				body: [
-					[{text: 'Header with Colspan = 2', style: 'tableHeader', colSpan: 2, alignment: 'center'}, {}, {text: 'Header 3', style: 'tableHeader', alignment: 'center'}],
-					[{text: 'Header 1', style: 'tableHeader', alignment: 'center'}, {text: 'Header 2', style: 'tableHeader', alignment: 'center'}, {text: 'Header 3', style: 'tableHeader', alignment: 'center'}],
-					['Sample value 1', 'Sample value 2', 'Sample value 3'],
-					[{rowSpan: 3, text: 'rowSpan set to 3\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'}, 'Sample value 2', 'Sample value 3'],
-					['', 'Sample value 2', 'Sample value 3'],
-					['Sample value 1', 'Sample value 2', 'Sample value 3'],
-					['Sample value 1', {colSpan: 2, rowSpan: 2, text: 'Both:\nrowSpan and colSpan\ncan be defined at the same time'}, ''],
-					['Sample value 1', '', ''],
-				]
-			}
-		},
-		{text: 'Headers', pageBreak: 'before', style: 'subheader'},
-		'You can declare how many rows should be treated as a header. Headers are automatically repeated on the following pages',
-		{text: ['It is also possible to set keepWithHeaderRows to make sure there will be no page-break between the header and these rows. Take a look at the document-definition and play with it. If you set it to one, the following table will automatically start on the next page, since there\'s not enough space for the first row to be rendered here'], color: 'gray', italics: true},
-		{
-			style: 'tableExample',
-			table: {
-				headerRows: 1,
-				// dontBreakRows: true,
-				// keepWithHeaderRows: 1,
-				body: [
-					[{text: 'Header 1', style: 'tableHeader'}, {text: 'Header 2', style: 'tableHeader'}, {text: 'Header 3', style: 'tableHeader'}],
-					[
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-						'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-					]
-				]
-			}
-		},
-    ], 
+      {
+        text: `Bike: ${bikeDetails.make} ${bikeModel} - ${
+          bikeDetails.color
+        } - Aprox Value $${bikeDetails.bikeValue / 100}`,
+        style: "subheader",
+        fontSize: 20
+      },
+
+      { text: `Assessor: ${assessor} `, style: "text" },
+      { text: `Base Service: ${services.baseService}`, style: "text" },
+
+      { text: "Service Items", style: "subheader" },
+      { ul: serviceItemNames },
+      { text: "Part Items", style: "subheader" },
+      { ul: servicePartNames },
+
+      { text: "Comments", style: "subheader" },
+
+      {
+        ul: [
+          { text: "Comments will go here!" },
+          { text: `Replacement bike required? - ${temporaryBike}` },
+          {
+            text: `Does the bike have sentimental value? - ${
+              bikeDetails.sentimentValue
+            }`,
+            style: "text"
+          },
+          { text: `Is this request urgent? - ${urgent} ` }
+        ]
+      },
+      { text: "Owner", style: "subheader" },
+      {
+        ul: [
+          { text: customerDetails.name, bold: true },
+          { text: customerDetails.email, bold: true },
+          { text: customerDetails.phone, bold: true }
+        ]
+      },
+
+      {
+        text: `Pick Up Date: ${pickupDate.toDateString()}`,
+        style: "subheader",
+        bold: true
+      },
+      {
+        text: `Total Price $${totalCost / 100}`,
+        style: "subheader",
+        bold: true
+      }
+    ],
     styles: {
       header: {
-        fontSize: 18,
+        fontSize: 24,
         bold: true,
         margin: [0, 0, 0, 10]
       },
       subheader: {
-        fontSize: 16,
+        fontSize: 18,
         bold: true,
         margin: [0, 10, 0, 5]
       },
-      tableExample: {
-        margin: [0, 5, 0, 15]
+      text: {
+        fontSize: 12,
+        margin: [5, 5, 5, 5]
+      },
+      servicesTable: {
+        margin: [5, 5, 5, 15]
       },
       tableHeader: {
         bold: true,
-        fontSize: 13,
-        color: 'black'
+        fontSize: 15,
+        color: "black"
       }
     },
     defaultStyle: {
@@ -147,4 +107,4 @@ export default (assessment) => {
     }
   };
   pdfMake.createPdf(docDefinition).download();
-}
+};
