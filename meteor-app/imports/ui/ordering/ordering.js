3fc7732dbbfe5689038feb1428e4b65dfd364279
Eryk Grasela <grasela.eryk@gmail.com>
2018-07-24 22:11:40 +1000

    Add loader and aligned top search/cart/uplaod file bar

--- meteor-app/imports/ui/ordering/ordering.js
+++ meteor-app/imports/ui/ordering/ordering.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
-import { Grid, Segment, Header, Input, Button } from 'semantic-ui-react'
-
+import { Grid, Segment, Header, Input, Button, Dimmer, Loader, Image, Message} from 'semantic-ui-react'
+import Alert from 'react-s-alert';
 import PartCard from '/imports/ui/ordering/ordering-part-card'
 import PartList from '/imports/ui/ordering/ordering-part-list'
 import CartIcon from '/imports/ui/ordering/cart-icon'
@@ -21,37 +21,68 @@ class Ordering extends React.Component {
 
   }
   render() {
+    if(this.props.loading){
+      return(
+     
+        <div>
+            <Dimmer active inverted>
+              <Loader size='large'>Loading</Loader>
+            </Dimmer>
+
+          <Image src='/images/wireframe/short-paragraph.png' />
+       </div>
+
+      )
+    }
   const { activeOrder, uploadXL } = this.props
   let noOfParts = 0
+  
     return (
+      
       <Grid centered columns={1}>
-        <Grid.Row>
+  
+      <Header as='h2' textAlign='center'> <div>Parts</div> </Header>
+     <Grid centered columns={3} width={4}>
+        <Grid.Row columns='equal' verticalAlign='top'>
+       <Grid.Column>
+            {(!this.props.loading && activeOrder) && activeOrder.orderedParts.forEach(part => {
+              noOfParts += part.qty
+              return noOfParts
+            }) }
+             <CartIcon noOfParts={noOfParts} /> 
+        </Grid.Column>
+ <Grid.Column>
         <Input
         placeholder='Search Part Number'
         onChange={this.props.onSearchInput}
         value={this.props.partSearchQuery}
         icon={'search'}
       />
-          </Grid.Row>
-          <Grid.Row>
+          </Grid.Column>
+         <Grid.Column >
           {this.state.addParts 
           ? 
-          <Button content="Upload" onClick={this.toggleAddPart}/>
+          <Button content="Upload parts data file now!" onClick={this.toggleAddPart}/>
           : 
           <UploadXL uploadXL={uploadXL} toggleAddPart={this.toggleAddPart}/>}
-          </Grid.Row>
+         
+          {(this.props.parts < 1 ? <Message
+                error
+                header='There is no Parts Data'
+                list={[
+                  'Please upload your data file above'
+                ]}
+              /> : '')}
+       
+ 
+         
+
+             </Grid.Column>
+             </Grid.Row>
+     </Grid>
           <Grid.Row>
           <Grid.Column>
-            
-          <Segment raised>
-            {(!this.props.loading && activeOrder) && activeOrder.orderedParts.forEach(part => {
-              noOfParts += part.qty
-              return noOfParts
-            }) }
-            <Header as='h2' textAlign='center'> <div>Parts</div>  <CartIcon noOfParts={noOfParts} /> </Header>
-            
 
-            </Segment>
             <PartList
               title={'Part Title:'}
               parts={this.props.parts}
--- meteor-app/imports/ui/ordering/uploadXL.js
+++ meteor-app/imports/ui/ordering/uploadXL.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Alert from 'react-s-alert';
-
+import { Grid } from 'semantic-ui-react'
 import { Button, Form, Input, Header } from 'semantic-ui-react'
 
 const UploadXL = ({ uploadXL, toggleAddPart }) => (
@@ -10,8 +10,14 @@ const UploadXL = ({ uploadXL, toggleAddPart }) => (
     uploadXL(e)
   }}>
     <Header> Upload your xlsx file! </Header>
+    <Grid centered>
+    <Grid.Row>
     <Input type="file" />
-    <Button type="submit" content="Upload a new data file" />
+    </Grid.Row>
+    <Grid.Row>
+    <Button type="submit" content="Upload a new data file" positive />
+    </Grid.Row>
+    </Grid>
   </Form>
 )
 
