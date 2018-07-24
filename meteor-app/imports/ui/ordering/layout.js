8015d11c7361422a034dbe1d6d87e7cf81a25674
Eryk Grasela <grasela.eryk@gmail.com>
2018-07-24 22:43:29 +1000

    Fix alerts on no file upload

--- meteor-app/imports/ui/ordering/layout.js
+++ meteor-app/imports/ui/ordering/layout.js
@@ -29,9 +29,9 @@ export default withTracker((props) => {
   function uploadXL(e) {
     
     e.preventDefault()
-   
+    
     const input = e.target[0]
-    !input ? Alert.info(`Adding your parts`) : Alert.info(`Oops! Forgot to add the file? Try again uploading the file`)
+    input.files[0] ? Alert.info(`Adding your parts`) : Alert.info(`Oops! Forgot to add the file? Try again uploading the file`)
     const reader = new FileReader()
     reader.onloadend = function () {
       const data = reader.result
