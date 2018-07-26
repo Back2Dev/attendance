1f3ca206bcf50f853dcec89d041fa68f941a08b7
Mike king <mikkelking@hotmail.com>
2018-07-26 18:07:27 +1000

    rm ;

--- meteor-app/imports/ui/ordering/layout.js
+++ meteor-app/imports/ui/ordering/layout.js
@@ -1,34 +1,34 @@
-import { Meteor } from "meteor/meteor";
-import { withTracker } from "meteor/react-meteor-data";
-import { Session } from "meteor/session";
-import Ordering from "/imports/ui/ordering/ordering";
-import Parts from "/imports/api/parts/schema";
-import Orders from "/imports/api/orders/schema";
-import { ReactiveVar } from "meteor/reactive-var";
-import Alert from "react-s-alert";
-import CONSTANTS from "/imports/api/constants";
-import { escapeRegExp } from "lodash";
-const success = new ReactiveVar(false);
-const error = new ReactiveVar(false);
-const msg = new ReactiveVar("");
-const newId = new ReactiveVar("");
+import { Meteor } from "meteor/meteor"
+import { withTracker } from "meteor/react-meteor-data"
+import { Session } from "meteor/session"
+import Ordering from "/imports/ui/ordering/ordering"
+import Parts from "/imports/api/parts/schema"
+import Orders from "/imports/api/orders/schema"
+import { ReactiveVar } from "meteor/reactive-var"
+import Alert from "react-s-alert"
+import CONSTANTS from "/imports/api/constants"
+import { escapeRegExp } from "lodash"
+const success = new ReactiveVar(false)
+const error = new ReactiveVar(false)
+const msg = new ReactiveVar("")
+const newId = new ReactiveVar("")
 
 function setError(e) {
-  newId.set(null);
-  error.set(true);
-  success.set(false);
-  msg.set(e.reason);
-  Alert.error(e.reason);
+  newId.set(null)
+  error.set(true)
+  success.set(false)
+  msg.set(e.reason)
+  Alert.error(e.reason)
 }
-Session.set("partSearchQuery", "");
+Session.set("partSearchQuery", "")
 export default withTracker(props => {
-  const partsHandle = Meteor.subscribe("all.parts");
-  const ordersHandle = Meteor.subscribe("all.orders");
+  const partsHandle = Meteor.subscribe("all.parts")
+  const ordersHandle = Meteor.subscribe("all.orders")
   if (!partsHandle.ready()) {
-    Session.set("partSearchQuery", "");
+    Session.set("partSearchQuery", "")
   }
   const filter = query => {
-    const searching = query != "";
+    const searching = query != ""
     if (searching) {
       return {
         $or: [
@@ -37,52 +37,52 @@ export default withTracker(props => {
           },
           { name: { $regex: new RegExp(escapeRegExp(query)), $options: "i" } }
         ]
-      };
+      }
     } else {
-      return {};
+      return {}
     }
-  };
+  }
   const addToCart = async orderedPart => {
     const currentOrder = await Orders.findOne({
       status: CONSTANTS.ORDER_STATUS_NEW
-    });
-    let totalPrice = currentOrder.totalPrice + orderedPart.price;
-    let orderedParts = currentOrder.orderedParts;
+    })
+    let totalPrice = currentOrder.totalPrice + orderedPart.price
+    let orderedParts = currentOrder.orderedParts
     let found = orderedParts.find(function(part) {
-      return part.partId === orderedPart.partId;
-    });
+      return part.partId === orderedPart.partId
+    })
     if (!found) {
       const res = await Meteor.callAsync(
         "orders.addPart",
         currentOrder._id,
         orderedPart,
         totalPrice
-      );
+      )
       if (res) {
-        Alert.info(`Successfully added ${orderedPart.name} to cart`);
+        Alert.info(`Successfully added ${orderedPart.name} to cart`)
       }
     } else {
       orderedParts.forEach(p => {
         if (p.partId === orderedPart.partId) {
-          p.qty += 1;
-          return p;
+          p.qty += 1
+          return p
         }
-      });
+      })
       const res = await Meteor.callAsync(
         "order.updateQty",
         currentOrder._id,
         orderedParts,
         totalPrice
-      );
+      )
       if (res) {
-        Alert.info(`Successfully added ${orderedPart.name} to cart`);
+        Alert.info(`Successfully added ${orderedPart.name} to cart`)
       }
     }
-  };
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
   }
-  const onSearchInput = q => Session.set("partSearchQuery", q.target.value);
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+  }
+  const onSearchInput = q => Session.set("partSearchQuery", q.target.value)
 
   return {
     activeOrder: Orders.findOne({ status: CONSTANTS.ORDER_STATUS_NEW }),
@@ -94,5 +94,5 @@ export default withTracker(props => {
     loading: !partsHandle.ready() || !ordersHandle.ready(),
     partSearchQuery: Session.get("partSearchQuery"),
     onSearchInput
-  };
-})(Ordering);
+  }
+})(Ordering)
