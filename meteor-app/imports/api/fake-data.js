6313cea485a728e93ba2699f6a42ae8c9f50146e
Jessica <30883219+jessodri@users.noreply.github.com>
2018-08-14 11:16:48 +1000

    Add fake job cards method

--- meteor-app/imports/api/fake-data.js
+++ meteor-app/imports/api/fake-data.js
@@ -2,68 +2,70 @@ import faker from 'faker'
 import { JOB_STATUS_READABLE } from '/imports/api/constants'
 
 export const fakeJob = () => {
-  faker.seed(123)
+  // faker.seed(123)
+  const name = faker.name.findName()
+  const make = faker.random.arrayElement(['Apollo', 'Giant', 'Malvern Star', 'Specialized'])
+  const color = faker.commerce.color()
+
   return {
-    _id: '1234',
     customerDetails: {
-      name: faker.name.findName(),
+      name,
       phone: faker.phone.phoneNumberFormat(),
       email: faker.internet.email(),
       refurbishment: faker.random.boolean(),
     },
     bikeDetails: {
-      make: faker.random.arrayElement(['Apollo', 'Giant', 'Malvern Star', 'Specialized']),
+      make,
       model: faker.random.arrayElement(['TZ1000', 'Fireball', 'Chicken Dance', 'Zoom2000']),
-      color: faker.commerce.color(),
+      color,
       bikeValue: faker.random.number({ min: 500, max: 100000 }),
       sentimentValue: faker.random.boolean(),
     },
     services: {
       serviceItem: [
         {
-          name: 'item1',
-          price: 3400,
+          name: faker.random.arrayElement(['Check functionality/adjust brakes and gears', 'Remove, clean and oil chain', 'Check wheels are true']),
+          price: 100,
         },
         {
-          name: 'item2',
-          price: 4500,
+          name: faker.random.arrayElement(['Check functionality/adjust brakes and gears', 'Remove, clean and oil chain', 'Check wheels are true']),
+          price: 100,
         },
         {
-          name: 'item3',
-          price: 6400,
+          name: faker.random.arrayElement(['Check functionality/adjust brakes and gears', 'Remove, clean and oil chain', 'Check wheels are true']),
+          price: 100,
         },
       ],
       baseService: faker.random.arrayElement(['Minor Service', 'Major Service', 'Custom Service']),
-      totalServiceCost: 6700,
+      totalServiceCost: 300,
     },
     parts: {
       partsItem: [
         {
-          name: 'item1',
-          price: 3400,
+          name: 'Tube',
+          price: 100,
         },
         {
-          name: 'item2',
-          price: 4500,
+          name: 'Cable',
+          price: 100,
         },
         {
-          name: 'item3',
-          price: 6400,
+          name: 'Tyre',
+          price: 100,
         },
       ],
-      totalPartsCost: 100,
-      additionalFees: 4500,
-      totalCost: 100,
+      totalPartsCost: 300,
     },
-    totalCost: 12000,
+    additionalFees: 100,
+    totalCost: 700,
     dropoffDate: new Date(),
     pickupDate: new Date(),
     urgent: faker.random.boolean(),
     assessor: faker.name.findName(),
     mechanic: faker.name.findName(),
-    comment: 'This Bike Is Amazing',
+    comment: 'This Bike Is Amazing', 
     temporaryBike: faker.random.boolean(),
-    status: faker.random.arrayElement([Object.keys(JOB_STATUS_READABLE)]),
-    search: 'name bike make colour',
+    status: parseInt(faker.random.arrayElement(Object.keys(JOB_STATUS_READABLE))),
+    search: `${name} ${make} ${color}`,
   }
 }
