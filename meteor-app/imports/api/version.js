c25d7f2bc1dea244cfabcce445247d5f9603cf50
Mike King <mikkelking@hotmail.com>
2018-11-21 23:24:24 +1100

    Completed templating of meteor-app/0.9.0

--- meteor-app/imports/api/version.js
+++ meteor-app/imports/api/version.js
@@ -1,11 +1,13 @@
 /**
- * Created by mikkel in June 2016.
+ * Created by mikkel in June 2017.
  */
 
 var versionService = {
-  version: function() {
-    return "0.2.0"
-  }
-}
 
-module.exports = versionService
+  version: function() {
+    return "0.9.0"
+  }
+
+};
+
+module.exports = versionService;
