0b2ffd895ff7526988cda0bfdf35de7820ca3b38
Mike King <mikkelking@hotmail.com>
2018-11-21 23:19:11 +1100

    Added more avatars

--- meteor-app/imports/ui/config/member-add-schemas.js
+++ meteor-app/imports/ui/config/member-add-schemas.js
@@ -147,8 +147,8 @@ export default [
           type: "string",
           title: "Avatar",
           default: "default.jpg",
-          enum: ["default.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg"],
-          enumNames: ["default", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen"]
+          enum: ["default.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "test11.png",  "test14.png", "test16.png", "test17.png", "test18.png", "test19.png", "test20.png", "test21.png", "test24.png", "test25.png", "test26.png", "test28.png", "test29.png"],
+          enumNames: ["default", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11", "t11"]
         },
       },
     },
