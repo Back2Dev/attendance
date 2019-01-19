48c9344844e834cda4e4b1a604681e1ce16f6f1c
mikkel <mikkelking@hotmail.com>
2019-01-19 14:31:43 +1100

    removed a debugger and target-density - fixes older ipad

--- meteor-app/imports/startup/server/members-data.js
+++ meteor-app/imports/startup/server/members-data.js
@@ -1,9 +1,9 @@
-import { Meteor } from 'meteor/meteor' // base
-import Members from '/imports/api/members/members'
-import Rejects from '/imports/api/members/rejects'
-import casual from 'casual'            // casual random data generator
-import moment from 'moment'
-const debug = require('debug')('b2b:members')
+import { Meteor } from "meteor/meteor"; // base
+import Members from "/imports/api/members/members";
+import Rejects from "/imports/api/members/rejects";
+import casual from "casual"; // casual random data generator
+import moment from "moment";
+const debug = require("debug")("b2b:members");
 
 // A little deviousness on allowing bulk import of member data
 //
@@ -11,51 +11,51 @@ const debug = require('debug')('b2b:members')
 // 2) The /private/members.json folder must exist (won't happen in prod)
 // 3) You can do a Meteor.call('import.members','Special member')
 //
-casual.seed(+new Date()) // Just use the current time as a seed
-const specialMember = casual.full_name
+casual.seed(+new Date()); // Just use the current time as a seed
+const specialMember = casual.full_name;
 // const specialMember='Jayne Rutherford'
-debug(`Special member = ${specialMember}`)
+debug(`Special member = ${specialMember}`);
 
 Meteor.methods({
-  'import.members'(secret) {
+  "import.members"(secret) {
     if (secret === specialMember) {
       //
       // Clean up
       //
-      Rejects.remove({})
-      Members.remove({})
+      Rejects.remove({});
+      Members.remove({});
 
-      debug('importing members....')
-      const membersArray = JSON.parse(Assets.getText('members.json'))
+      debug("importing members....");
+      const membersArray = JSON.parse(Assets.getText("members.json"));
       membersArray.forEach(member => {
         try {
-          member.name = `${member.firstname} ${member.lastname}`
-          const existing = Members.findOne({ email: member.email })
+          member.name = `${member.firstname} ${member.lastname}`;
+          const existing = Members.findOne({ email: member.email });
           if (existing) {
-            debug(`${member.name} exists already`)
-            member.reason = "Duplicate"
-            Rejects.insert(member)
+            debug(`${member.name} exists already`);
+            member.reason = "Duplicate";
+            Rejects.insert(member);
           } else {
-            debug("+ " + member.name)
-            Members.insert(member)
+            debug("+ " + member.name);
+            Members.insert(member);
           }
         } catch (error) {
-          debug(`Error [${error.message}], Failed to import `, member)
-          member.reason = error.message
-          Rejects.insert(member)
+          debug(`Error [${error.message}], Failed to import `, member);
+          member.reason = error.message;
+          Rejects.insert(member);
         }
-      })
+      });
     } else {
       throw new Meteor.Error(`Members import was moved to a private repo 
-        for security reasons (unless you know a secret code)`)
+        for security reasons (unless you know a secret code)`);
     }
   },
-  'seed.members'() {
-    const n = 10
+  "seed.members"() {
+    const n = 10;
     // seed ensures same data is generated
-    casual.seed(123)
+    casual.seed(123);
 
-    const array_of = function (times, generator) {
+    const array_of = function(times, generator) {
       let result = [];
       for (let i = 0; i < times; ++i) {
         result.push(generator());
@@ -63,16 +63,25 @@ Meteor.methods({
       return result;
     };
 
-    casual.define('member', function () {
+    casual.define("member", function() {
       return {
         avatar: `${casual.integer(1, 10)}.jpg`,
         sessionCount: casual.integer(1, 10),
-        sessions: array_of(casual.integer(1, 16), () => ({ memberId: 'randomSession' })),
-        lastIn: moment().subtract(casual.integer(1, 168), 'hours').toDate(),
+        sessions: array_of(casual.integer(1, 16), () => ({
+          memberId: "randomSession"
+        })),
+        lastIn: moment()
+          .subtract(casual.integer(1, 168), "hours")
+          .toDate(),
         addressPostcode: casual.integer(3000, 4000).toString(),
         addressState: "VIC",
         addressStreet: casual.address1,
-        addressSuburb: casual.random_element(["Melbourne", "St Kilda", "Middle Park", "South Melbourne"]),
+        addressSuburb: casual.random_element([
+          "Melbourne",
+          "St Kilda",
+          "Middle Park",
+          "South Melbourne"
+        ]),
         bikesHousehold: casual.integer(1, 10),
         email: casual.email,
         emergencyContact: casual.full_name,
@@ -82,39 +91,46 @@ Meteor.methods({
         mobile: casual.phone,
         name: casual.full_name,
         phone: casual.phone,
-        workStatus: casual.random_element(["Full Time", "Part Time", "Pension/Disability", "Unemployed", "Student", "Retired"]),
+        workStatus: casual.random_element([
+          "Full Time",
+          "Part Time",
+          "Pension/Disability",
+          "Unemployed",
+          "Student",
+          "Retired"
+        ]),
         reasons: casual.sentences(3),
-        primaryBike: casual.random_element(["Road/racer", "Hybrid", "Mountain", "Cruiser", "Ladies", "Gents", "Fixie/Single Speed"]),
-      }
-    })
+        primaryBike: casual.random_element([
+          "Road/racer",
+          "Hybrid",
+          "Mountain",
+          "Cruiser",
+          "Ladies",
+          "Gents",
+          "Fixie/Single Speed"
+        ])
+      };
+    });
 
-    const membersArray =
-      array_of(n, () => casual.member)
-        .forEach(r => Members.insert(r))
+    const membersArray = array_of(n, () => casual.member).forEach(r =>
+      Members.insert(r)
+    );
   }
-})
+});
 
 Meteor.startup(() => {
   if (Members.find().count() === 0) {
-    Meteor.call('seed.members')
+    Meteor.call("seed.members");
   }
 
   // Migration script, give all records an isSuper field
-  Members.update(
-    { "isSuper": { $exists: false } },
-    { $set: { isSuper: false } },
-    { multi: true },
-  )
+  Members.update({ isSuper: { $exists: false } }, { $set: { isSuper: false } });
 
   // Migration script, Set Mark to isSuper
-  Members.update(
-    { name: "Mark Bradley" },
-    { $set: { isSuper: true } },
-  )
+  Members.update({ name: "Mark Bradley" }, { $set: { isSuper: true } });
   // Migration script, give all records a default pin
   // Members.update(
   //   { "pin": { $exists: false } },
-  //   { $set: { pin: '12         34' } },
-  //   { multi: true }
+  //   { $set: { pin: '12         34' } }
   // )
-})
+});
--- meteor-app/imports/ui/config/member-add-schemas.js
+++ meteor-app/imports/ui/config/member-add-schemas.js
@@ -8,9 +8,35 @@ const b2bSchema = {
       title: "Lets get to know each other.",
       // required: ["bikesHousehold", "reasons"],
       properties: {
-        bikesHousehold: { type: "number", title: "How many bikes in your household?" },
-        primaryBike: { type: "string", title: "What type of bike do you ride the most?", enum: ["Road/racer", "Hybrid", "Mountain", "Cruiser", "Ladies", "Gents", "Fixie/Single Speed"] },
-        workStatus: { type: "string", title: "Work status", enum: ["Full Time", "Part Time", "Pension/Disability", "Unemployed", "Student", "Retired"] },
+        bikesHousehold: {
+          type: "number",
+          title: "How many bikes in your household?"
+        },
+        primaryBike: {
+          type: "string",
+          title: "What type of bike do you ride the most?",
+          enum: [
+            "Road/racer",
+            "Hybrid",
+            "Mountain",
+            "Cruiser",
+            "Ladies",
+            "Gents",
+            "Fixie/Single Speed"
+          ]
+        },
+        workStatus: {
+          type: "string",
+          title: "Work status",
+          enum: [
+            "Full Time",
+            "Part Time",
+            "Pension/Disability",
+            "Unemployed",
+            "Student",
+            "Retired"
+          ]
+        },
         reasons: { type: "string", title: "Reasons for volunteering" }
       }
     },
@@ -18,26 +44,27 @@ const b2bSchema = {
       bikesHousehold: {
         "ui:widget": "updown",
         "ui:placeholder": "Enter the number of bikes you own",
-        "ui:autofocus": true,
+        "ui:autofocus": true
       },
       primaryBike: {
         "ui:widget": "select",
-        "ui:placeholder": "Select a type of bike",
+        "ui:placeholder": "Select a type of bike"
       },
       workStatus: {
         "ui:widget": "select",
-        "ui:placeholder": "Select your employment status",
+        "ui:placeholder": "Select your employment status"
       },
       reasons: {
         "ui:widget": "textarea",
-        "ui:placeholder": "Some good starting points:\nWhat makes you want to to volunteer at Back2Bikes?\nHave you ever done any other volunteering before?\nHave you worked on bikes or something similar before?",
+        "ui:placeholder":
+          "Some good starting points:\nWhat makes you want to to volunteer at Back2Bikes?\nHave you ever done any other volunteering before?\nHave you worked on bikes or something similar before?",
         "ui:options": {
-          "rows": 12
+          rows: 12
         }
       }
     }
   }
-}
+};
 //
 // Peak Adventure schema overrides
 //
@@ -46,50 +73,87 @@ const paSchema = {
     schema: {
       type: "object",
       properties: {
-        sports: { 
+        sports: {
           type: "array",
           title: "Which of these sports are you active in?",
-          "uniqueItems": true,
+          uniqueItems: true,
           items: {
             type: "string",
-            enum: ["Kayaking", "Road cycling", "MTB", "Running", "Swimming", "Triathlon", "Multisport"],
-          },
+            enum: [
+              "Kayaking",
+              "Road cycling",
+              "MTB",
+              "Running",
+              "Swimming",
+              "Triathlon",
+              "Multisport"
+            ]
+          }
         },
-        reasons: { type: "string", title: "Tell us why you come to Peak Adventure sessions?" }
+        reasons: {
+          type: "string",
+          title: "Tell us why you come to Peak Adventure sessions?"
+        }
       }
     },
     uiSchema: {
       sports: {
-        "ui:widget": "checkboxes",
+        "ui:widget": "checkboxes"
       },
       reasons: {
         "ui:widget": "textarea",
         "ui:placeholder": "",
         "ui:options": {
-          "rows": 12
+          rows: 12
         }
       }
     }
   }
-}
+};
 
 const schemas = {
   b2b: b2bSchema,
-  pa: paSchema,
-}
+  pa: paSchema
+};
 
 const defaultSchema = [
   {
-    stepTitle: 'About You',
-    stepDescription: '',
+    stepTitle: "About You",
+    stepDescription: "",
     schema: {
       type: "object",
       title: "Lets get to know each other.",
       // required: ["bikesHousehold", "reasons"],
       properties: {
-        bikesHousehold: { type: "number", title: "How many bikes in your household?" },
-        primaryBike: { type: "string", title: "What type of bike do you ride the most?", enum: ["Road/racer", "Hybrid", "Mountain", "Cruiser", "Ladies", "Gents", "Fixie/Single Speed"] },
-        workStatus: { type: "string", title: "Work status", enum: ["Full Time", "Part Time", "Pension/Disability", "Unemployed", "Student", "Retired"] },
+        bikesHousehold: {
+          type: "number",
+          title: "How many bikes in your household?"
+        },
+        primaryBike: {
+          type: "string",
+          title: "What type of bike do you ride the most?",
+          enum: [
+            "Road/racer",
+            "Hybrid",
+            "Mountain",
+            "Cruiser",
+            "Ladies",
+            "Gents",
+            "Fixie/Single Speed"
+          ]
+        },
+        workStatus: {
+          type: "string",
+          title: "Work status",
+          enum: [
+            "Full Time",
+            "Part Time",
+            "Pension/Disability",
+            "Unemployed",
+            "Student",
+            "Retired"
+          ]
+        },
         reasons: { type: "string", title: "Reasons for volunteering" }
       }
     },
@@ -97,28 +161,29 @@ const defaultSchema = [
       bikesHousehold: {
         "ui:widget": "updown",
         "ui:placeholder": "Enter the number of bikes you own",
-        "ui:autofocus": true,
+        "ui:autofocus": true
       },
       primaryBike: {
         "ui:widget": "select",
-        "ui:placeholder": "Select a type of bike",
+        "ui:placeholder": "Select a type of bike"
       },
       workStatus: {
         "ui:widget": "select",
-        "ui:placeholder": "Select your employment status",
+        "ui:placeholder": "Select your employment status"
       },
       reasons: {
         "ui:widget": "textarea",
-        "ui:placeholder": "Some good starting points:\nWhat makes you want to to volunteer at Back2Bikes?\nHave you ever done any other volunteering before?\nHave you worked on bikes or something similar before?",
+        "ui:placeholder":
+          "Some good starting points:\nWhat makes you want to to volunteer at Back2Bikes?\nHave you ever done any other volunteering before?\nHave you worked on bikes or something similar before?",
         "ui:options": {
-          "rows": 12
+          rows: 12
         }
       }
     }
   },
   {
-    stepTitle: 'Contact',
-    stepDescription: '',
+    stepTitle: "Contact",
+    stepDescription: "",
     schema: {
       title: "Details",
       type: "object",
@@ -128,67 +193,69 @@ const defaultSchema = [
         email: { type: "string", format: "email", title: "Email" },
         addressStreet: { type: "string", title: "Street Address" },
         addressSuburb: { type: "string", title: "Suburb" },
-        addressState: { type: "string", title: "State", default: "VIC", enum: ["VIC", "NSW", "SA", "QLD", "NT", "WA", "TAS"] },
+        addressState: {
+          type: "string",
+          title: "State",
+          default: "VIC",
+          enum: ["VIC", "NSW", "SA", "QLD", "NT", "WA", "TAS"]
+        },
         addressPostcode: { type: "string", title: "Postcode" },
         phone: { type: "string", title: "Phone number" },
         mobile: { type: "string", title: "Mobile number" },
         pin: { type: "string", title: "PIN number" },
-        pinConfirm: { type: "string", title: "PIN number" },
+        pinConfirm: { type: "string", title: "PIN number" }
       }
     },
     uiSchema: {
       name: {
         "ui:placeholder": "Enter your name",
-        "ui:autofocus": true,
+        "ui:autofocus": true
       },
-      
-      email: {
-        "ui:placeholder": "Enter your email address",
 
+      email: {
+        "ui:placeholder": "Enter your email address"
       },
       addressStreet: {
-        "ui:placeholder": "Enter your street address e.g. 12 Luck Street",
-
+        "ui:placeholder": "Enter your street address e.g. 12 Luck Street"
       },
       addressSuburb: {
-        "ui:placeholder": "Enter your suburb",
-
+        "ui:placeholder": "Enter your suburb"
       },
       addressState: {
-        "ui:placeholder": "Enter your state",
+        "ui:placeholder": "Enter your state"
       },
       addressPostcode: {
-        "ui:placeholder": "Enter your postcode",
+        "ui:placeholder": "Enter your postcode"
       },
       phone: {
         "ui:placeholder": "Enter your phone number",
         "ui:options": {
-          inputType: 'tel'
+          inputType: "tel"
         }
       },
       mobile: {
         "ui:placeholder": "Enter your mobile number",
         "ui:options": {
-          inputType: 'tel'
+          inputType: "tel"
         }
       },
       pin: {
         "ui:placeholder": "Enter a PIN number for signing in and out.",
         "ui:options": {
-          inputType: 'tel'
+          inputType: "tel"
         }
       },
       pinConfirm: {
         "ui:placeholder": "Enter the same PIN number again to confirm.",
         "ui:options": {
-          inputType: 'tel'
+          inputType: "tel"
         }
-      },
+      }
     }
   },
   {
-    stepTitle: 'Emergency',
-    stepDescription: '',
+    stepTitle: "Emergency",
+    stepDescription: "",
     schema: {
       type: "object",
       title: "Who should we contact in an emergency?",
@@ -196,28 +263,32 @@ const defaultSchema = [
       properties: {
         emergencyContact: { type: "string", title: "Emergency Contact Name" },
         emergencyEmail: { type: "string", title: "Emergency Contact Email" },
-        emergencyPhone: { type: "string", title: "Emergency Contact Mobile number" },
-      },
+        emergencyPhone: {
+          type: "string",
+          title: "Emergency Contact Mobile number"
+        }
+      }
     },
     uiSchema: {
       emergencyContact: {
         "ui:placeholder": "Enter your emergency contact's full name",
-        "ui:autofocus": true,
+        "ui:autofocus": true
       },
       emergencyEmail: {
         "ui:placeholder": "Enter your emergency contact's email"
       },
       emergencyPhone: {
-        "ui:placeholder": "Enter your emergency contact's mobile or phone number",
+        "ui:placeholder":
+          "Enter your emergency contact's mobile or phone number",
         "ui:options": {
-          inputType: 'tel'
+          inputType: "tel"
         }
-      },
+      }
     }
   },
   {
-    stepTitle: 'Avatar',
-    stepDescription: '',
+    stepTitle: "Avatar",
+    stepDescription: "",
     schema: {
       type: "object",
       title: "Choose an avatar",
@@ -227,10 +298,76 @@ const defaultSchema = [
           type: "string",
           title: "Avatar",
           default: "default.jpg",
-          enum: ["default.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "test11.png",  "test14.png", "test16.png", "test17.png", "test18.png", "test19.png", "test20.png", "test21.png", "test24.png", "test25.png", "test26.png", "test28.png", "test29.png"],
-          enumNames: ["default", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "t11", "t12", "t13", "t14", "t15", "t16", "t17", "t18", "t19", "t20", "t21", "t22", "t23", "t24", "t25", "t26", "t27"]
-        },
-      },
+          enum: [
+            "default.jpg",
+            "1.jpg",
+            "2.jpg",
+            "3.jpg",
+            "4.jpg",
+            "5.jpg",
+            "6.jpg",
+            "7.jpg",
+            "8.jpg",
+            "9.jpg",
+            "10.jpg",
+            "11.jpg",
+            "12.jpg",
+            "13.jpg",
+            "14.jpg",
+            "15.jpg",
+            "16.jpg",
+            "test11.png",
+            "test14.png",
+            "test16.png",
+            "test17.png",
+            "test18.png",
+            "test19.png",
+            "test20.png",
+            "test21.png",
+            "test24.png",
+            "test25.png",
+            "test26.png",
+            "test28.png",
+            "test29.png"
+          ],
+          enumNames: [
+            "default",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "t11",
+            "t12",
+            "t13",
+            "t14",
+            "t15",
+            "t16",
+            "t17",
+            "t18",
+            "t19",
+            "t20",
+            "t21",
+            "t22",
+            "t23",
+            "t24",
+            "t25",
+            "t26",
+            "t27"
+          ]
+        }
+      }
     },
     uiSchema: {
       avatar: {
@@ -238,26 +375,24 @@ const defaultSchema = [
         "ui:options": {
           label: false
         }
-      },
+      }
     }
   }
-]
+];
 
 //
 // Copy in the schema overrides
 //
 if (Meteor.settings.public.recruit && schemas[Meteor.settings.public.recruit]) {
-  const newSchema = schemas[Meteor.settings.public.recruit]
-  const steps = ['about', 'contact', 'emergency', 'avatar']
-  debugger
+  const newSchema = schemas[Meteor.settings.public.recruit];
+  const steps = ["about", "contact", "emergency", "avatar"];
   steps.forEach((step, ix) => {
-    const stepName = `${step}Step`
+    const stepName = `${step}Step`;
     if (newSchema[stepName] && newSchema[stepName].schema) {
-      defaultSchema[ix].schema = newSchema[stepName].schema
-      defaultSchema[ix].uiSchema = newSchema[stepName].uiSchema
+      defaultSchema[ix].schema = newSchema[stepName].schema;
+      defaultSchema[ix].uiSchema = newSchema[stepName].uiSchema;
     }
-  })
-
+  });
 }
 
-export default defaultSchema
+export default defaultSchema;
