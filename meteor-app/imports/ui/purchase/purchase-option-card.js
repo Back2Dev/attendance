053c55e4189874acb1863347f53344b7305f580d
mikkel <mikkelking@hotmail.com>
2019-03-09 11:21:15 +1100

    Minor fixes to make it run

--- meteor-app/imports/ui/purchase/purchase-option-card.js
+++ meteor-app/imports/ui/purchase/purchase-option-card.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import PropTypes from 'prop-types';
 import { Card, List, Icon, Button } from 'semantic-ui-react'
 
-class PurchaseCard extends Component {
+const PurchaseCard = props => {
+  const cardColor = 'pink' // Mike added this
+  const cardClick = () => {}
 
-  render() {
-
-    return (
-      <Button style={{backgroundColor: "white", minWidth: "100%"}}>
-        <Card style={{backgroundColor: cardColor, color: "white", minWidth: "100%"}}>
-          <Card.Content style={{width: "100%", minHeight: "420px"}} onClick={onClick}>
-            <Card.Header style={{color: "white", paddingTop: "15px", fontSize: "1.5em"}} >
-              {`Workshop`}
-            </Card.Header>
-          </Card.Content>
-        </Card >
-      </Button>
-    )
-  }
+  return (
+    <Button style={{ backgroundColor: 'white', minWidth: '100%' }}>
+      <Card style={{ backgroundColor: cardColor, color: 'white', minWidth: '100%' }}>
+        <Card.Content style={{ width: '100%', minHeight: '420px' }} onClick={cardClick}>
+          <Card.Header style={{ color: 'white', paddingTop: '15px', fontSize: '1.5em' }}>{`Workshop`}</Card.Header>
+        </Card.Content>
+      </Card>
+    </Button>
+  )
 }
 
 export default PurchaseCard
