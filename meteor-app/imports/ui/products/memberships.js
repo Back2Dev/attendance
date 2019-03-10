8dc591da854fb4a19fb6a06971d6d4d9e8388e04
guymcmanusgithub <guy@guymcmanus.com>
2019-03-10 22:45:58 +1100

    cleaned up code in memberships

--- meteor-app/imports/ui/products/memberships.js
+++ meteor-app/imports/ui/products/memberships.js
@@ -71,21 +71,3 @@ export default class Memberships extends Component {
 
 }
 
-//     state = {
-//       selected: 'radio-1'
-//     };
-//     componentDidUpdate () {
-//       alert(document.querySelector('input[name=myRadio]:checked').value);
-//     }
-//     render () {
-//       return (
-//         <div>
-//           <input type='radio' id='radio-1' name='myRadio' value='radio-1'
-//             checked={this.state.selected === 'radio-1'} onChange={(e) => this.setState({ selected: e.target.value })} />
-//           <br />
-//           <input type='radio' id='radio-2' name='myRadio' value='radio-2' 
-//             checked={this.state.selected === 'radio-2'} onChange={(e) => this.setState({ selected: e.target.value })} />
-//         </div>
-//       );
-//     }
-//   };
