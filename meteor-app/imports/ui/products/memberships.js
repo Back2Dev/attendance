83248b9a5d339d48df956c0ae253936b68a60b49
mikkel <mikkelking@hotmail.com>
2019-03-10 21:16:27 +1100

    Make labels clickable, convert to functional component (using hooks)

--- meteor-app/imports/ui/products/memberships.js
+++ meteor-app/imports/ui/products/memberships.js
@@ -1,91 +1,77 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Checkout from '../purchase/checkout'
 import './products.css'
 
-export default class Memberships extends Component {
-    state = {
-      selected: '3-month',
-    }
-    componentDidUpdate () {
-        // alert(document.querySelector('input[name=membershipDuration]:checked').value);
-    }
+const Memberships = props => {
+  const [selected, select] = useState('12-month')
 
-    // handleFrequencyChange = (e) => {
-    //     this.setState({
-    //         selectedOption: e.target.value
-    //     })
-    // }
+  return (
+    <div className="product-content membership-container">
+      <h4>Membership options</h4>
 
-    render() {
+      <p>Memberships allow you to access and use the workhop facilities at Back2bikes.</p>
+      <form>
+        <div>
+          <label>
+            <input
+              type="radio"
+              id="3-month"
+              name="membershipDuration"
+              value="3-month"
+              checked={this.state.selected === '3-month'}
+              onChange={e => select(e.target.value)}
+            />
+            3 Month Membership - $40
+          </label>
+        </div>
 
-          return (
-              <div className="product-content membership-container">
-              
-                <h4>Membership Types</h4>
+        <div>
+          <label>
+            <input
+              type="radio"
+              id="6-month"
+              name="membershipDuration"
+              value="6-month"
+              checked={this.state.selected === '6-month'}
+              onChange={e => select(e.target.value)}
+            />
+            6 Month Membership - $60
+          </label>
+        </div>
 
-                <p>Memberships allow you to access and use the facilities at Back 2 Bikes.</p>
-                <form>
-                    <div>
-                    <input type='radio' id='3-month' name='membershipDuration' value='3-month'
-                        checked={this.state.selected === '3-month'} onChange={(e) => this.setState({ selected: e.target.value })} />
-                    <label htmlFor="frequency">3 Month Membership - $40</label>
-                    </div>
+        <div>
+          <label>
+            <input
+              type="radio"
+              id="12-month"
+              name="membershipDuration"
+              value="12-month"
+              checked={this.state.selected === '12-month'}
+              onChange={e => select(e.target.value)}
+            />
+            12 Month Membership - $80
+          </label>
+        </div>
+      </form>
 
-                    <div>
-                    <input type='radio' id='6-month' name='membershipDuration' value='6-month' 
-                        checked={this.state.selected === '6-month'} onChange={(e) => this.setState({ selected: e.target.value })} />
-                    <label htmlFor="frequency">6 Month Membership - $60</label>
-                    </div>
-
-                    <div>
-                    <input type='radio' id='12-month' name='membershipDuration' value='12-month' 
-                        checked={this.state.selected === '12-month'} onChange={(e) => this.setState({ selected: e.target.value })} />
-                    <label htmlFor="frequency">12 Month Membership - $80</label>
-                    </div>
-
-                </form>
-        
-              
-                {this.state.selected === "3-month" &&
-                    <>
-                        <p>current total is $40</p>
-                        <Checkout amount="4000"/>
-                    </>
-                }
-                {this.state.selected === "6-month" &&
-                    <>
-                        <p>current total is $60</p>
-                        <Checkout amount="6000"/>
-                    </>
-                }
-                {this.state.selected === "12-month" &&
-                    <>
-                        <p>current total is $80</p>
-                        <Checkout amount="8000"/>
-                    </>
-                }
-             
-              </div>
-          )
-        }
-
+      {this.state.selected === '3-month' && (
+        <>
+          <p>current total is $40</p>
+          <Checkout amount="4000" />
+        </>
+      )}
+      {this.state.selected === '6-month' && (
+        <>
+          <p>current total is $60</p>
+          <Checkout amount="6000" />
+        </>
+      )}
+      {this.state.selected === '12-month' && (
+        <>
+          <p>current total is $80</p>
+          <Checkout amount="8000" />
+        </>
+      )}
+    </div>
+  )
 }
-
-//     state = {
-//       selected: 'radio-1'
-//     };
-//     componentDidUpdate () {
-//       alert(document.querySelector('input[name=myRadio]:checked').value);
-//     }
-//     render () {
-//       return (
-//         <div>
-//           <input type='radio' id='radio-1' name='myRadio' value='radio-1'
-//             checked={this.state.selected === 'radio-1'} onChange={(e) => this.setState({ selected: e.target.value })} />
-//           <br />
-//           <input type='radio' id='radio-2' name='myRadio' value='radio-2' 
-//             checked={this.state.selected === 'radio-2'} onChange={(e) => this.setState({ selected: e.target.value })} />
-//         </div>
-//       );
-//     }
-//   };
