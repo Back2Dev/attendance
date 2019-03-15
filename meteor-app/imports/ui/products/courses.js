a5cfbb1f5909917aaabe0ab77a248fe3bc83db70
guymcmanusgithub <guy@guymcmanus.com>
2019-03-15 12:44:36 +1100

    added courses stories

--- meteor-app/imports/ui/products/courses.js
+++ meteor-app/imports/ui/products/courses.js
@@ -6,34 +6,36 @@ const Courses = props => {
 
   const [courses, setCourses] = useState([])
 
-    // if(rendered) {
+  const { img = '/images/gym.jpg', title = "", description = "", startDate="", endDate=""} = props
+
+    if(courses) {
       return (
         <div className="course-container product-content">
-          {courses.map( 
-            course => {return (
+          {/* {courses.map(  */}
+            {/* course => {return ( */}
               <div class="course-card">
                 <div class="course-card-details">
-                  <img src={course.img}/>
-                  <h1>{course.title}</h1>
-                  <span>Starting on: {course.startDate}, Ending on: {course.endDate}</span>
-                  <p>{course.description}</p>
+                  <img src={img}/>
+                  <h1>{title}</h1>
+                  <span>Starting on: {startDate}, Ending on: {endDate}</span>
+                  <p>{description}</p>
                   <Checkout/> 
                   {/* i need to pass the price as props here */}
                   <hr/>
                 </div>
               </div>
-            )}
-          )}
+            {/* )} */}
+          {/* )} */}
         </div>         
       )}
-    //   else {
-    //     return (
-    //       <div className="course-container product-content">
-    //         <h3>There are currently no courses</h3>
-    //       </div>
-    //     )
-    //   }
-    // }      
+      else {
+        return (
+          <div className="course-container product-content">
+            <h3>There are currently no courses</h3>
+          </div>
+        )
+      }
+    }      
 
 
 export default Courses
