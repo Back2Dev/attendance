2e1f901bd1b35c18d51e1c2bfda1fbed995cc77b
mikkel <mikkelking@hotmail.com>
2019-03-17 09:02:17 +1100

    CHecking useState err

--- meteor-app/imports/ui/products/courses.js
+++ meteor-app/imports/ui/products/courses.js
@@ -3,42 +3,39 @@ import Checkout from '../purchase/checkout'
 import './courses.css'
 
 const Courses = props => {
-
   const [courses, setCourses] = useState([])
 
-  const { img = '/images/gym.jpg', title = "", description = "", startDate="", endDate=""} = props
+  const { img = '/images/gym.jpg', title = '', description = '', startDate = '', endDate = '' } = props
 
-    if(courses) {
-      return (
-        <div className="course-container product-content">
-          {/* {courses.map(  */}
-            {/* course => {return ( */}
-              <div class="course-card">
-                <div class="course-card-details">
-                  <img src={img}/>
-                  <h1>{title}</h1>
-                  <span>Starting on: {startDate}, Ending on: {endDate}</span>
-                  <p>{description}</p>
-                  <Checkout/> 
-                  {/* i need to pass the price as props here */}
-                  <hr/>
-                </div>
-              </div>
-            {/* )} */}
-          {/* )} */}
-        </div>         
-      )}
-      else {
-        return (
-          <div className="course-container product-content">
-            <h3>There are currently no courses</h3>
+  if (courses) {
+    return (
+      <div className="course-container product-content">
+        {/* {courses.map(  */}
+        {/* course => {return ( */}
+        <div class="course-card">
+          <div class="course-card-details">
+            <img src={img} />
+            <h1>{title}</h1>
+            <span>
+              Starting on: {startDate}, Ending on: {endDate}
+            </span>
+            <p>{description}</p>
+            <Checkout />
+            {/* i need to pass the price as props here */}
+            <hr />
           </div>
-        )
-      }
-    }      
-
+        </div>
+        {/* )} */}
+        {/* )} */}
+      </div>
+    )
+  } else {
+    return (
+      <div className="course-container product-content">
+        <h3>There are currently no courses</h3>
+      </div>
+    )
+  }
+}
 
 export default Courses
-
-
-
