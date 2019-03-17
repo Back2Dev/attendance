46f0ffdaa4097b43e466cfcb88e236fca6ba65c1
Mike King <mikkelking@hotmail.com>
2019-03-17 13:43:16 +1100

    added schema for cart

--- meteor-app/imports/api/products/schema.js
+++ meteor-app/imports/api/products/schema.js
@@ -5,86 +5,108 @@ import CONSTANTS from '../constants'
 
 const Products = new Mongo.Collection('products')
 export const ProductTypes = new Mongo.Collection('productTypes')
+export const Carts = new Mongo.Collection('carts')
 
 export const ProductTypesSchema = new SimpleSchema({
   name: {
     type: String,
-    label: 'Product Type Name'
+    label: 'Product Type Name',
   },
   description: {
     type: String,
-    label: 'Product Type Description'
+    label: 'Product Type Description',
   },
   type: {
     type: SimpleSchema.Integer,
     label: 'Product Type Code',
-    allowedValues: Object.keys(CONSTANTS.PRODUCT_TYPES_READABLE).map(key => parseInt(key, 10))
+    allowedValues: Object.keys(CONSTANTS.PRODUCT_TYPES_READABLE).map(key => parseInt(key, 10)),
   },
   color: {
     type: String,
-    defaultValue: 'green'
+    defaultValue: 'green',
   },
   image: {
     type: String,
     label: 'Product Type Image',
-    optional: true
+    optional: true,
   },
   icon: {
     type: String,
     label: 'Product Type Icon',
-    optional: true
-  }
+    optional: true,
+  },
 })
 
 export const ProductsSchema = new SimpleSchema({
   name: {
     type: String,
-    label: 'Product Name'
+    label: 'Product Name',
   },
   description: {
     type: String,
-    label: 'Product Description'
+    label: 'Product Description',
   },
   type: {
     type: SimpleSchema.Integer,
     label: 'Product Type: pass, membership, course',
-    allowedValues: Object.keys(CONSTANTS.PRODUCT_TYPES_READABLE).map(key => parseInt(key, 10))
+    allowedValues: Object.keys(CONSTANTS.PRODUCT_TYPES_READABLE).map(key => parseInt(key, 10)),
   },
   duration: {
     type: SimpleSchema.Integer,
     label: 'Product Duration in weeks (if applicable)',
-    optional: true
+    optional: true,
   },
   price: {
     type: SimpleSchema.Integer,
     label: 'Product Price in cents.  If free please leave blank',
     optional: true,
-    defaultValue: 0
+    defaultValue: 0,
   },
   image: {
     type: String,
     label: 'Product Image',
-    optional: true
+    optional: true,
   },
   active: {
     type: Boolean,
-    label: 'Is the product available'
+    label: 'Is the product available',
   },
   startDate: {
     type: Date,
     label: 'Product start date',
-    optional: true
+    optional: true,
   },
   endDate: {
     type: Date,
     label: 'Product end date',
-    optional: true
+    optional: true,
   },
   createdAt,
-  updatedAt
+  updatedAt,
+})
+
+export const CartsSchema = new SimpleSchema({
+  price: {
+    type: SimpleSchema.Integer,
+    label: 'Total Price in cents',
+    defaultValue: 0,
+  },
+  totalqty: {
+    type: SimpleSchema.Integer,
+    label: 'Total quantity',
+    defaultValue: 0,
+  },
+  products: {
+    type: Array,
+    optional: true,
+  },
+  'products.$': ProductsSchema,
+  createdAt,
+  updatedAt,
 })
 
 Products.attachSchema(ProductsSchema)
 ProductTypes.attachSchema(ProductTypesSchema)
+Carts.attachSchema(CartsSchema)
 
 export default Products
