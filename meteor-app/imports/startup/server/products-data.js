05e1cf9a8c7cc260c78da30320107535f3ebb318
mikkel <mikkelking@hotmail.com>
2019-03-18 07:34:49 +1100

    Added images

--- meteor-app/imports/startup/server/products-data.js
+++ meteor-app/imports/startup/server/products-data.js
@@ -8,21 +8,24 @@ const productTypes = [
     name: 'Maintenance Courses',
     description: 'We offer maintenance courses, you can learn to repair your bike yourself',
     color: 'orange',
-    icon: 'wrench'
+    icon: 'wrench',
+    image: '/images/maintenance.jpg'
   },
   {
     type: CONSTANTS.PRODUCT_TYPES.PASS,
     name: 'Workshop time',
     description: 'Use our fully equipped workshop to fix your bike. Get advice and help about how to do it',
     color: 'green',
-    icon: 'cog'
+    icon: 'cog',
+    image: '/images/workshop.jpg'
   },
   {
     type: CONSTANTS.PRODUCT_TYPES.MEMBERSHIP,
     name: 'Memberships',
     description: 'Join as a member, get access to discounts and workshop time',
     color: 'blue',
-    icon: 'cogs'
+    icon: 'cogs',
+    image: '/images/membership.jpg'
   }
 ]
 const b2bProducts = [
@@ -31,35 +34,88 @@ const b2bProducts = [
     price: 36000,
     description: '6 week bicycle maintenance course',
     type: CONSTANTS.PRODUCT_TYPES.COURSE,
-    active: true
+    active: true,
+    image: '/images/maintenance.jpg'
   },
   {
     name: 'Punctures session',
     price: 8000,
     description: 'Week 1: Punctures',
     type: CONSTANTS.PRODUCT_TYPES.COURSE,
-    active: true
+    active: true,
+    image: '/images/puncture.jpg'
   },
   {
-    name: 'Gears session',
-    price: 15000,
-    description: 'Week 3&4: Gears',
+    name: 'Brakes session',
+    price: 8000,
+    description: 'Week 2: Brakes',
     type: CONSTANTS.PRODUCT_TYPES.COURSE,
-    active: true
+    active: true,
+    image: '/images/brakes.jpg'
+  },
+  {
+    name: 'Gears sessions (2 weeks)',
+    price: 15000,
+    description: 'Weeks 3 & 4: Gears',
+    type: CONSTANTS.PRODUCT_TYPES.COURSE,
+    active: true,
+    image: '/images/gears.jpg'
+  },
+  {
+    name: 'Wheels session',
+    price: 8000,
+    description: 'Week 5: Wheel bearings',
+    type: CONSTANTS.PRODUCT_TYPES.COURSE,
+    active: true,
+    image: '/images/wheels.jpg'
+  },
+  {
+    name: 'Forks session',
+    price: 8000,
+    description: 'Week 6: Forks and Bottom brackets',
+    type: CONSTANTS.PRODUCT_TYPES.COURSE,
+    active: true,
+    image: '/images/forks.jpg'
   },
   {
     name: 'Workshop session',
     price: 2000,
     description: 'Evening workshop access',
     type: CONSTANTS.PRODUCT_TYPES.PASS,
-    active: true
+    active: true,
+    image: '/images/workshop.jpg'
   },
   {
-    name: '6 month membership',
+    name: "Women's workshop session",
+    price: 2000,
+    description: "Evening workshop access (women's nights)",
+    type: CONSTANTS.PRODUCT_TYPES.PASS,
+    active: true,
+    image: '/images/womens.jpg'
+  },
+  {
+    name: '1 month membership',
+    price: 4000,
+    description: 'Evening workshop access for 6 months',
+    type: CONSTANTS.PRODUCT_TYPES.MEMBERSHIP,
+    active: true,
+    image: '/images/bicycle-membership.jpg'
+  },
+  {
+    name: '3 month membership',
     price: 20000,
     description: 'Evening workshop access for 6 months',
     type: CONSTANTS.PRODUCT_TYPES.MEMBERSHIP,
-    active: true
+    active: true,
+    image: '/images/membership.jpg'
+  },
+  {
+    name: '12 month membership',
+    price: 40000,
+    description: 'Evening workshop access for 6 months',
+    type: CONSTANTS.PRODUCT_TYPES.MEMBERSHIP,
+    active: true,
+    image: '/images/membership.jpg'
   }
 ]
 Meteor.methods({
