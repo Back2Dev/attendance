963199a97c779c57270c53ce720ffecb27d39d74
Mike King <mikkelking@hotmail.com>
2019-03-17 14:50:59 +1100

    Hacked Courses component to prevent snapshot failure

--- meteor-app/imports/ui/products/courses.js
+++ meteor-app/imports/ui/products/courses.js
@@ -3,17 +3,17 @@ import Checkout from '../purchase/checkout'
 import './courses.css'
 
 const Courses = props => {
-  const [courses, setCourses] = useState([])
+  // const [courses, setCourses] = useState(0)
 
   const { img = '/images/gym.jpg', title = '', description = '', startDate = '', endDate = '' } = props
 
-  if (courses) {
+  if (img) {
     return (
       <div className="course-container product-content">
         {/* {courses.map(  */}
         {/* course => {return ( */}
-        <div class="course-card">
-          <div class="course-card-details">
+        <div className="course-card">
+          <div className="course-card-details">
             <img src={img} />
             <h1>{title}</h1>
             <span>
@@ -29,13 +29,12 @@ const Courses = props => {
         {/* )} */}
       </div>
     )
-  } else {
-    return (
-      <div className="course-container product-content">
-        <h3>There are currently no courses</h3>
-      </div>
-    )
   }
+  return (
+    <div className="course-container product-content">
+      <h3>There are currently no courses</h3>
+    </div>
+  )
 }
 
 export default Courses
--- meteor-app/imports/ui/products/courses.stories.js
+++ meteor-app/imports/ui/products/courses.stories.js
@@ -4,11 +4,11 @@ import { storiesOf } from '@storybook/react'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react'
 
 const courseInfo = {
-    img:'/images/gym.jpg',
-    title: 'awesome course similar to awesome sauce, but a course',
-    startDate: '2019-02-18T16:00:00Z',
-    endDate: '2019-03-18T16:00:00Z',
-    description: '10 visits, usable at any of our evening sessions '
+  img: '/images/gym.jpg',
+  title: 'awesome course similar to awesome sauce, but a course',
+  startDate: '2019-02-18T16:00:00Z',
+  endDate: '2019-03-18T16:00:00Z',
+  description: '10 visits, usable at any of our evening sessions ',
 }
 
 import Courses from './courses'
@@ -19,9 +19,8 @@ storiesOf('Courses', module)
   .add('Courses Card', () => {
     const story = (
       <div>
-        <Courses {...courseInfo} /> 
-      </div>   
+        <Courses {...courseInfo} />
+      </div>
     )
     return story
   })
-
