cc283db449fcdc203f83bc2ce2ed6cd0a64b5e6c
Mikkel <michael.king@dbresults.com.au>
2019-03-18 11:59:47 +1100

    Fixed service validation rule

--- meteor-app/imports/ui/config/bike-assessment-schemas.js
+++ meteor-app/imports/ui/config/bike-assessment-schemas.js
@@ -1,19 +1,20 @@
-export default [{
+export default [
+  {
     stepTitle: 'Base Service',
     stepDescription: '',
     schema: {
-      type: "object",
-      title: "",
+      type: 'object',
+      title: '',
       properties: {
         package: {
-          type: "string",
-          enum: ["Minor Service", "Major Service"]
+          type: 'string',
+          enum: ['Minor Service', 'Major Service']
         }
       }
     },
     uiSchema: {
       package: {
-        "ui:widget": "hidden"
+        'ui:widget': 'hidden'
       }
     }
   },
@@ -21,75 +22,75 @@ export default [{
     stepTitle: 'Services',
     stepDescription: '',
     schema: {
-      title: "Select Services",
-      type: "object",
+      title: 'Select Services',
+      type: 'object',
       required: ['assessor', 'bikeMake', 'bikeColor', 'approxBikeValue'],
       properties: {
         assessor: {
-          type: "string",
-          title: "Assessor",
-          enum: ["Mark", "Mike", ""],
-          default: ""
+          type: 'string',
+          title: 'Assessor',
+          enum: ['Mark', 'Mike', ''],
+          default: ''
         },
         bikeMake: {
-          type: "string",
-          title: "Bike Make"
+          type: 'string',
+          title: 'Bike Make'
         },
         bikeModel: {
-          type: "string",
-          title: "Bike Model"
+          type: 'string',
+          title: 'Bike Model'
         },
         bikeColor: {
-          type: "string",
-          title: "Bike Color"
+          type: 'string',
+          title: 'Bike Color'
         },
         approxBikeValue: {
-          type: "integer",
-          title: "Approx. Bike Value"
+          type: 'integer',
+          title: 'Approx. Bike Value'
         },
         services: {
-          type: "array",
-          title: "Services",
+          type: 'array',
+          title: 'Services',
           items: {
-            type: "string",
-            enum: [" Service 1", " Service 2", " Service 3"]
+            type: 'string',
+            enum: [' Service 1', ' Service 2', ' Service 3']
           },
           uniqueItems: true
         },
         serviceCost: {
-          type: "integer",
-          title: "Total Service Cost"
+          type: 'integer',
+          title: 'Total Service Cost'
         }
       }
     },
     uiSchema: {
       assessor: {
-        "ui:placeholder": "Who is doing the assessment?",
-        "ui:autoFocus": true,
+        'ui:placeholder': 'Who is doing the assessment?',
+        'ui:autoFocus': true
       },
 
       bikeMake: {
-        "ui:placeholder": "Bike make..",
-        "ui:autoFocus": true,
+        'ui:placeholder': 'Bike make..',
+        'ui:autoFocus': true
       },
       bikeModel: {
-        "ui:placeholder": "Bike model..",
-        "ui:autoFocus": true,
+        'ui:placeholder': 'Bike model..',
+        'ui:autoFocus': true
       },
       bikeColor: {
-        "ui:placeholder": "Bike color..",
-        "ui:autoFocus": true,
+        'ui:placeholder': 'Bike color..',
+        'ui:autoFocus': true
       },
       approxBikeValue: {
-        "ui:placeholder": "$Approx. price",
-        "ui:widget": "updown",
-        classNames: "currency" 
+        'ui:placeholder': '$Approx. price',
+        'ui:widget': 'updown',
+        classNames: 'currency'
       },
       services: {
-        "ui:widget": "checkboxes"
+        'ui:widget': 'checkboxes'
       },
       serviceCost: {
-        "ui:widget": "hidden"
+        'ui:widget': 'hidden'
       }
     }
   },
@@ -97,67 +98,66 @@ export default [{
     stepTitle: 'Parts',
     stepDescription: '',
     schema: {
-      type: "object",
-      required: ['additionalFee','discount'],
-      title: "Select Parts",
+      type: 'object',
+      required: ['additionalFee', 'discount'],
+      title: 'Select Parts',
       properties: {
         parts: {
-          type: "array",
-          title: "Parts",
+          type: 'array',
+          title: 'Parts',
           items: {
-            type: "string",
-            enum: [" Bike Part 1", " Bike Part 2", " Bike Part 3"],
+            type: 'string',
+            enum: [' Bike Part 1', ' Bike Part 2', ' Bike Part 3']
           },
           uniqueItems: true
         },
         partsCost: {
-          type: "integer",
-          title: "Total Parts Cost"
+          type: 'integer',
+          title: 'Total Parts Cost'
         },
         comments: {
-          type: "string",
-          title: "Comments"
+          type: 'string',
+          title: 'Comments'
         },
         additionalFee: {
-          type: "integer",
-          title: "Enter additional cost if required (list reason in comments)"
+          type: 'integer',
+          title: 'Enter additional cost if required (list reason in comments)'
         },
         discount: {
-          type: "integer",
-          title: "Discount"
+          type: 'integer',
+          title: 'Discount'
         },
         discountReason: {
-          type: "string",
-          title: "Reason for discount"
-        },
-        
-      },
+          type: 'string',
+          title: 'Reason for discount'
+        }
+      }
     },
     uiSchema: {
       parts: {
-        "ui:widget": "checkboxes"
+        'ui:widget': 'checkboxes'
       },
       partsCost: {
-        "ui:widget": "hidden"
+        'ui:widget': 'hidden'
       },
       comments: {
-        "ui:widget": "textarea",
-        "ui:placeholder": "Please enter any comments if there are any additional fees or services needed..",
-        "ui:options": {
-          "rows": 10
+        'ui:widget': 'textarea',
+        'ui:placeholder':
+          'Please enter any comments if there are any additional fees or services needed..',
+        'ui:options': {
+          rows: 10
         }
       },
       additionalFee: {
-        classNames: "currency",
-        "ui:placeholder": "$Additional Cost",
-        "ui:widget": "updown"
+        classNames: 'currency',
+        'ui:placeholder': '$Additional Cost',
+        'ui:widget': 'updown'
       },
       discount: {
-        classNames: "currency",
-        "ui:placeholder": "$Discount",
-        "ui:widget": "updown"
-      },
-
+        classNames: 'currency',
+        'ui:placeholder': '$Discount',
+        'ui:widget': 'updown'
+      }
     }
   },
 
@@ -165,74 +165,74 @@ export default [{
     stepTitle: 'Review',
     stepDescription: '',
     schema: {
-      type: "object",
-      title: "",
-      properties: {},
+      type: 'object',
+      title: '',
+      properties: {}
     },
-    uiSchema: {},
+    uiSchema: {}
   },
   {
     stepTitle: 'Customer Details',
     stepDescription: '',
     schema: {
-      type: "object",
-      title: "Enter Customer Details",
+      type: 'object',
+      title: 'Enter Customer Details',
       properties: {
         isRefurbish: {
-          type: "boolean",
-          title: "Is this bike being refurbished?",
+          type: 'boolean',
+          title: 'Is this bike being refurbished?',
           default: false
         },
         pickUpDate: {
-          type: "string",
-          title: "Pick-up Date",
-          format: "date",
-          default: "2018-10-10"
-        },
+          type: 'string',
+          title: 'Pick-up Date',
+          format: 'date',
+          default: '2018-10-10'
+        }
       },
       dependencies: {
         isRefurbish: {
-          "oneOf": [
+          oneOf: [
             {
               properties: {
-                isRefurbish: { enum: [true, ""] }
-              },
-          },
+                isRefurbish: { enum: [true, ''] }
+              }
+            },
             {
               properties: {
                 isRefurbish: { enum: [false] },
                 name: {
-                  type: "string",
-                  title: "Name"
+                  type: 'string',
+                  title: 'Name'
                 },
                 phone: {
-                  type: "string",
-                  title: "Phone Number"
+                  type: 'string',
+                  title: 'Phone Number'
                 },
                 email: {
-                  type: "string",
-                  title: "Email"
+                  type: 'string',
+                  title: 'Email'
                 },
                 replacementBike: {
-                  type: "boolean",
-                  title: "Replacement bike required?",
+                  type: 'boolean',
+                  title: 'Replacement bike required?',
                   default: false
                 },
                 sentimentalValue: {
-                  type: "boolean",
-                  title: "Does bike have sentimental value?",
+                  type: 'boolean',
+                  title: 'Does bike have sentimental value?',
                   default: false
                 },
                 requestUrgent: {
-                  type: "boolean",
-                  title: "Is this request urgent?",
+                  type: 'boolean',
+                  title: 'Is this request urgent?',
                   default: false
-                },
-        
+                }
               },
-              "oneOf": [
-                {required: ["name", "phone"]},
-                {required: ["name", "email"]}
+              anyOf: [
+                { required: ['name', 'phone'] },
+                { required: ['name', 'phone', 'email'] },
+                { required: ['name', 'email'] }
               ]
             }
           ]
@@ -244,11 +244,11 @@ export default [{
       name: {},
       phone: {},
       email: {
-        "ui:widget": "email"
+        'ui:widget': 'email'
       },
       replacementBike: {},
       sentimentalValue: {},
-      requestUrgent: {},
-    },
+      requestUrgent: {}
+    }
   }
 ]
