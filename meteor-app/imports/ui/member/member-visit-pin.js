d407f79d66af4f190ed2ea965c6027b4cb3d9790
mikkel <mikkelking@hotmail.com>
2019-03-30 09:25:28 +1100

    Improved pin handling, auto-tab, set pin, loading

--- meteor-app/imports/ui/member/member-visit-pin.js
+++ meteor-app/imports/ui/member/member-visit-pin.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import { Form, Label, Button, Header, Input, Message } from 'semantic-ui-react'
 import '/imports/ui/member/member-visit-pin.css'
 
 class MemberVisitPin extends React.Component {
-
   constructor(props) {
     super(props)
 
     this.state = {
       pin: '',
-      pinMatchError: false,
+      pinMatchError: false
     }
 
-    this.pinInput = React.createRef();
+    this.pinInput = React.createRef()
   }
 
   onPinInput = (e, props) => {
@@ -29,53 +28,43 @@ class MemberVisitPin extends React.Component {
   componentDidUpdate() {
     const { pin } = this.state
     if (pin.length >= 4) {
-      this.props.onSubmitPin(pin) 
+      this.props.onSubmitPin(pin)
     }
   }
 
   render() {
     const inputSettings = {
       maxLength: '4',
-      type: "tel",
-      pattern: "/[0-9]/",
-      inputMode: "numeric",
-      style: { 
-        width: '80%', 
-        margin: '10px 0', 
-        fontSize: '20px', 
-        textAlign: 'center' 
-      },
+      type: 'tel',
+      pattern: '/[0-9]/',
+      inputMode: 'numeric',
+      style: {
+        width: '80%',
+        margin: '10px 0',
+        fontSize: '20px',
+        textAlign: 'center'
+      }
     }
     return (
-      <div className='member-visit-pin'>
-        {
-          this.props.setPinSuccess &&
-          <Message attached='top' success
-            header='Success'
-            content='Sign in with your new PIN now!'
-          >
-          </Message>
-        }
+      <div className="member-visit-pin">
+        {this.props.setPinSuccess && (
+          <Message attached="top" success header="Success" content="Sign in with your new PIN now!" />
+        )}
         <Form.Field>
-          {
-            !this.props.setPinSuccess &&
-            <label htmlFor='pinInput'>
-              Enter your PIN:
-          </label>
-          }
+          {!this.props.setPinSuccess && <label htmlFor="pinInput">Enter your PIN:</label>}
           <Input
             error={this.state.pin.length >= 4 && !this.props.validPin}
             ref={this.pinInput}
-            name='pinInput'
+            name="pinInput"
             id="pin"
-            {...inputSettings}  
+            focus
+            {...inputSettings}
             onChange={this.onPinInput}
           />
           <Button as={Label} pointing onClick={this.props.toggleForgotPinForm}>
             Forgotten your PIN? Click here
           </Button>
         </Form.Field>
-
       </div>
     )
   }
@@ -85,7 +74,7 @@ MemberVisitPin.propTypes = {
   forgotPin: PropTypes.func.isRequired,
   validPin: PropTypes.bool.isRequired,
   onSubmitPin: PropTypes.func.isRequired,
-  toggleForgotPinForm: PropTypes.func.isRequired,
-};
+  toggleForgotPinForm: PropTypes.func.isRequired
+}
 
 export default MemberVisitPin
