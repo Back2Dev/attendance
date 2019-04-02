313471ff185d83e419d735ea27ec95b4f2e2c647
Mike King <mikkelking@hotmail.com>
2019-04-02 16:53:57 +1100

    Refactoring

--- meteor-app/imports/ui/punch-card/Punch-card.stories.js
+++ meteor-app/imports/ui/punch-card/Punch-card.stories.js
@@ -4,6 +4,9 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 import { Card } from 'semantic-ui-react'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react'
 import PunchCard from './punch-card'
 
-storiesOf('Punch-card', module).add('Punchcard', () => <PunchCard />)
+storiesOf('Punch-card', module)
+  .addDecorator(withKnobs)
+  .add('Punchcard', () => <PunchCard visits={number('Visits', 10)} numVisits={number('Used', 3)} />)
--- meteor-app/imports/ui/punch-card/digits.js
+++ meteor-app/imports/ui/punch-card/digits.js
@@ -1,23 +1,20 @@
-import React from "react";
-import { Card } from "semantic-ui-react";
-
-import One from "./../one.png";
+import React from 'react'
+import { Card } from 'semantic-ui-react'
 
 const digitStyle = {
-  width: "50px",
-  height: "50px"
-};
-
-class DigitsArr extends React.Component {
-  render() {
-    var arr = [One];
-    var elements = [];
-    for (var i = 0; i < 6; i++) {
-      // push the component to elements!
-      elements.push(<Card raised image={arr[0]} />);
-    }
-    return <Card.Group itemsPerRow={5}>{elements}</Card.Group>;
-  }
+  width: '50px',
+  height: '50px',
+  background: 'red',
+  borderRadius: '50%'
 }
 
-export default DigitsArr;
+const DigitsArr = props => {
+  var elements = []
+  for (var i = 1; i <= 18; i++) {
+    // push the component to elements!
+    elements.push(<div style={digitStyle}>{i}</div>)
+  }
+  return <span>{elements}</span>
+}
+
+export default DigitsArr
