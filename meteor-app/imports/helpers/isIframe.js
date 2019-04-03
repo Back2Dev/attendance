eced96b212d34d6aedf901fabc38b0baf8778eae
mikkel <mikkelking@hotmail.com>
2019-04-03 14:18:12 +1100

    snapshots

--- meteor-app/imports/helpers/isIframe.js
+++ meteor-app/imports/helpers/isIframe.js
@@ -1,7 +1,7 @@
 function isIframe(item) {
   if (window.parent.location === window.location) return false
   const reg = new RegExp(item + '$')
-  if (window.location.match(reg)) return true
+  if (window.location.href.match(reg)) return true
 }
 
 export default isIframe
