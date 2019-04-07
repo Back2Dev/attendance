4fd490085d94171e8429cab1c1f5ab762ae3cfb0
Saoud Nazir <mirzasaoud60@gmail.com>
2019-04-07 21:10:40 +1000

    Punch Card improved

--- meteor-app/imports/ui/punch-card/punch-card.js
+++ meteor-app/imports/ui/punch-card/punch-card.js
@@ -1,21 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Digit.css'
 
 const PunchCard = props => {
+  //A function that fetchs array of attended and 'not' attended weeks
+
   const digitClicked = () => {
     alert('clicked')
   }
+  const weeks = [
+    //Sample array
+    { '1': true },
+    { '2': false },
+    { '3': false },
+    { '4': true },
+    { '5': true },
+    { '6': false },
+    { '7': true },
+    { '8': false },
+    { '9': true },
+    { '10': true },
+    { '11': false },
+    { '12': true }
+  ]
+
+  //Arrays
   const { visits, numVisits } = props
   const elements = []
-  for (let i = 1; i <= visits; i++) {
-    // push the component to elements!
-    const style = i <= numVisits ? 'DigitUsed' : 'Digit'
-    elements.push(<div className={style}>{i}</div>)
+  const weekValue = [] //Array of attended and 'not'attended weeks. let say true is attended and false is not attended
+  const weekNum = [] //Key of each object in weeks array
+  let weeksDone = 0
+  let weeksLeft = 0
+
+  //populating values in arrays
+  for (let i = 0; i < weeks.length; i++) {
+    weekValue.push(Object.values(weeks[i]))
+  }
+  for (let i = 0; i < weeks.length; i++) {
+    weekNum.push(Object.keys(weeks[i]))
+  }
+
+  for (let i = 0; i < weeks.length; i++) {
+    //const style = weekValue[i] ? 'DigitUsed' : 'Digit'
+
+    if (weekValue[i] == 'false') {
+      elements.push(
+        <div className="Digit" onClick={() => digitClicked}>
+          {weekNum[i]}
+        </div>
+      )
+      weeksLeft++
+    } else if (weekValue[i] == 'true') {
+      elements.push(<div className="DigitUsed">X</div>)
+      weeksDone++
+    }
   }
 
   return (
     <div className="Punch-Card">
+      <div className="Container">
+        <h1 className="header">Workshop Weeks</h1>
+      </div>
       <div className="Container">{elements}</div>
+      <div className="footer">
+        Weeks completed: {weeksDone} and Weeks left: {weeksLeft}
+      </div>
     </div>
   )
 }
