8c0aaca66485b1728e664ca4dbe2d680607e2754
mikkel <mikkelking@hotmail.com>
2019-05-04 20:12:18 +1000

    Added casual email text

--- meteor-app/imports/ui/renew/main.js
+++ meteor-app/imports/ui/renew/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { Button, Card, Segment, Grid, Header } from 'semantic-ui-react'
+import { Image, Card, Segment, Grid, Header } from 'semantic-ui-react'
 
 import MemberCard from '/imports/ui/member/member-card-small'
 import { ProductCardOnly } from '/imports/ui/shop/product-card'
@@ -10,10 +10,14 @@ import { ProductCardOnly } from '/imports/ui/shop/product-card'
 const Main = props => {
   const add = props => {}
   const remove = props => {}
+  const logoFile = Meteor.settings.public.logo || '/images/logo-tiny.jpg'
   if (props.loading) return <div>Loading...</div>
   return (
     <div>
-      <Header as="h1">{Meteor.settings.public.org} membership renewal</Header>
+      <Header as="h1">
+        {' '}
+        <Image src={logoFile} height="35px" /> {Meteor.settings.public.org} membership renewal
+      </Header>
       <Segment>
         <Grid style={{ height: '100%' }} verticalAlign="middle" divided>
           <Grid.Column width={6}>
