1cc8e444d5f299be5ad288af33227e659f620e29
Saoud Nazir <mirzasaoud60@gmail.com>
2019-05-07 20:16:36 +1000

    Improved Emailing Code

--- meteor-app/imports/startup/server/email.js
+++ meteor-app/imports/startup/server/email.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 import { Email } from 'meteor/email'
 const debug = require('debug')('b2b:email')
+const sgMail = require('@sendgrid/mail')
 import { eventLog } from '/imports/api/eventlogs'
 import Purchases from '/imports/api/purchases/schema'
 import Members from '/imports/api/members/schema'
@@ -37,13 +38,12 @@ Meteor.methods({
       log.error('Error from email gateway', error)
     }
   },
-  sendMembershipRenewal(to, name, type, expiryDate) {
-    const sgMail = require('@sendgrid/mail')
+  sendMembershipEmail(to, name, type, expiryDate, apiKey) {
     sgMail.setApiKey(Meteor.settings.private.sendgridApikey)
     const options = {
       to,
       from: Meteor.settings.private.fromEmail,
-      templateId: Meteor.settings.private.expiredMembershipID,
+      templateId: apiKey,
       dynamic_template_data: {
         name,
         type,
@@ -52,17 +52,41 @@ Meteor.methods({
     }
     sgMail.send(options)
   },
-  sendRenewals() {
+
+  sendMembershipRenewals() {
     Members.find({}).forEach(member => {
-      Purchases.find({ memberId: member._id, code: 'PA-MEMB-12', expiry: { $lt: new Date() } }).forEach(purchase => {
-        /*Meteor.call(
-          'sendRenewalEmail',
+      Purchases.find({
+        memberId: member._id,
+        $or: [{ code: 'PA-MEMB-12' }, { code: 'PA-MEMB-3' }],
+        expiry: { $lt: new Date() }
+      }).forEach(purchase => {
+        Meteor.call(
+          'sendMembershipEmail',
           member.email,
           member.name,
           purchase.productName,
-          Moment(purchase.expiry).format('Do MMM YYYY')
-        )*/
-        debug(purchase.purchaser)
+          Moment(purchase.expiry).format('Do MMM YYYY'),
+          Meteor.settings.private.expiredMembershipID
+        )
+      })
+    })
+  },
+  sendMembershipReminderEmail() {
+    Members.find({}).forEach(member => {
+      Purchases.find({
+        memberId: member._id,
+        $or: [{ code: 'PA-MEMB-12' }, { code: 'PA-MEMB-3' }],
+        expiry: { $gt: new Date() }
+      }).forEach(purchase => {
+        Meteor.call(
+          'sendMembershipEmail',
+          member.email,
+          member.name,
+          purchase.productName,
+          Moment(purchase.expiry).format('Do MMM YYYY'),
+          Meteor.settings.private.validMembershipID
+        )
+        debug('Email sending to ' + purchase.purchaser)
       })
     })
   }
