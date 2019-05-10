33e160fa04ef4da3ae14679128553205698f4f01
mikkel <mikkelking@hotmail.com>
2019-05-11 02:38:01 +1000

    Fixed product search performance problem

--- meteor-app/imports/ui/ordering/ordering.js
+++ meteor-app/imports/ui/ordering/ordering.js
@@ -1,135 +1,142 @@
-import React from "react";
-import PropTypes from "prop-types"
-import { withRouter } from "react-router-dom";
-import {
-  Grid,
-  Container,
-  Header,
-  Input,
-  Button,
-  Dimmer,
-  Loader,
-  Image,
-  Message,
-  Segment,
-  Icon
-} from "semantic-ui-react";
-import UploadXL from "/imports/ui/ordering/uploadXL";
-import PartCard from "/imports/ui/ordering/ordering-part-card";
-import PartList from "/imports/ui/ordering/ordering-part-list";
-import CartIcon from "/imports/ui/ordering/cart-icon";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
+import { Grid, Label, Header, Input, Button, Dimmer, Loader, Image, Message, Segment, Icon } from 'semantic-ui-react'
+import UploadXL from '/imports/ui/ordering/uploadXL'
+import PartCard from '/imports/ui/ordering/ordering-part-card'
+import PartList from '/imports/ui/ordering/ordering-part-list'
+import CartIcon from '/imports/ui/ordering/cart-icon'
 
-class Ordering extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      addParts: true
-    };
-  }
+const Ordering = props => {
+  const [addParts, setAdd] = React.useState(true)
+  const [searchFor, setSearch] = React.useState('')
+  const [virgin, setVirgin] = React.useState(true)
 
   toggleAddPart = () => {
-    this.setState({ addParts: !this.state.addParts });
+    setAdd(!addParts)
   }
 
-  render() {
-    if (this.props.loading) {
-      return (
-        <div>
-          <Dimmer active inverted>
-            <Loader size="huge">Loading... </Loader>
-          </Dimmer>
+  onSearchInput = e => {
+    setSearch(e.target.value)
+  }
 
-          <Image src="/images/wireframe/short-paragraph.png" />
-        </div>
-      );
+  search = e => {
+    // e.preventDefault()
+    props.onSearchInput(searchFor)
+  }
+
+  onKeyPress = target => {
+    if (target.key === 'Enter') {
+      props.onSearchInput(searchFor)
+      setVirgin(false)
     }
-    const { activeOrder, uploadXL } = this.props;
-    let noOfParts = 0;
+  }
 
+  if (props.loading) {
     return (
-      <Grid container columns="equal">
-        {!this.props.loading &&
-          activeOrder &&
-          activeOrder.orderedParts.forEach(part => {
-            noOfParts += part.qty;
-            return noOfParts;
-          })}
+      <div>
+        <Dimmer active inverted>
+          <Loader size="huge">Loading... </Loader>
+        </Dimmer>
 
-        <Grid.Row columns={1}>
-          <Grid.Column width={16}>
-            <Header as="h2" textAlign="center">
-              {" "}
-              <div>Parts Search</div>{" "}
-            </Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={1} centered>
-          <CartIcon noOfParts={noOfParts} />
-          <br />
-          <Input
-            placeholder="Part number or name"
-            className="member-search"
-            onChange={this.props.onSearchInput}
-            value={this.props.partSearchQuery}
-            icon={"search"}
-            size="huge"
-            fluid
-          />{" "}
-        </Grid.Row>
+        <Image src="/images/wireframe/short-paragraph.png" />
+      </div>
+    )
+  }
 
-        <Grid.Row columns={1} centered>
-          {this.props.parts < 1 ? (
-            <>
-              <Message
-                icon="inbox"
-                header="Nothing found"
-                content="Please try again"
-              />
-              <Button
-                onClick={this.toggleAddPart}
-                style={{
-                  height: "100px",
-                  marginTop: "20px",
-                  marginBottom: "20px"
-                }}
-                color="grey"
-              >
-                <h1>Add updated pricelist</h1>
-              </Button>
+  const { activeOrder, uploadXL } = props
+  let noOfParts = 0
+  const { parts = [] } = props
 
-              {this.state.addParts ? (
-                ""
-              ) : (
-                <UploadXL
-                  uploadXL={this.props.uploadXL}
-                  toggleAddPart={this.toggleAddPart}
-                />
-              )}
-            </>
-          ) : (
-            ""
+  return (
+    <Grid container columns="equal">
+      {!props.loading &&
+        activeOrder &&
+        activeOrder.orderedParts.forEach(part => {
+          noOfParts += part.qty
+          return noOfParts
+        })}
+
+      <Grid.Row columns={1}>
+        <Grid.Column width={16}>
+          <Header as="h2" textAlign="center">
+            {' '}
+            <div>Parts Search</div>{' '}
+          </Header>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={1} centered>
+        <CartIcon noOfParts={noOfParts} />
+        <br />
+        <Input
+          placeholder="Part number or name"
+          className="member-search"
+          onChange={onSearchInput}
+          defaultValue={searchFor}
+          onKeyPress={onKeyPress}
+          size="huge"
+          fluid
+        >
+          <input />
+          {parts.length > 0 && !virgin && (
+            <Label color="yellow" floating>
+              {parts.length}
+            </Label>
           )}
-        </Grid.Row>
+          <Button type="submit" icon labelPosition="right" color="green" size="huge" onClick={search}>
+            <Icon name="search" />
+            Search
+          </Button>
+        </Input>
+      </Grid.Row>
 
+      <Grid.Row columns={1} centered>
+        {!searchFor && (
+          <Message icon="info" header="Please enter part number or name" content="and press ENTER to search" />
+        )}
+
+        {parts < 1 ? (
+          <>
+            <Message icon="blind" header="Nothing found" content="Please try again" />
+            <Button
+              onClick={toggleAddPart}
+              style={{
+                height: '100px',
+                marginTop: '20px',
+                marginBottom: '20px'
+              }}
+              color="grey"
+            >
+              <h1>Add updated pricelist</h1>
+            </Button>
+
+            {addParts ? '' : <UploadXL uploadXL={props.uploadXL} toggleAddPart={toggleAddPart} />}
+          </>
+        ) : (
+          ''
+        )}
+      </Grid.Row>
+
+      {searchFor && !virgin && (
         <Grid.Row>
           <Grid.Column>
             <PartList
-              title={"Part Title:"}
-              parts={this.props.parts}
-              activeOrder={this.props.activeOrder}
-              addToCart={this.props.addToCart}
+              title={'Part Title:'}
+              parts={parts}
+              activeOrder={props.activeOrder}
+              addToCart={props.addToCart}
               Component={PartCard}
               componentClassName="part-card-main"
-              loading={this.props.loading}
+              loading={props.loading}
             />
           </Grid.Column>
         </Grid.Row>
-      </Grid>
-    );
-  }
+      )}
+    </Grid>
+  )
 }
 
-export default withRouter(Ordering);
+export default withRouter(Ordering)
 
 Ordering.propTypes = {
   activeOrder: PropTypes.object,
@@ -138,5 +145,5 @@ Ordering.propTypes = {
   loading: PropTypes.bool.isRequired,
   partSearchQuery: PropTypes.string,
   onSearchInput: PropTypes.func,
-  uploadXL: PropTypes.func.isRequired,
+  uploadXL: PropTypes.func.isRequired
 }
