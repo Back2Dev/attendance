561b4134233d01e6b6ec06d56e17959d30f32e9b
Mikkel <mikkelking@hotmail.com>
2019-08-29 07:07:27 +1000

    Do not use last product in cart

--- meteor-app/imports/ui/shop/renew.js
+++ meteor-app/imports/ui/shop/renew.js
@@ -11,7 +11,7 @@ import ProductButton from '/imports/ui/shop/product-button'
 import { CartContext } from './cart-data'
 
 const Renew = props => {
-  const [product, setProduct] = React.useState(props.myProduct || props.cart.products[0])
+  const [product, setProduct] = React.useState(props.cart.products[0] || props.myProduct)
   const { state, dispatch } = React.useContext(CartContext)
   // With the next action, the product is in the cart already
   const next = () => {
