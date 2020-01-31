a3a857016e99c32330f3af063172611da2dcccfc
Patrick Carmel <pato@live.com.au>
2020-01-31 13:35:31 +1100

    added and created mercx tests to meteor

---
+++ meteor-app/tests/cypress/integration/main/e2e-multi-pass-bruce.js
@@ -0,0 +1,10 @@
+/* Pseudo code for Multi Pass
+  * Open the application
+  
+  * Navigate to /kiosk
+  * Find test Bruce Lee
+  * login on Bruce Lee
+  * Add an event
+  * sign of Bruce Lee
+  * Create loop which will iterate 15 times for this
+  */
---
+++ meteor-app/tests/cypress/integration/main/e2e-multi-pass-merx.js
@@ -0,0 +1,10 @@
+/* Pseudo code for Multi Pass
+  * Open the application
+  
+  * Navigate to /kiosk
+  * Find test Eddie Merx
+  * login on Eddie Merx
+  * Add an event
+  * sign of Eddie Merx
+  * Create loop which will iterate 15 times for this
+  */
