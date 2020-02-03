5e170208f08c6085ec0bf69e9a406f81f679c27b
Patrick Carmel <pato@live.com.au>
2020-02-03 12:06:26 +1100

    Added tough guy test script to project

---
+++ meteor-app/tests/cypress/integration/main/e2e-multi-add-tough.js
@@ -0,0 +1,10 @@
+/* Pseudo code for Multi Pass Tough 
+  * Open the application
+  
+  * Navigate to /kiosk
+  * Find test member Tough Guy
+  * login on Tough Guy
+  * Add an event
+  * sign of Tough Guy
+  * Create loop which will iterate 15 times for this
+  */
