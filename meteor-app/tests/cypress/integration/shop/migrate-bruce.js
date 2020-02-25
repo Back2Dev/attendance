1687f0b6ebfdcf37315c5d6a1f469545d473f6d5
Jack <jackzhuoyang@gmail.com>
2020-02-25 15:20:28 +1100

    Added migration tests

---
+++ meteor-app/tests/cypress/integration/shop/migrate-bruce.js
@@ -0,0 +1,28 @@
+import dude from '../../support/test-data-bruce-lee'
+
+describe('Create a Bruce Lee', function() {
+  it(`Creates a Bruce`, function() {
+    cy.visit('/')
+    addBruce()
+  })
+})
+
+describe('Migrate Bruce', function() {
+  it('Migrates with no purchases', function() {
+    cy.visit(`/admin/userprofiles/${dude.member._id}`)
+    cy.get('input[name=email]')
+      .clear()
+      .type('admin@back2bikes.com.au')
+
+    cy.get('input[name=password]')
+      .clear()
+      .type('me2')
+
+    cy.get('button')
+      .contains('Submit')
+      .should('be.enabled')
+      .click()
+
+    cy.contains('Merge').click()
+  })
+})
---
+++ meteor-app/tests/cypress/integration/shop/migrate-eddie.js
@@ -0,0 +1,39 @@
+import dude from '../../support/test-data-eddie-mercx'
+
+const numSessions = dude.member.sessionCount
+
+describe('Create an Eddie Mercx', function() {
+  it(`Creates an Eddie`, function() {
+    cy.visit('/')
+    addEddie()
+  })
+})
+
+describe('Migrate Eddie', function() {
+  it('Migrates casual member with no current purchases', function() {
+    cy.visit(`/admin/userprofiles/${dude.member._id}`)
+    cy.get('input[name=email]')
+      .clear()
+      .type('admin@back2bikes.com.au')
+
+    cy.get('input[name=password]')
+      .clear()
+      .type('me2')
+
+    cy.get('button')
+      .contains('Submit')
+      .should('be.enabled')
+      .click()
+
+    cy.contains('Merge').click()
+    cy.get('#numPurchases').contains(numSessions)
+    cy.get('.content').contains(numSessions + ' items')
+  })
+})
+
+describe('Reset Eddie Mercx', function() {
+  it(`Creates an Eddie`, function() {
+    cy.visit('/')
+    addEddie()
+  })
+})
