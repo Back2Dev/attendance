d721a50f3f5d784d7e517fc14972414009228ad9
Luke Dawson <l.j.dwsn@gmail.com>
2020-02-25 13:00:44 +1100

    updated my component to a hook

--- meteor-app/imports/ui/member/return-to-kiosk-button.js
+++ meteor-app/imports/ui/member/return-to-kiosk-button.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class Kioskbutton extends React.Component {
-  render() {
-    return (
-      <div>
-        <button>
-          <Link to="/">home</Link>
-        </button>
-      </div>
-    )
-  }
+function Kioskbutton() {
+  return (
+    <div>
+      <button>
+        <Link to="/">home</Link>
+      </button>
+    </div>
+  )
 }
 
 export { Kioskbutton as default }
