6e9573c97f4f679ee37924ef67bd5da673e5daaa
Carl Davidson <carl.davidson@hotmail.com>
2020-02-28 17:03:47 +1100

    minor changes to state management of component

--- meteor-app/imports/ui/assessment/AssessmentWHOCarl.js
+++ meteor-app/imports/ui/assessment/AssessmentWHOCarl.js
@@ -1,16 +1,41 @@
 import React, { useState } from 'react'
 import './AssessmentWHOCarlStyles.css'
 
-function AssessmentChecklist() {
+function todo({index, removeItem}){
+    return(
+        <div>
+            <button onClick={() => removeItem(index)}>X</button>
+        </div>
+    )
+}
+
+function AssessmentChecklist({ addItem }) {
+  const [value, setValue] = useState('')
+  const [item, setItem] = useState([])
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!value) return
+    addItem(value)
+    setValue('')
+  }
+
+  const removeItem = index => {
+      const newValue = [...value]
+      newValue.splice(index, 1)
+      setValue(newValue)
+  }
+
   return (
     <div>
       <div className="input-container">
-        <form>
-          <label>
-            Search:
-            <input type="text" placeholder="Search.." class="ac-input" />
-          </label>
-          <input type="submit" value="submit" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="submit"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Search Services/Items..."
+            removeItem={removeItem}
+          />
         </form>
       </div>
     </div>
