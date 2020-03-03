97254d6ceec4244c599ec4ca26346505380ac270
Murtaza <Murtaza_taloo@hotmail.com>
2020-03-03 14:05:31 +1100

    added a story for the tag component

---
+++ meteor-app/imports/ui/assessment/serviceItem-tag.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+// import { action } from '@storybook/addon-actions'
+
+import ServiceItemTag from './serviceItem-tag'
+
+storiesOf('ServiceItemTag', module).add('ServiceItemTag', () => <ServiceItemTag />)
