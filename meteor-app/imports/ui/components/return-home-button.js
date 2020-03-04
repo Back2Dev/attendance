c33879d09dd57ca5d1c668bfc844705bd37602fc
Jack <jackzhuoyang@gmail.com>
2020-03-04 12:37:26 +1100

    Cleanup some files

---
+++ meteor-app/imports/ui/components/return-home-button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import context from '/imports/ui/utils/nav'
+
+const goHome = props => {
+  return <Button onClick={props.history.push(context.goHome())}>Home</Button>
+}
+
+export default goHome
