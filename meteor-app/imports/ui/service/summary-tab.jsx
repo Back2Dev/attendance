2758e9e47bb92017de32291df91ac1c9b7120897
Yunfeng-Song <songyunfeng.donnie@gmail.com>
2020-03-06 20:18:33 +1100

    tab 3 works

--- meteor-app/imports/ui/service/summary-tab.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-//import JobCard from './assessment-job-card'
-
-const Summary = props => {
-  return <div>hahaha</div>
-}
-
-export default Summary
--- meteor-app/imports/ui/service/summary-tab.stories.js
+++ meteor-app/imports/ui/service/summary-tab.stories.js
@@ -1,6 +1,46 @@
 import React from 'react'
-
 import { storiesOf } from '@storybook/react'
 import Summary from './summary-tab'
+import { action } from '@storybook/addon-actions'
+import { fakeJob } from '/imports/test/fake-data'
+import { withKnobs, number } from '@storybook/addon-knobs/react'
+import { LOG_EVENT_TYPES, JOB_STATUS, NEW_JOB, STATUS_UPDATE } from '/imports/api/constants'
 
-storiesOf('task', module).add('default1', () => <Summary />)
+const members = [
+  {
+    id: 1,
+    name: 'Mike King'
+  },
+  {
+    id: 2,
+    name: 'Matt Wiseman'
+  }
+]
+
+const currentJob = {
+  ...fakeJob(),
+  status: number('Status', JOB_STATUS.NEW)
+}
+
+const logs = [
+  {
+    _id: 123,
+    user: 'Mike',
+    aId: currentJob._id,
+    status: JOB_STATUS.NEW,
+    eventType: LOG_EVENT_TYPES.NEW_JOB,
+    data: 'test log',
+    createdAt: new Date('2018-09-01')
+  }
+]
+
+storiesOf('task', module).add('default', () => (
+  <Summary
+    key={currentJob._id}
+    currentJob={currentJob}
+    updateStatus={action('Update Status')}
+    members={members}
+    logs={logs}
+    job={currentJob}
+  />
+))
