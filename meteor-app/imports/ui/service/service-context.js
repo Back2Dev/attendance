216cedcb4039e70907514cb0bfaf3d64ae990fe3
Murtaza <Murtaza_taloo@hotmail.com>
2020-03-11 15:26:56 +1100

    uncommented the context data file

--- meteor-app/imports/ui/service/service-context.js
+++ meteor-app/imports/ui/service/service-context.js
@@ -1,14 +1,7 @@
 import React, { useState, createContext } from 'react'
-// import { serviceState } from './service-context-data'
+import { serviceState } from './service-context-data'
 
 export const ServiceContext = createContext()
-
-const data = []
-
-const serviceState = {
-  data: data,
-  tags: []
-}
 
 export const ServiceContextProvider = props => {
   const [state, setState] = useState(serviceState)
