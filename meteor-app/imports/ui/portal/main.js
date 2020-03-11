bdc09b62c36d3f534ad80ea98349d8714a6d729b
Jack <jackzhuoyang@gmail.com>
2020-03-11 16:10:03 +1100

    Fixed portal page

--- meteor-app/imports/ui/portal/main.js
+++ meteor-app/imports/ui/portal/main.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
-import GoHome from '/imports/ui/components/go-home-button'
-import { Button, Card, Segment, Grid, Header, Image, Container, Icon, Table } from 'semantic-ui-react'
+import { Button, Card, Segment, Grid, Header, Image, Container, Icon } from 'semantic-ui-react'
 import MembershipCard from '/imports/ui/member-card/member-card'
 import MultiVisitsCard from '/imports/ui/punch-card/multi-visits-card'
 import MemberVisitsCard from '/imports/ui/punch-card/member-visits-card'
@@ -10,17 +9,7 @@ import { startOfDay } from 'date-fns'
 
 const MemberPortal = props => {
   if (props.loading) return <div>Loading...</div>
-  const cards = {}
-  if (props.member.subsType === 'member') {
-    props.sessions.forEach(session => {
-      const y = moment(session.timeIn).year()
-      const m = moment(session.timeIn).month()
-      if (!cards[y]) {
-        cards[y] = { months: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
-      }
-      cards[y].months[m] += 1
-    })
-  }
+
   return (
     <Segment>
       <Header as="h1" style={{ textAlign: 'center' }}>
@@ -29,13 +18,11 @@ const MemberPortal = props => {
       <Grid.Column width={6}>
         <Card.Group style={{ paddingBottom: '20px' }}>
           <MembershipCard className="member-visit-card" member={props.member} />)
-          {props.member.subsType === 'member' && props.purchase && <MemberVisitsCard memberDuration={12} />}
         </Card.Group>
       </Grid.Column>
       <Button type="button" onClick={() => props.toEdit()} style={{ height: '50px' }}>
         Edit your profile
       </Button>
-      <GoHome />
       {props.addCard === 1 && !props.member.paymentCustId ? (
         <Button
           type="button"
@@ -51,7 +38,6 @@ const MemberPortal = props => {
         <Button
           style={{ height: '50px' }}
           color="red"
-          floated="right"
           onClick={() => props.history.push(`/shop/renew/${props.member._id}/${props.cart._id}`)}
         >
           Pay Now
@@ -59,82 +45,33 @@ const MemberPortal = props => {
       )}
       <Container style={{ display: 'flex', flexWrap: 'wrap' }}>
         {props.purchases &&
-          props.member.subsType === 'pass' &&
-          props.purchases.map((purchase, ix) => (
-            <MultiVisitsCard
-              key={ix}
-              usedVisits={purchase.sessions.length}
-              totalVisits={purchase.sessions.length + purchase.remaining}
-              paid={purchase.paymentStatus === 'paid'}
-            />
-          ))}
-        {props.sessions &&
-          props.member.subsType === 'member' &&
-          Object.keys(cards).map((year, ix) => {
-            return <MemberVisitsCard key={ix} months={cards[year].months} title={`Visits ${year}`} />
+          props.purchases.map(purchase => {
+            if (purchase.code === 'PA-PASS-MULTI-10') {
+              return (
+                <MultiVisitsCard
+                  usedVisits={purchase.sessions.length}
+                  totalVisits={purchase.sessions.length + purchase.remaining}
+                  paid={purchase.paymentStatus === 'paid'}
+                />
+              )
+            } else if (purchase.code.includes('PA-MEMB')) {
+              return (
+                <MemberVisitsCard
+                  startDate={purchase.createdAt}
+                  expiryDate={purchase.expiry}
+                  paid={purchase.paymentStatus === 'paid'}
+                  sessions={purchase.sessions}
+                />
+              )
+            }
           })}
       </Container>
-      <Card fluid>
-        <Card.Content header={`Purchases (${props.purchases.length})`} />
-        <Card.Content>
-          <Card.Description>
-            <Table basic="very" celled>
-              <Table.Body>
-                {props.purchases.length === 0 ? (
-                  <div>(None)</div>
-                ) : (
-                  props.purchases.map((purchase, ix) => (
-                    <Table.Row negative={purchase.paymentStatus === 'unpaid'} key={`s${ix}`}>
-                      <Table.Cell>{purchase.productName}</Table.Cell>
-                      <Table.Cell>{'$' + purchase.price / 100}</Table.Cell>
-                      <Table.Cell>{moment(purchase.createdAt).format('D MMM YYYY')}</Table.Cell>
-                      <Table.Cell>
-                        {purchase.paymentStatus === 'unpaid' ? (
-                          <b>{purchase.paymentStatus.toUpperCase()}</b>
-                        ) : (
-                          purchase.paymentStatus
-                        )}
-                      </Table.Cell>
-                    </Table.Row>
-                  ))
-                )}{' '}
-              </Table.Body>
-            </Table>
-          </Card.Description>
-        </Card.Content>
-      </Card>
-      <Card fluid>
-        <Card.Content header={`Sessions (${props.sessions.length})`} />
-        <Card.Content>
-          <Table basic="very" celled>
-            <Table.Body>
-              {props.sessions.length === 0 ? (
-                <div>(none)</div>
-              ) : (
-                props.sessions.map((session, ix) => (
-                  <Table.Row key={`s${ix}`}>
-                    <Table.Cell>{session.name}</Table.Cell>
-                    <Table.Cell>{moment(session.createdAt).format('D MMM YYYY')}</Table.Cell>
-                  </Table.Row>
-                ))
-              )}
-            </Table.Body>
-          </Table>
-        </Card.Content>
-      </Card>
-      {props.member.subsType === 'pass' &&
-        props.purchases.map(purchase => (
-          <MultiVisitsCard
-            style={{ marginLeft: 0 }}
-            usedVisits={purchase.sessions.length}
-            totalVisits={purchase.sessions.length + purchase.remaining}
-          />
-        ))}
     </Segment>
   )
 }
 
 MemberPortal.propTypes = {
+  cart: PropTypes.object.isRequired,
   member: PropTypes.object.isRequired,
   purchase: PropTypes.object.isRequired,
   purchases: PropTypes.array.isRequired,
