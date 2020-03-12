f188255b491899406601523cbd5b3d6586dd5733
Jack <jackzhuoyang@gmail.com>
2020-03-12 16:34:52 +1100

    Fixed pin update

--- meteor-app/imports/ui/member/member-edit-user-details.js
+++ meteor-app/imports/ui/member/member-edit-user-details.js
@@ -4,15 +4,14 @@ import { Form, Input, Message } from 'semantic-ui-react'
 export default UserDetailForm = ({ formData, setUser }) => {
   const [password, setPassword] = React.useState('')
   const [newPin, setNewPin] = React.useState(formData.pin)
-  const [matchPin, setMatchPin] = React.useState(null)
   const [data, setData] = React.useState(formData)
   const [error, setError] = React.useState(null)
 
   const handleSubmit = (e, h) => {
     setError(null)
-    if (newPin.length !== 4) {
+    if (data.pin.length !== 4) {
       return setError('Pin number must be 4 digits')
-    } else if (newPin !== matchPin) {
+    } else if (newPin !== data.pin) {
       return setError('Pin does not match')
     }
     setUser(data)
@@ -61,7 +60,7 @@ export default UserDetailForm = ({ formData, setUser }) => {
         <Form.Field
           label="Confirm New Pin"
           onChange={e => {
-            setMatchPin(e.target.value)
+            setData({ ...formData, pin: e.target.value })
           }}
           control={Input}
         />
