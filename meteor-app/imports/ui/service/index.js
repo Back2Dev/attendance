05e0fcebee1b0981a928d1975c6470063a5f7240
Yunfeng-Song <songyunfeng.donnie@gmail.com>
2020-03-12 12:50:32 +1100

    add tabs in index file

--- meteor-app/imports/ui/service/index.js
+++ meteor-app/imports/ui/service/index.js
@@ -2,10 +2,35 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Grid, Segment } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
+import { Tab } from 'semantic-ui-react'
+import { Roles } from 'meteor/alanning:roles'
+
 import Service from './service'
+import Summary from './summary-tab'
+import FromTrail from './clientOP2'
+import Client from './client'
+
+const panes = [
+  {
+    menuItem: 'Service',
+    render: () => (
+      <Tab.Pane>
+        <Service />
+      </Tab.Pane>
+    )
+  },
+  {
+    menuItem: 'Details',
+    render: () => (
+      <Tab.Pane>
+        <FromTrail />
+      </Tab.Pane>
+    )
+  }
+]
 
 const ServiceAdd = props => {
-  return <Service />
+  return <Tab panes={panes} />
 }
 
 ServiceAdd.propTypes = {
