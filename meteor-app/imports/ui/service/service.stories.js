c3dd2c0d57f4e89d37201a85fc1feb9cf4773a9f
mikkel <mikkelking@hotmail.com>
2020-03-13 14:26:02 +1100

    Fixing storybook for service

--- meteor-app/imports/ui/service/service.stories.js
+++ meteor-app/imports/ui/service/service.stories.js
@@ -5,4 +5,6 @@ import storiesData from './service-data-stories'
 
 console.log('servicedata from the stories', storiesData)
 
-storiesOf('Service', module).add('Service', () => <Service />)
+storiesOf('Service', module).add('Service', () => {
+  return <Service tags={[]} data={storiesData.data} totalServicePrice={0} />
+})
