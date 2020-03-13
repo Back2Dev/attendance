661dcc8a367389e99bcd3c7c1ff02fe27c9c9e2d
Patrick Carmel <pato@live.com.au>
2020-03-13 12:35:41 +1100

    wworking on member portal edit test script got it to go to the edit details page

--- meteor-app/tests/cypress/integration/main/e2e-member-portal-edit.js
+++ meteor-app/tests/cypress/integration/main/e2e-member-portal-edit.js
@@ -1,17 +1,24 @@
 /* Pseudo code for testing member portal buttons
   * Navigate to /login
-  * enter  login details
-  * click on the member portal page
-  * enter members details
+  * Enter login details
+  * Click on the member portal page
+  * Enter members details
 
-  * open up test members portal
-  * click on the edit profile button
+  * Open up test members portal
+  * Click on the edit profile button
   * Determine whether it retrieves the edit profile webpage
   * 
-  * find a test member
-  * Enter a WWCC which is expired
+  * Click on the register your credit card button
+  * 
   * Check for error message
 */
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test. We do this because of some ugly js errors
+  // from a js library we are using
+  return false
+})
 
 describe('Retrieve kiosk webpage', function() {
   it('Retrieve kiosk webpage', function() {
@@ -30,15 +37,36 @@ describe('Retrieve kiosk webpage', function() {
       .should('be.enabled')
       .click()
 
-    cy.get('div').contains('Cathrine King')
+    cy.get('div')
+      .contains('Cathrine King')
+      .should('exist')
 
     cy.get('button')
       .contains('Edit your profile')
       .should('be.enabled')
       .click()
 
-    cy.get('h1').contains('Enter your details')
+    cy.get('h1')
+      .contains('Edit your details')
+      .should('exist')
 
     cy.get('a[href="/member-portal"').click()
+
+    cy.get('h1')
+      .contains('Peak Adventure')
+      .should('exist')
+
+    cy.get('div')
+      .contains('Cathrine King')
+      .should('exist')
+
+    cy.get('button')
+      .contains('Register your credit card')
+      .should('be.enabled')
+      .click()
+
+    cy.get('h2')
+      .contains('Credit Card Registration')
+      .should('exist')
   })
 })
