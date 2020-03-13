aa5aca3d6d1c1a246a00769019617144aac5e3a4
mikkel <mikkelking@hotmail.com>
2020-03-13 13:57:04 +1100

    tidy

--- meteor-app/imports/ui/service/serviceItem-tag.stories.js
+++ meteor-app/imports/ui/service/serviceItem-tag.stories.js
@@ -1,9 +1,6 @@
 import React from 'react'
-
 import { storiesOf } from '@storybook/react'
-
 import ServiceItemTag from './serviceItem-tag'
-
 import { storiesData } from './service-data-stories'
 
 console.log('servicedata from the item-tag stories', storiesData)
