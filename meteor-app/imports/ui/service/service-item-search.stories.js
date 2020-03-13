6f9cafb0fbd91a3de2ab7f6d80bae6dc8126899f
mikkel <mikkelking@hotmail.com>
2020-03-13 14:50:08 +1100

    Fixed service stories

--- meteor-app/imports/ui/service/service-item-search.stories.js
+++ meteor-app/imports/ui/service/service-item-search.stories.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import SearchBar from './service-item-search'
+import { ServiceContextProvider } from './service-context'
+import SearchBar from './service-item-search-container'
 
 import storiesData from './service-data-stories'
 
@@ -9,14 +9,8 @@ console.log('servicedata from the  search stories', storiesData)
 
 storiesOf('Service', module).add('SearchBar', () => {
   return (
-    <>
-      Note: Entering text won't do a search, because the parent component is responsible for that
-      <SearchBar
-        handleResultSelect={action('select result')}
-        handleSearchChange={action('search change')}
-        results={[]}
-        value=""
-      />
-    </>
+    <ServiceContextProvider {...storiesData}>
+      <SearchBar></SearchBar>
+    </ServiceContextProvider>
   )
 })
