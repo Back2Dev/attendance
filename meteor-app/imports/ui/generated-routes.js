baf0ecd2165c85ee14a1025220f9011abd81a7d7
Mike King <mikkelking@hotmail.com>
2020-03-15 17:51:48 +1100

    Beginning to import generated modules

---
+++ meteor-app/imports/ui/generated-routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+// Generated code to import all of the DB UI components
+import EventsList from '/imports/ui/events/lister'
+
+//
+// This file contains a list of routes for database admin pages
+// It is generated from a list of modules
+//
+export default GeneratedRoute = () => (
+  <>
+    <Route path="/admin/events" component={EventsList} />
+  </>
+)
