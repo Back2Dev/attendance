f4400d7d9f1b23142a3080f7d94b11c5ee9af9a1
Patrick Carmel <pato@live.com.au>
2020-04-01 12:02:18 +1100

    added e2e-add-service

---
+++ meteor-app/tests/cypress/integration/main/e2e-add-service.js
@@ -0,0 +1,15 @@
+/* Pseudo code for Service
+  * Navigate to /login
+  * Enter login details
+  * Click on the Service
+  * Pick Major service
+
+  * Select different parts to add
+  * Minimise indivual jobs within major serivce
+  * Remove the major service
+  * 
+  * Enter minor service in search bar
+  * Click on minor service
+  * Add and remove extra parts
+  * disable jobs with the minor service option
+*/
