ef8a6b1e8ac79e6204a5125a1fe4fe1b3c991826
Patrick Carmel <pato@live.com.au>
2020-04-02 12:12:26 +1100

    working on e2e-add-service

--- meteor-app/tests/cypress/integration/main/e2e-add-service.js
+++ meteor-app/tests/cypress/integration/main/e2e-add-service.js
@@ -13,3 +13,75 @@
   * Add and remove extra parts
   * disable jobs with the minor service option
 */
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test. We do this because of some ugly js errors
+  // from a js library we are using
+  return false
+})
+
+describe('Retrieve Login webpage', function() {
+  it('Retrieve Login webpage', function() {
+    cy.visit('login')
+
+    cy.get('input[type="email"]')
+      .should('exist')
+      .clear()
+      .type('admin@back2bikes.com.au')
+
+    cy.get('input[type="password"]')
+      .should('exist')
+      .clear()
+      .type('me2')
+
+    cy.get('button')
+      .contains('Submit')
+      .should('exist')
+      .should('be.enabled')
+      .click()
+
+    cy.get('a[href="/service"]')
+      .should('exist')
+      .click()
+
+    cy.get('input[type="text"]')
+      .should('exist')
+      .clear()
+      .type('Maj')
+
+    cy.get('div[class="title"]')
+      .contains('Major Service')
+      .should('exist')
+      .click()
+
+    cy.get('span[class="expand"]')
+      .should('exist')
+      .click()
+
+    cy.get(':nth-child(10) > .handle')
+      .should('exist')
+      .click()
+
+    cy.get(':nth-child(4) > .handle')
+      .should('exist')
+      .click()
+
+    cy.get(':nth-child(2) > .handle')
+      .should('exist')
+      .click()
+
+    cy.get('input[type="text"]')
+      .should('exist')
+      .clear()
+      .type('spokes')
+
+    cy.get('div[name="Spokes and nipples"]')
+      .should('exist')
+      .click()
+
+    cy.get('.tag-wrapper > :nth-child(1) > .handle')
+      .should('exist')
+      .click()
+  })
+})
