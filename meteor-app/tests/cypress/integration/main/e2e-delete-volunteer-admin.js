26fd280a929310de6b04c5779ce82bc020152cd0
Chris-Tri <christophertri90@gmail.com>
2020-04-14 13:33:48 +1000

    cleaned few pieces of code

--- meteor-app/tests/cypress/integration/main/e2e-delete-volunteer-admin.js
+++
@@ -1,8 +0,0 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test. We do this because of some ugly js errors
-  // from a js library we are using
-  return false
-})
-
-describe('Clean up', () => {})
