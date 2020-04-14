b851e93f50d5f94b8b005061c172e05ad8cd5d0a
Mike King <mikkelking@hotmail.com>
2020-04-14 12:37:46 +1000

    Brought in streamer, for some UI niceness around progress updates

--- meteor-app/client/index.js
+++ meteor-app/client/index.js
@@ -1,1 +1,16 @@
-import '/imports/startup/client/startup';
+import '/imports/startup/client/startup'
+
+const streamer = new Meteor.Streamer('chat')
+
+if (Meteor.isClient) {
+  sendMessage = function (message) {
+    streamer.emit('message', message)
+    console.log('me: ' + message)
+  }
+
+  streamer.on('message', function (message) {
+    console.log('user: ', message)
+  })
+  streamer.emit('message', 'message from client')
+  streamer.emit('message', { a: 'message from client' })
+}
