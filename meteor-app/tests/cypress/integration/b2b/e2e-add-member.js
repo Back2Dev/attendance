59f03457ea8f1bc0a894a62bb002772020f55bf7
Patrick Carmel <pato@live.com.au>
2020-04-20 12:50:05 +1000

    fixing nth:child code in e2e-add-member

--- meteor-app/tests/cypress/integration/b2b/e2e-add-member.js
+++ meteor-app/tests/cypress/integration/b2b/e2e-add-member.js
@@ -17,7 +17,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
 
-describe('Adds Member to portal', function () {
+describe('Adds Member to portal', function() {
   this.beforeEach(() => {
     cy.visit('/login')
     cy.get('input[type="email"]') // Email
@@ -37,7 +37,7 @@ describe('Adds Member to portal', function () {
       .click()
   })
 
-  it('creates a volunteer', function () {
+  it('creates a volunteer', function() {
     cy.visit('/kiosk')
 
     cy.get('button')
@@ -46,18 +46,27 @@ describe('Adds Member to portal', function () {
       .should('be.enabled')
       .click()
 
-    cy.get('#root_bikesHousehold').clear().type('4')
+    cy.get('#root_bikesHousehold')
+      .clear()
+      .type('4')
 
     // TODO: eliminate use of nth-child
-    cy.get(':nth-child(4)  .ui  .search').should('be.enabled').click()
-
-    cy.get('.ui  .visible  :nth-child(5)')
-      //   .should('be.enabled')
+    cy.get('div[role="combobox"]')
+      .contains('Select a type of bike')
+      .should('exist')
       .click()
 
-    cy.get(':nth-child(5)  .ui .search').should('be.enabled').click()
+    cy.get('div[role="option"]')
+      .contains('Ladies')
+      .should('exist')
+      .click()
 
-    cy.get('.ui > .visible > :nth-child(3)')
+    cy.get('div[role="combobox"]')
+      .contains('Select your employment status')
+      .should('exist')
+      .click()
+
+    cy.get('div[role="option"]')
       .contains('Pension/Disability')
       .click()
 
@@ -66,41 +75,67 @@ describe('Adds Member to portal', function () {
       .should('be.enabled')
       .click()
 
-    cy.get('#root_name').clear().type('Ginny Tonic')
+    cy.get('#root_name')
+      .clear()
+      .type('Ginny Tonic')
 
-    cy.get('#root_email').clear().type('tests@test.com')
+    cy.get('#root_email')
+      .clear()
+      .type('tests@test.com')
 
     cy.get('#root_addressStreet')
       .clear()
       .type('12 potted plant grove')
 
-    cy.get('#root_addressSuburb').clear().type('Gangnam')
+    cy.get('#root_addressSuburb')
+      .clear()
+      .type('Gangnam')
 
-    cy.get('.ui > .search').click()
-    cy.get('.selected').click()
+    cy.get('div[role="combobox"]')
+      .contains('Enter your state')
+      .click()
 
-    cy.get('#root_addressPostcode').clear().type('3000')
+    cy.get('div[role="option"]')
+      .contains('VIC')
+      .click()
 
-    cy.get('#root_mobile').clear().type('111 101 211')
+    cy.get('#root_addressPostcode')
+      .clear()
+      .type('3000')
 
-    cy.get('#root_pin').clear().type('2713')
+    cy.get('#root_mobile')
+      .clear()
+      .type('111 101 211')
 
-    cy.get('#root_pinConfirm').clear().type('2713')
+    cy.get('#root_pin')
+      .clear()
+      .type('2713')
+
+    cy.get('#root_pinConfirm')
+      .clear()
+      .type('2713')
 
     cy.get('button[type="submit"]')
       .contains('Next')
       .should('be.enabled')
       .click()
 
-    cy.get('#root_emergencyContact').clear().type('Do Bong Soon')
+    cy.get('#root_emergencyContact')
+      .clear()
+      .type('Do Bong Soon')
 
     cy.get('#root_emergencyEmail')
       .clear()
       .type('intern.do@dointern.com.GCS.RX')
 
-    cy.get('#root_emergencyPhone').clear().type('999-000-110')
+    cy.get('#root_emergencyPhone')
+      .clear()
+      .type('999-000-110')
 
-    cy.get('button').contains('Next').should('be.enabled').click()
+    cy.get('button')
+      .contains('Next')
+      .should('be.enabled')
+      .click()
 
     cy.get('img[src="/images/avatars/test21.png"]').click()
 
@@ -109,21 +144,28 @@ describe('Adds Member to portal', function () {
       .should('be.enabled')
       .click()
 
-    cy.get('#root_privacy').should('exist').click({ force: true })
+    cy.get('#root_privacy')
+      .should('exist')
+      .click({ force: true })
 
     cy.get('button[type="submit"]')
       .contains('Next')
       .should('be.enabled')
       .click()
 
-    cy.get('button').contains('Submit').should('be.enabled').click()
+    cy.get('button')
+      .contains('Submit')
+      .should('be.enabled')
+      .click()
 
     cy.get('div[list="away"]')
       .contains('Ginny Tonic')
       .should('exist')
       .click()
 
-    cy.get('#pin').clear().type('2713')
+    cy.get('#pin')
+      .clear()
+      .type('2713')
 
     cy.get('button')
       .contains('Edit your profile')
@@ -136,7 +178,9 @@ describe('Adds Member to portal', function () {
       .should('exist')
       .click()
 
-    cy.get(':nth-child(5) > .ui > input').clear().type('2713')
+    cy.get(':nth-child(5) > .ui > input')
+      .clear()
+      .type('2713')
 
     cy.get('button')
       .contains('Submit')
@@ -153,7 +197,9 @@ describe('Adds Member to portal', function () {
       .click()
 
     // TODO: Why are we logging in again here? I don't think it's achieving anything
-    cy.get('a').contains('Home').click()
+    cy.get('a')
+      .contains('Home')
+      .click()
 
     cy.get('input[type="email"]')
       .should('exist')
