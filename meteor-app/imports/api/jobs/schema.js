801da8ccf9d70f987f41c19c994e2b84a83fbbe9
Jack <jackzhuoyang@gmail.com>
2020-06-09 18:24:33 +1000

    Added search

--- meteor-app/imports/api/jobs/schema.js
+++ meteor-app/imports/api/jobs/schema.js
@@ -11,15 +11,29 @@ export const JobsSchema = new SimpleSchema({
   _id: RegExId,
   jobNo: String,
   name: { type: String, optional: true, label: 'Customer name' },
-  phone: { type: String, optional: true, label: 'Customer phone number' },
+  phone: {
+    type: String,
+    optional: true,
+    label: 'Customer phone number',
+  },
   email: { type: String, optional: true, label: 'Customer email' },
   isRefurbish: { type: Boolean, label: 'Is a refurbishment' },
   make: { type: String, label: 'Bike make' },
   model: { type: String, optional: true, label: 'Bike model' },
   color: { type: String, label: 'Bike color' },
-  bikeValue: { type: SimpleSchema.Integer, label: 'Estimated bike value in cents' },
-  sentimentValue: { type: Boolean, optional: true, label: 'Bike holds sentimental value' },
-  serviceItems: { type: Array, label: 'Details of services/parts required' },
+  bikeValue: {
+    type: SimpleSchema.Integer,
+    label: 'Estimated bike value in cents',
+  },
+  sentimentValue: {
+    type: Boolean,
+    optional: true,
+    label: 'Bike holds sentimental value',
+  },
+  serviceItems: {
+    type: Array,
+    label: 'Details of services/parts required',
+  },
   'serviceItems.$': { type: ServiceItemsSchema },
   totalCost: {
     type: SimpleSchema.Integer,
@@ -32,23 +46,38 @@ export const JobsSchema = new SimpleSchema({
       }, 0)
       const check = calcServicesCost === this.value
       if (!check) {
-        return new Meteor.Error('Total services/parts cost not equal the sum of its parts!')
+        return new Meteor.Error(
+          'Total services/parts cost not equal the sum of its parts!'
+        )
       }
     },
   },
   dropoffDate: { type: Date, label: 'Bike drop-off date' },
   pickupDate: { type: Date, label: 'Bike pick-up date' },
-  urgent: { type: Boolean, label: 'Field to indicate if bike repair is urgent' },
+  urgent: {
+    type: Boolean,
+    label: 'Field to indicate if bike repair is urgent',
+  },
   assessor: { type: String, label: 'Assessor name' },
   mechanic: { type: String, optional: true, label: 'Mechanic name' },
-  comment: { type: String, optional: true, label: 'Field for putting in notes or additional services required' },
-  temporaryBike: { type: Boolean, label: 'Field to indicate if a temporary bike was provided' },
+  comment: {
+    type: String,
+    optional: true,
+    label:
+      'Field for putting in notes or additional services required',
+  },
+  temporaryBike: {
+    type: Boolean,
+    label: 'Field to indicate if a temporary bike was provided',
+  },
   status: {
     type: SimpleSchema.Integer,
-    allowedValues: Object.keys(JOB_STATUS_READABLE).map((key) => parseInt(key, 10)),
+    allowedValues: Object.keys(JOB_STATUS_READABLE).map((key) =>
+      parseInt(key, 10)
+    ),
     label: 'Status of job',
   },
-  // search: { type: String, label: 'Combination of searchable data' },
+  search: { type: String, label: 'Combination of searchable data' },
   paid: { type: Boolean, defaultValue: false },
   charge_token: { type: String, optional: true },
   card: { type: Object, optional: true, blackbox: true },
