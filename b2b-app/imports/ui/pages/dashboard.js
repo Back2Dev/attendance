da34e325bfa185a11f51ffd96a670714e8263969
Minh Nguyen <minhna@gmail.com>
2021-04-06 14:51:20 +0700

    create empty dashboard page

---
+++ b2b-app/imports/ui/pages/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Typography } from '@material-ui/core'
+
+const StyledDashboardPage = styled.div``
+
+function DashboardPage() {
+  return (
+    <StyledDashboardPage>
+      <Typography variant="h1">Dashboard</Typography>
+    </StyledDashboardPage>
+  )
+}
+
+export default DashboardPage
