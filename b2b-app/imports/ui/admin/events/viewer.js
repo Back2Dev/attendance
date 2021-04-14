028ab179e94340c1f4cacab1397d10c383991e39
Mikkel <mikkelking@hotmail.com>
2021-04-14 16:57:08 +1000

    Added events and courses

---
+++ b2b-app/imports/ui/admin/events/viewer.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor'
+import { withTracker } from 'meteor/react-meteor-data'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import Events from '/imports/api/events/schema'
+import { meteorCall } from '/imports/ui/utils/meteor'
+import View from './view'
+
+const debug = require('debug')('se:viewer')
+const idField = '_id'
+let push
+
+const remove = (id) => meteorCall('rm.events', 'Deleting', id)
+const update = (id, form) => {
+  meteorCall('update.events', 'updating', { id, form })
+  push('/admin/events')
+}
+
+const Loading = (props) => {
+  push = useHistory()?.push
+  if (props.loading) return <div>Loading...</div>
+  return <View {...props}></View>
+}
+const Tracker = withTracker((props) => {
+  history = props.history
+  const id = props.match.params.id
+  const subsHandle = Meteor.subscribe('id.events', id)
+  const item = Events.findOne(id) || {}
+  return {
+    id,
+    item,
+    remove,
+    update,
+    loading: !subsHandle.ready(),
+  }
+})(Loading)
+
+export default Tracker
