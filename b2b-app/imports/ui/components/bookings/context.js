be7d25d4fe49383fe86133b294391b83ccf8e76d
Minh Nguyen <minhna@gmail.com>
2021-04-16 17:56:56 +0700

    create bookings context

--- b2b-app/imports/ui/components/bookings/context.js
+++ b2b-app/imports/ui/components/bookings/context.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { useTracker } from 'meteor/react-meteor-data'
 
@@ -15,6 +15,11 @@ export const BookingsProvider = (props) => {
 
   const { member } = useContext(AccountContext)
 
+  const eventIds = useRef([])
+  const coachIds = useRef([])
+
+  const [eventsWithExtraData, setEventsWithExtraData] = useState([])
+
   const getCoachByCoachId = (coachId) => {
     return Members.findOne({ _id: coachId })
   }
@@ -27,28 +32,68 @@ export const BookingsProvider = (props) => {
   }
 
   const { loading, events } = useTracker(() => {
-    if (Meteor.isServer) {
-      return { loading: false, events: null }
-    }
+    // console.log('subscribe future.events')
     const sub = Meteor.subscribe('future.events')
-    const events = Events.find({ active: true, when: { $gt: new Date() } })
-    const eventsWithExtraData = events.map((event) => {
-      return {
-        ...event,
-        coach: getCoachByCoachId(event.coachId),
-        mySession: getMySessionByEventId(event._id),
-      }
-    })
-
     return {
       loading: !sub.ready(),
-      events: eventsWithExtraData,
+      events: Events.find({ active: true, when: { $gt: new Date() } }).fetch(),
     }
   }, [])
 
+  useEffect(() => {
+    const newEventIds = []
+    const newCoachIds = []
+    events.map((item) => {
+      newEventIds.push(item._id)
+      newCoachIds.push(item.coachId)
+    })
+    eventIds.current = newEventIds
+    coachIds.current = newCoachIds
+  }, [events])
+
+  const { loading: loadingCoaches } = useTracker(() => {
+    if (!coachIds.current.length) {
+      return { loading: false }
+    }
+    // console.log('subscribe members.byIds', coachIds.current)
+    const sub = Meteor.subscribe('members.byIds', coachIds.current)
+    return {
+      loading: !sub.ready(),
+    }
+  }, [coachIds.current])
+
+  const { loading: loadingSessions } = useTracker(() => {
+    if (!eventIds.current.length) {
+      return { loading: false }
+    }
+    const sub = Meteor.subscribe('sessions.mineByEventIds', eventIds.current)
+    return {
+      loading: !sub.ready(),
+    }
+  }, [eventIds.current])
+
+  useEffect(() => {
+    setEventsWithExtraData(
+      events?.map((item) => {
+        return {
+          ...item,
+          coach: getCoachByCoachId(item.coachId),
+          session: getMySessionByEventId(item._id),
+        }
+      })
+    )
+  }, [events, loadingCoaches, loadingSessions])
+
   return (
     <BookingsContext.Provider
-      value={{ loading, events, getCoachByCoachId, getMySessionByEventId }}
+      value={{
+        loading,
+        loadingCoaches,
+        loadingSessions,
+        events: eventsWithExtraData,
+        getCoachByCoachId,
+        getMySessionByEventId,
+      }}
     >
       {children}
     </BookingsContext.Provider>
