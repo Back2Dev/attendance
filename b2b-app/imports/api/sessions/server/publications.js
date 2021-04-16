a2f83d748956c2094fcaccef3d4fee8c9ebe789e
Minh Nguyen <minhna@gmail.com>
2021-04-16 17:57:35 +0700

    publish member sessions

--- b2b-app/imports/api/sessions/server/publications.js
+++ b2b-app/imports/api/sessions/server/publications.js
@@ -1,11 +1,21 @@
 import { Meteor } from 'meteor/meteor'
+
+import Members from '/imports/api/members/schema'
 import Sessions from '../schema'
 import '../methods'
 /* Commented out related publications (if any) - best to add these in manually as required
-import Members from '/imports/api/members/schema'
 import Events from '/imports/api/events/schema'
 import Tools from '/imports/api/tools/schema' 
 */
+
+Meteor.publish('sessions.mineByEventIds', function (eventIds) {
+  const currentMember = Members.findOne({ userId: this.userId })
+
+  return Sessions.find({
+    memberId: currentMember._id,
+    eventId: { $in: eventIds },
+  })
+})
 
 Meteor.publish('all.sessions', () => {
   return Sessions.find({})
