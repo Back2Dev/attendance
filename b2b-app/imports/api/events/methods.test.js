cc6123ad7cfbfa84fd58b188bc3b6419e4f4aff8
Minh Nguyen <minhna@gmail.com>
2021-04-20 16:46:14 +0700

    add test cases for cancel.events method

--- b2b-app/imports/api/events/methods.test.js
+++ b2b-app/imports/api/events/methods.test.js
@@ -1,17 +1,18 @@
 import { Meteor } from 'meteor/meteor'
-// import { Random } from 'meteor/random'
+import { Random } from 'meteor/random'
 
 import { use, expect } from 'chai'
 import assertArrays from 'chai-arrays'
 
 import Factory from '/imports/test/factories'
+import Members from '/imports/api/members/schema.js'
 import './methods'
 
 use(assertArrays)
 
 const debug = require('debug')('b2b:events:test')
 
-describe.only('Test book.events method', () => {
+describe('Test book.events method', () => {
   const theMethod = Meteor.server.method_handlers['book.events']
   it('book.events should return an object with status is failed with bad params', () => {
     const badParams = [
@@ -58,6 +59,61 @@ describe.only('Test book.events method', () => {
     let result
     expect(() => {
       result = theMethod.apply(thisContext, [{ eventId: event._id }])
+    }).not.to.throw()
+    debug({ result })
+    expect(result).to.have.property('status').which.equal('success')
+  })
+})
+describe('Test cancel.events method', () => {
+  const theMethod = Meteor.server.method_handlers['cancel.events']
+  it('cancel.events should return an object with status is failed with bad params', () => {
+    const badParams = [
+      { sessionId: 'some string' }, // missing eventId
+      { someParam: 'some value' }, // sessionId is missing
+    ]
+    badParams.map((item) => {
+      const thisContext = {
+        userId: null,
+      }
+      let result
+      expect(() => {
+        result = theMethod.apply(thisContext, [item])
+      }).not.to.throw()
+      // debug(result)
+      expect(result).to.have.property('status').which.equal('failed')
+    })
+  })
+  it('cancel.events should fail with not logged in user', () => {
+    const thisContext = {
+      userId: null, // not logged in
+    }
+    let result
+    expect(() => {
+      result = theMethod.apply(thisContext, [{ sessionId: Random.id() }])
+    }).not.to.throw()
+    // debug(result)
+    expect(result).to.have.property('status').which.equal('failed')
+  })
+  it('cancel.events should work with valid params', () => {
+    // create the event
+    const course = Factory.create('course')
+    const event = Factory.create('event', { courseId: course._id })
+    const user = Factory.create('user')
+    const member = Members.findOne({ userId: user._id })
+    const session = Factory.create('session', {
+      eventId: event._id,
+      memberId: member._id,
+      memberName: member.name,
+    })
+    debug('build event', event)
+    debug('build user', user)
+    debug('build session', session)
+    const thisContext = {
+      userId: user._id, // logged in
+    }
+    let result
+    expect(() => {
+      result = theMethod.apply(thisContext, [{ sessionId: session._id }])
     }).not.to.throw()
     debug({ result })
     expect(result).to.have.property('status').which.equal('success')
--- b2b-app/imports/test/factory.sessions.js
+++ b2b-app/imports/test/factory.sessions.js
@@ -4,4 +4,7 @@ import { Random } from 'meteor/random'
 import CONSTANTS from '/imports/api/constants'
 import Sessions from '/imports/api/sessions/schema'
 
-Factory.define('sessions', Sessions, {})
+Factory.define('session', Sessions, {
+  name: faker.lorem.words(3),
+  bookedAt: new Date(),
+})
