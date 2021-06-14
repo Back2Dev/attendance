8814437a0d6cf4b2e468342125da9c0eec03b342
Minh Nguyen <minhna@gmail.com>
2021-06-14 12:19:50 +0700

    working with the list filter

--- b2b-app/imports/ui/pages/services.js
+++ b2b-app/imports/ui/pages/services.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 
