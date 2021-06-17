14adccc27276522fcf26963ae07b02ba78d60c93
Tim <timiscoding@gmail.com>
2021-06-17 16:11:30 +1000

    Add stepper. Debug launch config.

--- b2b-app/imports/ui/admin/register/index.js
+++ b2b-app/imports/ui/admin/register/index.js
@@ -1,5 +1,110 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Stepper from '@material-ui/core/Stepper'
+import Step from '@material-ui/core/Step'
+import StepLabel from '@material-ui/core/StepLabel'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
-export default Register = () => {
-  return <div>Registration page</div>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '70%',
+  },
+  button: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}))
+
+function getSteps() {
+  return ['About you', 'Contact', 'Emergency', 'Avatar', 'Create account']
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return "Let's get to know each other"
+    case 1:
+      return 'Contact info'
+    case 2:
+      return 'Emergency contact'
+    case 3:
+      return 'Choose your profile picture'
+    case 4:
+      return 'Agree to terms and create account'
+  }
+}
+
+export default function HorizontalLinearStepper() {
+  const classes = useStyles()
+  const [activeStep, setActiveStep] = React.useState(0)
+  const steps = getSteps()
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  }
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
+  const handleReset = () => {
+    setActiveStep(0)
+  }
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label) => {
+          const stepProps = {}
+          const labelProps = {}
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          )
+        })}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>
+              All steps completed - you&apos;re finished
+              <pre>Form data that will be submitted goes here</pre>
+            </Typography>
+            <Button onClick={handleReset} className={classes.button}>
+              Reset
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>
+              {getStepContent(activeStep)}
+            </Typography>
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.button}
+              >
+                Back
+              </Button>
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}
+                className={classes.button}
+              >
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
