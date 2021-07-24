73c6fc613822d6c5c6b9d504bc47e4d8c61d1b7e
Tim <timiscoding@gmail.com>
2021-07-24 16:46:19 +1000

    Add storybook with sample stories

---
+++ b2b-app/.storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    '../imports/ui/**/*.stories.mdx',
+    '../imports/ui/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+}
