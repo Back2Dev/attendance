039629290a30443ded6f82bd0818345ab17de660
a-gorecki <andrew.gorecki@outlook.com>
2021-07-25 22:37:30 +1000

    update preview panel for new context schema

--- b2b-app/imports/ui/admin/forms/framework/previewPanel.js
+++ b2b-app/imports/ui/admin/forms/framework/previewPanel.js
@@ -4,5 +4,5 @@ import { EditorContext } from './framework'
 export const PreviewPanel = () => {
   const formContext = React.useContext(EditorContext)
 
-  return <p>{formContext.formEditorValue}</p>
+  return <p>{formContext.editors[0].editorValue}</p>
 }
