e043377be18531843e2cfa4bbdd118c663df9d1c
a-gorecki <andrew.gorecki@outlook.com>
2021-07-31 22:59:06 +1000

    run, save and autorun controls added

--- b2b-app/imports/ui/admin/forms/framework/editorPanel.js
+++ b2b-app/imports/ui/admin/forms/framework/editorPanel.js
@@ -5,6 +5,13 @@ import { EditorContext } from './framework'
 import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import IconButton from '@material-ui/core/IconButton'
+import SaveIcon from '@material-ui/icons/Save'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import Tooltip from '@material-ui/core/Tooltip'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
 
 require('codemirror/lib/codemirror.css')
 require('codemirror/theme/dracula.css')
@@ -29,6 +36,7 @@ export const EditorPanel = () => {
   const formContext = React.useContext(EditorContext)
 
   const [tab, setTab] = React.useState(0)
+  const [autoRun, setAutoRun] = React.useState(false)
 
   const handleTabChange = (e, index) => {
     setTab(index)
@@ -37,16 +45,43 @@ export const EditorPanel = () => {
   return (
     <div>
       <Paper square>
-        <Tabs
-          value={tab}
-          indicatorColor="primary"
-          textColor="primary"
-          onChange={handleTabChange}
-          aria-label="disabled tabs example"
-        >
-          <Tab label="details.form" />
-          <Tab label="detailsForm.json" />
-        </Tabs>
+        <div className="editorBar">
+          <Tabs
+            value={tab}
+            indicatorColor="primary"
+            textColor="primary"
+            onChange={handleTabChange}
+            aria-label="disabled tabs example"
+          >
+            <Tab label="details.form" />
+            <Tab label="detailsForm.json" />
+          </Tabs>
+          <div className="editorTools">
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={autoRun}
+                    onChange={() => setAutoRun(autoRun ? false : true)}
+                    name="autoRun"
+                    color="primary"
+                  />
+                }
+                label="AutoRun"
+              />
+            </FormGroup>
+            <Tooltip title="Run form">
+              <IconButton color="secondary" aria-label="run form">
+                <PlayArrowIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Save">
+              <IconButton color="primary" aria-label="save">
+                <SaveIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </div>
       </Paper>
 
       <CodeMirror
