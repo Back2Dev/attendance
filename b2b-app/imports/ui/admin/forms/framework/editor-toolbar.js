39db989f184a68a3ebbd46dc148f67d734e9d5ec
a-gorecki <andrew.gorecki@outlook.com>
2021-08-28 02:46:52 +1000

    settings popover wity autosave autorun toggles

--- b2b-app/imports/ui/admin/forms/framework/editor-toolbar.js
+++ b2b-app/imports/ui/admin/forms/framework/editor-toolbar.js
@@ -7,14 +7,17 @@ import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import IconButton from '@material-ui/core/IconButton'
 import SaveIcon from '@material-ui/icons/Save'
+import SettingsIcon from '@material-ui/icons/Settings'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow'
 import Tooltip from '@material-ui/core/Tooltip'
 import FormGroup from '@material-ui/core/FormGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Switch from '@material-ui/core/Switch'
+import Popover from '@material-ui/core/Popover'
 
 export const EditorToolbar = () => {
   const formContext = React.useContext(EditorContext)
+  const [settings, setSettings] = React.useState(false)
 
   const handleTabChange = (e, index) => {
     formContext.changeTab(index)
@@ -23,6 +26,10 @@ export const EditorToolbar = () => {
     } else {
       formContext.hideErrors()
     }
+  }
+
+  const handleSettings = () => {
+    setSettings(settings ? false : true)
   }
 
   return (
@@ -52,6 +59,58 @@ export const EditorToolbar = () => {
               label="AutoRun"
             />
           </FormGroup>
+          <Tooltip title="Settings">
+            <IconButton
+              color="grey"
+              aria-label="open settings"
+              onClick={() => {
+                handleSettings()
+              }}
+            >
+              <SettingsIcon />
+            </IconButton>
+          </Tooltip>
+          <Popover
+            open={settings}
+            onClose={handleSettings}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <div style={{ padding: '8px' }}>
+              <FormGroup row>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={formContext.autoRun}
+                      onChange={formContext.toggleAutoRun}
+                      name="autoRun"
+                      color="primary"
+                    />
+                  }
+                  label="AutoRun"
+                />
+              </FormGroup>
+              <FormGroup row>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={formContext.autoRun}
+                      onChange={formContext.toggleAutoRun}
+                      name="autoRun"
+                      color="primary"
+                    />
+                  }
+                  label="AutoSave"
+                />
+              </FormGroup>
+            </div>
+          </Popover>
           <Tooltip title="Run form">
             <IconButton
               color="secondary"
