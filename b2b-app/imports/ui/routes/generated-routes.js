0130a434fb9d340e7aafe651a0822c102f36a886
mikkel <mikkelking@hotmail.com>
2021-08-31 11:42:29 +1000

    tidy

--- b2b-app/imports/ui/routes/generated-routes.js
+++ b2b-app/imports/ui/routes/generated-routes.js
@@ -7,28 +7,15 @@ import { Route, Switch } from 'react-router-dom'
 import Loading from '/imports/ui/components/commons/loading.js'
 
 // Generated lazy imports go here...
-const SettingsList = lazy(() => import('/imports/ui/admin/settings/lister.js'))
 const UserList = lazy(() => import('/imports/ui/admin/users/lister'))
 const EventCalendar = lazy(() => import('/imports/ui/admin/calendar'))
-const Courses = lazy(() => import('/imports/ui/admin/courses'))
-const Tools = lazy(() => import('/imports/ui/admin/tools'))
-const Sessions = lazy(() => import('/imports/ui/admin/sessions'))
 const Registrations = lazy(() => import('/imports/ui/admin/registrations'))
 
 const Audits = lazy(() => import('/imports/ui/admin/audits/lister.js'))
-const Cronjobs = lazy(() => import('/imports/ui/admin/cronjobs/lister.js'))
-const Triggers = lazy(() => import('/imports/ui/admin/triggers/lister.js'))
-const MessageTemplates = lazy(() =>
-  import('/imports/ui/admin/message-templates/lister.js')
-)
 const Members = lazy(() => import('/imports/ui/admin/members/lister.js'))
-const Settings = lazy(() => import('/imports/ui/admin/settings/lister.js'))
-const Surveys = lazy(() => import('/imports/ui/admin/surveys/lister.js'))
-const Events = lazy(() => import('/imports/ui/admin/events/lister.js'))
 const ServiceItems = lazy(() => import('/imports/ui/admin/service-items/lister.js'))
 const Jobs = lazy(() => import('/imports/ui/admin/jobs/lister.js'))
 const Forms = lazy(() => import('/imports/ui/admin/forms'))
-const Users = lazy(() => import('/imports/ui/admin/users/lister'))
 
 //
 // This file contains a list of routes for database admin pages
