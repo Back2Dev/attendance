3af4debf202145443c2c0fff770752f46f40ff31
Pat carmel <pato@live.com.au>
2021-11-24 12:20:58 +1100

    working on the create services

--- b2b-app/tests/cypress/integration/working/create-service.js
+++ b2b-app/tests/cypress/integration/working/create-service.js
@@ -9,12 +9,21 @@ describe('log into app and create a service', () => {
     freshDatabase()
   })
 
-it('navigates to Back2bikes homepage and enters information and clicks sign in', function () {
+it('navigates to Back2bikes homepage logs in and switches role to admin', function () {
   cy.visit('/login')
   cy.get('[data-cy=email-input]').type(this.users.mike.username)
   cy.get('[data-cy=password-input]').type(this.users.mike.password)
   cy.get('[data-cy=login-btn]').should('exist').click()
+  cy.get('[data-cy=member-portal]').should('exist')
 
+  cy.get('[data-cy=primary-search-account-menu] > .MuiIconButton-label > .MuiAvatar-root > .MuiSvgIcon-root').click()
+
+  cy.get('#primary-search-account-menu > .MuiPaper-root > .MuiList-root > :nth-child(2)').contains('Switch role').click()
+  cy.get('[value="ADM"]').click()
 })
 
+it('opens admin menu and navigates to create service page', function () {
+ cy.get('.MuiToolbar-root > :nth-child(1) > .MuiIconButton-label > .MuiSvgIcon-root > path').click()
+
 })
+}) 
