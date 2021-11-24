8540195db81759515b43cc8fb80fc28c4b7166d7
Pat carmel <pato@live.com.au>
2021-11-24 11:48:31 +1100

    added the create services test

---
+++ b2b-app/tests/cypress/integration/working/create-service.js
@@ -0,0 +1,20 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test. We do this because of some ugly js errors
+  // from a js library we are using
+  return false
+})
+describe('log into app and create a service', () => {
+  before(function () {
+    freshDatabase()
+  })
+
+it('navigates to Back2bikes homepage and enters information and clicks sign in', function () {
+  cy.visit('/login')
+  cy.get('[data-cy=email-input]').type(this.users.mike.username)
+  cy.get('[data-cy=password-input]').type(this.users.mike.password)
+  cy.get('[data-cy=login-btn]').should('exist').click()
+
+})
+
+})
