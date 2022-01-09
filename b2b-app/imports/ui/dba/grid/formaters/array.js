b5897d4fb3da8dac2f20dedd9dad8d479b8d7e51
Minh Nguyen <minhna@gmail.com>
2022-01-10 00:10:29 +0700

    display tags formater

--- b2b-app/imports/ui/dba/grid/formaters/array.js
+++ b2b-app/imports/ui/dba/grid/formaters/array.js
@@ -1,10 +1,23 @@
-// import React from 'react'
+import React from 'react'
+import Chip from '@material-ui/core/Chip'
+import styled from 'styled-components'
+
+const StyledChip = styled(Chip)`
+  height: 28px;
+  margin-right: 5px;
+`
 
 const ArrayFormater = ({ column, row }) => {
   if (!row[column.key]) {
     return null
   }
-  return JSON.stringify(row[column.key], null, 2)
+  if (typeof row[column.key] !== 'object' || !row[column.key].length) {
+    return JSON.stringify(row[column.key], null, 2)
+  }
+  return row[column.key].map((item) => {
+    const label = typeof item === 'string' ? item : JSON.stringify(item)
+    return <StyledChip key={label} label={label} />
+  })
 }
 
 export default ArrayFormater
