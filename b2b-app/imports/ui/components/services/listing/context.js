21f31bac28a03e61b45a02bb001e64cc745590f5
Minh Nguyen <minhna@gmail.com>
2022-02-14 23:11:04 +0700

    improve default listing sort terms

--- b2b-app/imports/ui/components/services/listing/context.js
+++ b2b-app/imports/ui/components/services/listing/context.js
@@ -65,7 +65,7 @@ export const JobsListingProvider = ({ children }) => {
   const { loading, jobs } = useTracker(() => {
     // TODO: change the subscription, add permission checking
     const sub = Meteor.subscribe('all.jobs')
-    const jobs = Jobs.find({}, { sort: { pickupDate: -1 } }).fetch()
+    const jobs = Jobs.find({}, { sort: { pickupDate: -1, createdAt: -1 } }).fetch()
 
     return {
       loading: !sub.ready(),
