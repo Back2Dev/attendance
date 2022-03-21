508fa573b841679d08b1da3b938df99795c3d70d
Pat carmel <pato@live.com.au>
2022-03-21 15:35:02 +1100

    working on add member page

--- b2b-app/imports/ui/admin/standup-notes/add-member.js
+++ b2b-app/imports/ui/admin/standup-notes/add-member.js
@@ -1,24 +1,55 @@
-import { Label } from '$sb/components/question/question.stories'
+import { Button, Select, Typography } from '@material-ui/core'
 import React from 'react'
 
-const addmember = () => {
+const options = []
+
+const AddMember = () => {
   return (
     <div
       style={{
         display: 'flex',
         flexWrap: 'wrap',
-        // justifyContent: 'center',
-        flexDirection: 'row',
-        //   alignItems: 'flex-start',
+        //   justifyContent: 'space-around',
+        flexDirection: 'column',
+        alignContent: 'flex-start',
       }}
     >
-      <h1>"Add team Member" </h1>
-
-      <Button name="add-member" colour="purple" />
-
-      <Button name="cancel" colour="blue" />
+      <h1>Add team Member </h1>
+      <Typography variant="caption" component="div" gutterBottom style={{ order: 1 }}>
+        Add team member
+      </Typography>
+      <Select
+        type="Input"
+        options={options}
+        style={{ width: '50px', height: '70px', order: 1 }}
+      />
+      <Button
+        name="add-member"
+        style={{
+          width: '200px',
+          height: '70px',
+          order: 3,
+          rowGap: '40px',
+          columnGap: '50px',
+          alignSelf: 'flex-end',
+        }}
+      >
+        Add
+      </Button>
+      <Button
+        name="cancel"
+        style={{
+          width: '200px',
+          height: '70px',
+          backgroundcolor: 'pink',
+          order: 4,
+          alignSelf: 'center',
+        }}
+      >
+        Cancel
+      </Button>
     </div>
   )
 }
 
-export default addmember
+export default AddMember
--- b2b-app/imports/ui/admin/standup-notes/add-member.stories.js
+++ b2b-app/imports/ui/admin/standup-notes/add-member.stories.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-import addmember from './add-member'
+import AddMember from './add-member'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}))
 
 export default {
-  title: 'Add Member',
-  component: addmember,
+  title: 'Standup/Addmember',
+  component: AddMember,
   parameters: {
     viewport: {
       // defaultViewport:  'iphone6',
