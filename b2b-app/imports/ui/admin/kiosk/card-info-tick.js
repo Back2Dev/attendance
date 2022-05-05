569b74fee94400ab38fd06577beb2270c78ae78a
Pat carmel <pato@live.com.au>
2022-05-05 14:50:53 +1000

    added avatar list story and js file

--- b2b-app/imports/ui/admin/kiosk/card-info-tick.js
+++ b2b-app/imports/ui/admin/kiosk/card-info-tick.js
@@ -4,18 +4,21 @@ import { makeStyles } from '@material-ui/core/styles'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import { CardActions } from '@material-ui/core'
-import { CardActionArea, Typography, Button } from '@material-ui/core'
+import { CardActionArea, Typography, Button, Label } from '@material-ui/core'
 
 const useStyles = makeStyles({
   root: {
     width: '345px',
   },
-  media: {
-    height: 164,
+  avatar: {
+    height: '350px',
+  },
+  badge: {
+    width: '40px',
   },
 })
 
-const CardInfoTick = () => {
+const CardInfoTick = ({ childcheck, worktype, visitno }) => {
   const classes = useStyles()
   return (
     <div>
@@ -23,7 +26,8 @@ const CardInfoTick = () => {
 
       <Card className={classes.root}>
         <CardActionArea>
-          <CardMedia className={classes.media} image="/images/avatars/1.jpg" />
+          <CardMedia className={classes.avatar} image="/images/avatars/1.jpg" />
+
           <CardContent>
             <Typography variant="body2">Patrick Carmel</Typography>
           </CardContent>
@@ -31,6 +35,10 @@ const CardInfoTick = () => {
             <Button color="primary" disabled="true">
               Casual
             </Button>
+
+            {childcheck && <img className={classes.badge} src="/badges/star.png" />}
+            {visitno && <img className={classes.badge} src="/badges/card.png" />}
+            {worktype && <img className={classes.badge} src="/badges/cup.jpg" />}
           </CardActions>
         </CardActionArea>
       </Card>
