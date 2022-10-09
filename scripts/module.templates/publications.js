f8a0855123cc2e8d9bb51c2b7c6a9e1a3c6b80af
Mike King <mikkelking@hotmail.com>
2022-10-09 14:50:19 +1100

    limit all.collection publication to 50 records

--- scripts/module.templates/publications.js
+++ scripts/module.templates/publications.js
@@ -1,19 +1,12 @@
 import { Meteor } from 'meteor/meteor'
 import MyCollection from '../schema'
 import '../METHODS_FILE'
-/* Commented out related publications (if any) - best to add these in manually as required
-MyPubImports 
-*/
 
 Meteor.publish('all.myCollection', () => {
-  return MyCollection.find({})
+  // Note: We limit to 50 records, because this could be a memory expensive publication
+  return MyCollection.find({}, { limit: 50, sort: { createdAt: -1 } })
 })
 
 Meteor.publish('id.myCollection', (id) => {
-  return [
-    MyCollection.find(id),
-    /* Commented out related publications (if any) - best to add these in manually as required
-    RelatedPublications 
-    */
-  ]
+  return [MyCollection.find(id)]
 })
