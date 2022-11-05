c41dcc239f8c3759c987ea2572586d52e6e8a01a
Akila Helitha Paranawithana <akila.codes@gmail.com>
2022-11-05 11:52:54 +0530

    Added browse.js and browser.js to path

--- b2b-app/imports/ui/admin/pdf-templates/index.js
+++ b2b-app/imports/ui/admin/pdf-templates/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Loading from '/imports/ui/components/commons/loading.js'
+// import Loading from '/imports/ui/components/commons/loading.js'
 
 import Lister from './lister.js'
 import Editor from './editor.js'
 import Viewer from './viewer.js'
 import Adder from './adder.js'
 import NotFound from '/imports/ui/components/commons/not-found.js'
+import Browser from './browser.js'
 
 export default function PdfTemplates() {
   return (
@@ -16,6 +17,7 @@ export default function PdfTemplates() {
       <Route path="/admin/pdf-templates/add/" exact component={Adder} />
       <Route path="/admin/pdf-templates/view/:id" exact component={Viewer} />
       <Route path="/admin/pdf-templates" exact component={Lister} />
+      <Route path="/admin/pdf-templates/browse" exact component={Browser} />
       <Route component={NotFound} />
     </Switch>
   )
