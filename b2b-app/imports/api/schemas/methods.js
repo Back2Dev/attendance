6c990a56a258f2b0c2ce3b0edd9992b1db2ddd71
Akila Helitha Paranawithana <akila.codes@gmail.com>
2022-11-18 17:22:20 +0530

    Added methods for schemas.collections

--- b2b-app/imports/api/schemas/methods.js
+++ b2b-app/imports/api/schemas/methods.js
@@ -1,12 +1,13 @@
 // @ts-check
 import { Meteor } from 'meteor/meteor'
-import Schemas from './schema'
+import Schemas, { SchemasCollections } from './schema'
 require('debug')('app:schemas')
 import {
   deleteSchema,
   compileData,
   initAllSchemaDocuments,
   compileEditedSchemaDocs,
+  performAllDocumentValidations,
 } from './functions'
 
 // Initialize all the schemas
@@ -50,6 +51,83 @@ Meteor.methods({
       form._id = id
       compileEditedSchemaDocs([form])
       return { status: 'success', message: 'Added schema' }
+    } catch (e) {
+      return {
+        status: 'failed',
+        message: `Error adding schema: ${e.message}`,
+      }
+    }
+  },
+
+  // SCHEMAS_COLLECTIONS
+  'rm.schemas.collections': (form) => {
+    try {
+      SchemasCollections.find(form.id).forEach((doc) => {
+        if (
+          doc.collections.some((slug) => form.collection === slug) &&
+          doc.collections.length === 0
+        ) {
+          SchemasCollections.remove(form.id)
+        } else {
+        }
+      })
+      return { status: 'success', message: 'Removed document' }
+    } catch (e) {
+      return {
+        status: 'failed',
+        message: `Error removing document: ${e.message}`,
+      }
+    }
+  },
+  'update.schemas.collections': (form) => {
+    try {
+      const id = form._id
+      delete form._id
+      const document = SchemasCollections.find(id).fetch()[0]
+      /**
+       * @type {string[]}
+       */
+      const collections = document.collections
+      const isDocumentInCollection = collections.some((slug) => slug === form.collection)
+      if (!isDocumentInCollection) {
+        collections.push(form.collection)
+      }
+      const collection = form.collection
+      delete form.collection
+      const errors = performAllDocumentValidations(true, collection, form, false)
+
+      if (errors.length > 0) {
+        throw { message: errors.join(', ') }
+      }
+
+      if (!isDocumentInCollection) {
+        form.collections = collections
+      }
+
+      const n = SchemasCollections.update(id, { $set: form })
+      return { status: 'success', message: `Updated ${n} schema(s)` }
+    } catch (e) {
+      return {
+        status: 'failed',
+        message: `Error updating schema: ${e.message}`,
+      }
+    }
+  },
+  'insert.schemas.collections': (form) => {
+    try {
+      const collection = form.collection
+      delete form.collection
+      const errors = performAllDocumentValidations(true, collection, form, true)
+
+      if (errors.length > 0) {
+        throw { message: errors.join(', ') }
+      }
+
+      form.collections = [collection]
+
+      const id = SchemasCollections.insert(form)
+      form._id = id
+      return { status: 'success', message: 'Added document' }
     } catch (e) {
       return {
         status: 'failed',
