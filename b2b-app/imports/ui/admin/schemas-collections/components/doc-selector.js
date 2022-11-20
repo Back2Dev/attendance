8dd5d47f16e165de2c54b5b5de4c41b6b7710dee
Akila Helitha Paranawithana <akila.codes@gmail.com>
2022-11-20 22:52:15 +0530

    Added doc selector components(dropdown)

---
+++ b2b-app/imports/ui/admin/schemas-collections/components/doc-selector.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { SelectField } from 'uniforms-material'
+import { connectField } from 'uniforms'
+import { meteorCall } from '/imports/ui/utils/meteor'
+import { Meteor } from 'meteor/meteor'
+import { SchemasCollections } from '../../../../api/schemas/schema'
+import { useTracker } from 'meteor/react-meteor-data'
+
+function compileDisplayString(doc, displayFormat) {
+  const tmpDoc = { ...doc }
+  delete tmpDoc.collections
+  delete tmpDoc._id
+  try {
+    return eval('`' + displayFormat.replaceAll('{', '${doc.') + '`')
+  } catch (e) {
+    return Object.values(tmpDoc).join(', ')
+  }
+}
+
+function SchemaDocumentSelectorCreator(field) {
+  function SchemaDocumentSelector(props) {
+    const slug = field.relatedCollection
+
+    const [schema, setSchema] = useState({})
+
+    useEffect(async () => {
+      const response = await meteorCall('all.fields.schemas', undefined, slug, false)
+      setSchema(response.data)
+    }, [])
+
+    const { isLoading, data } = useTracker(() => {
+      const subscription = Meteor.subscribe('all.schemas.collections', slug)
+      let data = SchemasCollections.find({
+        collections: {
+          $in: schema.descendants || [],
+        },
+      }).fetch()
+      return { isLoading: subscription.ready(), data }
+    }, [schema])
+
+    if (!schema.descendants || !data) return <>Loading...</>
+    return (
+      <SelectField
+        allowedValues={data.map((doc) => doc._id)}
+        transform={(_id) => {
+          const doc = data.filter((doc) => doc._id === _id)[0]
+          return compileDisplayString(doc, field.displayFormat)
+        }}
+      />
+    )
+  }
+  return connectField(SchemaDocumentSelector)
+}
+
+export { SchemaDocumentSelectorCreator }
